5180df1f3b3b11286694dc65c9d2bde62af52139
Yashu Mittal <mittalyashu77@gmail.com>
2020-01-26 18:22:16 +0530

    Find member, save new member to database with hash password
    Validate member before adding new member to database with hash password and UUID

---
+++ server/controllers/auth/signup.js
@@ -0,0 +1,72 @@
+// modules 
+const bcrypt = require('bcrypt');
+const uuid = require('uuid/v1');
+
+const database = require("../../database");
+
+exports.signup = (req, res, next) => {
+  const email = req.body.email;
+  // password cannot be longer than 72 characters
+  const password = req.body.password;
+
+  // validate member existance in database
+  database.query(`
+    SELECT
+      email_address
+    FROM
+      member
+    WHERE
+      email_address = '${email}';
+  `).then(res => {
+    /**
+     * res.rows
+     * returns array of object
+     */
+    if (res.rows[0]) {
+      res.status(409).send({
+        "status": {
+          "code": 409,
+          "type": "error"
+        },
+        user: res.rows[0],
+        "error": {
+          "code": "invalid_email",
+          "message": "E-Mail already taken" 
+        }
+      })
+      next();
+    } else {
+      // password hashing
+      const bcryptSaltRounds = 10;
+      const bcryptSalt = bcrypt.genSaltSync(bcryptSaltRounds);
+      const passwordHash = bcrypt.hashSync(password, bcryptSalt);
+
+      // generate unique indentification
+      const memberId = uuid(email)
+
+      // save member to database
+      database
+        .query(`
+          INSERT INTO
+            member(member_id, email_address, password)
+          VALUES
+            ('${memberId}', '${email}', '${passwordHash}');
+        `)
+        .then(res => {
+          res.status(200).send({
+            status: {
+              code: 200,
+              type: "success"
+            }
+          })
+          next();
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+  }).catch(error => {
+    console.error(error);
+  })
+
+}
