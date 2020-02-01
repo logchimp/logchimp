eff9775794e003c473b99f8a86cfee427aafd093
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-01 11:16:03 +0530

    Signup APIs (#1)
    * Find member, save new member to database with hash password
    
    Validate member before adding new member to database with hash password and UUID
    
    * Install bcrypt@3.0.7 and uuid@3.4.0 package
    
    * Use bodyParser as middleware
    
    * create /auth/signup route
    
    Create signUp route available at /api/v1/auth/signup
    
    * rename database.query local variable res to result
    
    Renaming local variables defined in database.query() which causes conflict with functions argument variables.
    
    * return NOT NULL data from database
    
    Returning (member_id, email_address, created_at) those values after adding to database.
    
    * generate authToken using jwt package
    
    authToken generated using `member_id`, `email_address`, `created_at` expire after 1 hr and will be append in URL for the account verification via email.
    
    * todo: send email for account verification
    
    * remove user data from response
    
    Removing user data from response after finding the user in database.
    
    It's also not practice for security purposes.
    
    * Install jsonwebtoken@8.5.1 package

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // packages
 const express = require("express");
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const database = require("./database");
 const routes = require("./routes/v1");
@@ -16,6 +17,10 @@ database.connect().then(err => {
 
     // start express server at SERVER_PORT
     app.listen(process.env.SERVER_PORT, () => {
+      
+      // contains key-value pairs of data submitted in the request body
+      app.use(bodyParser.json()) 
+
       // enable all CORS requests
       app.use(cors());
 
