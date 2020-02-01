5b98279c823cd2a77f13e81c8177ce747452ba81
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-01 11:16:03 +0530

    Signup APIs (#1)
    * Find member, save new member to database with hash password
    
    Validate member before adding new member to database with hash password and UUID
    
    * Install bcrypt@3.0.7 and uuid@3.4.0 package
    
    * Use bodyParser as middleware
    
    * create /auth/signup route
    
    Create signUp route available at /api/v1/auth/signup
    
    * rename database.query local variable res to result
    
    Renaming local variables defined in database.query() which causes conflict with functions argument variables.
    
    * return NOT NULL data from database
    
    Returning (member_id, email_address, created_at) those values after adding to database.
    
    * generate authToken using jwt package
    
    authToken generated using `member_id`, `email_address`, `created_at` expire after 1 hr and will be append in URL for the account verification via email.
    
    * todo: send email for account verification
    
    * remove user data from response
    
    Removing user data from response after finding the user in database.
    
    It's also not practice for security purposes.
    
    * Install jsonwebtoken@8.5.1 package

---
+++ server/controllers/auth/signup.js
@@ -0,0 +1,85 @@
+// modules 
+const bcrypt = require('bcrypt');
+const uuid = require('uuid/v1');
+const jwt = require('jsonwebtoken');
+
+const database = require("../../database");
+
+exports.signup = (req, res, next) => {
+  const email = req.body.email;
+  // password cannot be longer than 72 characters
+  const password = req.body.password;
+
+  // validate member existance in database
+  database.query(`
+    SELECT
+      email_address
+    FROM
+      member
+    WHERE
+      email_address = '${email}';
+  `).then(result => {
+    /**
+     * res.rows
+     * returns array of object
+     */
+    if (result.rows[0]) {
+      res.status(409).send({
+        "status": {
+          "code": 409,
+          "type": "error"
+        },
+        "error": {
+          "code": "invalid_email",
+          "message": "E-Mail already taken" 
+        }
+      })
+      next();
+    } else {
+      // password hashing
+      const bcryptSaltRounds = 10;
+      const bcryptSalt = bcrypt.genSaltSync(bcryptSaltRounds);
+      const passwordHash = bcrypt.hashSync(password, bcryptSalt);
+
+      // generate unique indentification
+      const memberId = uuid(email)
+
+      // save member to database
+      database
+        .query(`
+          INSERT INTO
+            member(member_id, email_address, password)
+          VALUES
+            ('${memberId}', '${email}', '${passwordHash}')
+          RETURNING
+            member_id, email_address, created_at;
+        `)
+        .then(result => {
+          const newMember = result.rows[0];
+
+          /**
+           * authToken sent via email will expire after 1 hr
+           */
+          const authToken = jwt.sign(newMember, 'scretKey', { expiresIn: '1h' });
+
+          res.status(200).send({
+            status: {
+              code: 200,
+              type: "success"
+            },
+            member: newMember
+          })
+
+          // todo: send email for account verification
+
+          next();
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    }
+  }).catch(error => {
+    console.error(error);
+  })
+
+}
