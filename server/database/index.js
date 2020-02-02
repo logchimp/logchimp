b4759d2af1f85883ffd710734a49aeeadab87342
Tejas H <CodingCreate101@users.noreply.github.com>
2020-02-02 11:17:22 +0530

    Added bcrypt + few additional settings for DB config. (#9)
    * Added bcrypt + few additional settings for DB config.
    
    * Fixed merge conflict and un-did app.js changes

--- server/database/index.js
+++ server/database/index.js
@@ -4,7 +4,8 @@ const client = new Client({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT
+  port: process.env.PG_PORT,
+  password: process.env.PG_PASSWORD,
 });
 
 module.exports = client
