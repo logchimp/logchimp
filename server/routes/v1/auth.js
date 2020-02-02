6bb30a2e75efde010c4acac94d811db87aeb064b
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-02 18:22:07 +0530

    Login API (#4)
    * export auth modules from auth.js
    
    * login API
    
    By providing email and password returns member relavent information along with member auth token.
    
    * remove next() function after response dispatch
    
    * change console.log to console.error
    
    Co-authored-by: Tejas H <CodingCreate101@users.noreply.github.com>

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 // controller
-const auth = require('../../controllers/auth/signup');
+const auth = require('../../controllers/auth');
 
 router.post('/auth/signup', auth.signup)
+router.post('/auth/login', auth.login)
 
 module.exports = router
