8aa2145fd9e9a380078621f4568e31d900cfa45d
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-02 09:16:37 +0530

    update eslint@6.8.0 & create configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+	parserOptions: {
+		parser: "babel-eslint",
+		sourceType: "module",
+		ecmaVersion: 8
+	},
+	env: {
+		browser: true,
+		node: true
+	},
+  extends: ["eslint:recommended", "plugin:vue/essential"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    camelcase: 1,
+    "no-console": 1,
+    "vue/camelcase": "error"
+  }
+}
