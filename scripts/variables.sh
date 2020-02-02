b4f9b9a02b525a7bcac5044506046ce5f5d5f458
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-02 11:02:31 +0530

    Bash scripts for database setup (#6)
    * bash scripts to create tables
    
    This bash scripts creates all the table require for logchimp project.
    
    * script for deleting all database tables
    
    * remove extra closing bracket INSERT query

---
+++ scripts/variables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# postgreSQL database
+export PG_DATABASE=logchimp
+export PG_HOST=localhost
+export PG_USER=yashumittal
+export PG_PORT=5432
+
+# express
+export SERVER_PORT=3000
