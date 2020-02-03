c7916e8148515cc711078c7b49110ff85b6f70c8
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-03 12:28:24 +0530

    Token validation middleware (#13)
    * token validation middleware
    
    Getting the token from the request headers and using it for validation.
    
    * output console.error(err) for jwt token verification

---
+++ server/middlewares/token.js
@@ -0,0 +1,46 @@
+// modules
+const jwt = require('jsonwebtoken');
+
+exports.validate = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (token) {
+    // grab token from headers request
+    const headersToken = token.split(" ")[1];
+
+    jwt.verify(headersToken, "secretKey", err => {
+      if (err) {
+        console.error(err);
+
+        /**
+         * sending response to client
+         * only on if the token is missing
+         */
+        res.status(401).send({
+          status: {
+            code: 401,
+            type: "error"
+          },
+          error: {
+            code: "token_invalid",
+            message: "Token is invalid"
+          }
+        })
+      } else {
+        // move to next middleware on success
+        next();
+      }
+    })
+  } else {
+    res.status(401).send({
+      status: {
+        code: 401,
+        type: "error"
+      },
+      error: {
+        code: "token_missing",
+        message: "Token is missing"
+      }
+    })
+  }
+}
