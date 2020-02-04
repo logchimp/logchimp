224e30b29657e476c6924bfef5dd19fadf54dbb4
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-04 12:36:44 +0530

    filterPost based on sort type
    Filtering posts data based on query parameter and querying the database based on that.

--- server/controllers/post/index.js
+++ server/controllers/post/index.js
@@ -1,10 +1,12 @@
 const create = require('./create');
+const filterPost = require('./filterPost');
 const getPostById = require('./getPostById');
 const updatePostById = require('./updatePostById');
 const deleteById = require('./deleteById');
 
 module.exports = {
 	...create,
+	...filterPost,
 	...getPostById,
 	...updatePostById,
 	...deleteById
--- server/routes/v1/posts.js
+++ server/routes/v1/posts.js
@@ -9,6 +9,7 @@ const post = require('../../controllers/post');
 const token = require('../../middlewares/token');
 
 router.post('/post/create', token.validate, post.create)
+router.get('/posts', post.filterPost)
 router.get('/post/:slug', post.getPostById)
 router.patch('/post/update/:postId', token.validate, post.updatePostById)
 router.delete('/post/delete', token.validate, post.deleteById)
