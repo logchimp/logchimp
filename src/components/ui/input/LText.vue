cc020fc4e3dad348a7940246396d9ce2112547d4
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-04 13:57:29 +0530

    Auth modal (login & signup) (#11)
    * install axios@0.19.2 package
    
    * create primary button component
    
    * create text input component
    
    * create modal component
    
    * auth modal for signup
    
    * move global and body tag styles to main.sass
    
    Import 'Kulin park' font using remote url

---
+++ src/components/ui/input/LText.vue
@@ -0,0 +1,26 @@
+<template>
+	<input
+		:type="type"
+		class="input__text"
+		:class="{
+			'disabled': disabled
+		}"
+		@input="$emit('input', $event.target.value)"
+	/>
+</template>
+
+<script>
+export default {
+	name: "LText",
+	props: {
+		type: {
+			type: String,
+			default: "text"
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		}
+	}
+};
+</script>
---
+++ src/components/ui/modal/LModal.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="modal">
+		<div
+			class="modal__card"
+			:style="{
+				width: `${modelSize}px`
+			}"
+		>
+			<div class="modal__card-header">
+				<slot name="header" />
+			</div>
+			<div class="modal__card-body">
+				<slot />
+			</div>
+			<div class="modal__card-footer">
+				<slot name="footer" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "LModal",
+	props: {
+		size: {
+			type: String,
+			required: true
+		}
+	},
+	computed: {
+		modelSize() {
+			switch (this.size) {
+				case "small":
+				default:
+					return "450";
+			}
+		}
+	}
+};
+</script>
