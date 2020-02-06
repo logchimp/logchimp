06bb9ba563b87c9bce5e2978db0fd08534cc9881
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-06 11:40:13 +0530

    import auth modal to header component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,43 +1,51 @@
 <template>
-	<header>
-		<div class="container header__container">
-			<div class="header__logo">
-				<img src="@/assets/images/logo_invert_color.svg" />
-			</div>
-			<nav class="header__nav">
-				<div class="nav__list">
-					<div class="nav__item">
-						<img @click="toggleProfileDropdown" class="nav__profile" src="https://picsum.photos/200" />
-						<dropdown v-show="profileDropdown" class="nav__profile-dropdown">
-							<dropdown-item>
-								<template v-slot:icon>
-									<settings-icon />
-								</template>
-								Settings
-							</dropdown-item>
-							<dropdown-item>
-								<template v-slot:icon>
-									<logout-icon />
-								</template>
-								Sign out
-							</dropdown-item>
-						</dropdown>
-					</div>
-					<div class="nav__item nav__auth">
-						<div class="nav__auth-link">Login</div>
-						/
-						<div class="nav__auth-link">Signup</div>
-					</div>
+	<div>
+		<header>
+			<div class="container header__container">
+				<div class="header__logo">
+					<img src="@/assets/images/logo_invert_color.svg" />
 				</div>
-			</nav>
-		</div>
-	</header>
+				<nav class="header__nav">
+					<div class="nav__list">
+						<div class="nav__item">
+							<img
+								@click="toggleProfileDropdown"
+								class="nav__profile"
+								src="https://picsum.photos/200"
+							/>
+							<dropdown v-show="profileDropdown" class="nav__profile-dropdown">
+								<dropdown-item>
+									<template v-slot:icon>
+										<settings-icon />
+									</template>
+									Settings
+								</dropdown-item>
+								<dropdown-item>
+									<template v-slot:icon>
+										<logout-icon />
+									</template>
+									Sign out
+								</dropdown-item>
+							</dropdown>
+						</div>
+						<div @click="toggleAuthModal" class="nav__item nav__auth">
+							<div class="nav__auth-link">Login</div>
+							/
+							<div class="nav__auth-link">Signup</div>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</header>
+		<auth-modal v-if="authModal" />
+	</div>
 </template>
 
 <script>
 // components
 import Dropdown from "../ui/dropdown/DropdownGroup";
 import DropdownItem from "../ui/dropdown/DropdownItem";
+import AuthModal from "./Auth";
 
 // icons
 import SettingsIcon from "../../assets/images/settings";
@@ -47,18 +55,23 @@ export default {
 	name: "Header",
 	data() {
 		return {
-			profileDropdown: false
-		}
+			profileDropdown: false,
+			authModal: false
+		};
 	},
 	components: {
 		Dropdown,
 		DropdownItem,
+		AuthModal,
 		SettingsIcon,
 		LogoutIcon
 	},
 	methods: {
 		toggleProfileDropdown() {
 			this.profileDropdown = !this.profileDropdown;
+		},
+		toggleAuthModal() {
+			this.authModal = !this.authModal;
 		}
 	}
 };
