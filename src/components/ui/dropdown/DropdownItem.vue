c479552f49573495ad52baebd14128a8110a4557
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-06 11:45:49 +0530

    Header & Navbar (#15)
    * dropdown component
    
    Create dropdownGroup and dropdownItem component
    
    * base container class styles
    
    * main header component
    
    Header include logchimp inverted color logo, profile picture and login/signup buttons.
    
    * navbar component for navgiation b/w app
    
    * import auth modal to header component

---
+++ src/components/ui/dropdown/DropdownItem.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="dropdown__item">
+		<div class="dropdown__item-icon">
+			<slot name="icon"/>
+		</div>
+		<div class="dropdown__item-content">
+			<slot/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DropdropItem"
+}
+</script>
