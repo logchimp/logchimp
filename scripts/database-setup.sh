3b017acd1d3fd72d92c70d19305c095196f82db3
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-06 17:47:44 +0530

    remove category_id & status_id from create post API
    remove NOT NULL from post database table column category_id & status_id.

--- scripts/database-setup.sh
+++ scripts/database-setup.sh
@@ -38,8 +38,8 @@ psql \
     slug VARCHAR(150) NOT NULL,
     body_markdown TEXT,
     member_id VARCHAR(80) NOT NULL REFERENCES member(member_id),
-    category_id VARCHAR(80) NOT NULL REFERENCES category(category_id),
-    status_id VARCHAR(80) NOT NULL REFERENCES status(status_id),
+    category_id VARCHAR(80) REFERENCES category(category_id),
+    status_id VARCHAR(80) REFERENCES status(status_id),
     voters VARCHAR [],
     created_at timestamp DEFAULT current_timestamp,
     updated_at timestamp DEFAULT current_timestamp
