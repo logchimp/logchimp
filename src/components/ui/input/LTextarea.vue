3546e6da02558c22677c4dfa6abf6b6ecd3346a4
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-06 17:50:45 +0530

    textarea component

---
+++ src/components/ui/input/LTextarea.vue
@@ -0,0 +1,25 @@
+<template>
+	<textarea
+		class="input__field input__textarea"
+		:class="{
+			'input__field-disabled': disabled
+		}"
+		@input="$emit('input', $event.target.value)"
+	/>
+</template>
+
+<script>
+export default {
+	name: "LTextarea",
+	props: {
+		type: {
+			type: String,
+			default: "text"
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		}
+	}
+};
+</script>
