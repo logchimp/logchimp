da49373cd84f2771784994ef83a36cfe4262d3d0
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-06 07:24:53 +0530

    create post component (#17)
    Showing number of upvotes, title, bodyMarkdown.

---
+++ src/components/layout/Post.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="post">
+		<div class="post__voters">
+			<arrow-icon
+				class="post__voters-arrow"
+				:class="{ 'post__voters-vote': vote }"
+			/>
+			{{ voters }}
+		</div>
+		<div class="post__content">
+			<h3 class="post__content-title">{{ title }}</h3>
+			<p class="post__content-bodyMarkdown" v-html="bodyMarkdown" />
+		</div>
+	</div>
+</template>
+
+<script>
+import ArrowIcon from "../../assets/images/icons/arrow";
+
+export default {
+	name: "post",
+	components: {
+		ArrowIcon
+	},
+	props: {
+		title: {
+			type: String,
+			required: true
+		},
+		bodyMarkdown: {
+			type: String,
+			default: ""
+		},
+		voters: {
+			type: Number,
+			default: 0
+		},
+		vote: {
+			type: Boolean,
+			default: false
+		}
+	}
+};
+</script>
