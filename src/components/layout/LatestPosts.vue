7b7ddd8d297696ee9a5579b9a0b9e5dc763b1938
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-06 13:16:08 +0530

    fetch latest posts on mounted

--- src/components/layout/LatestPosts.vue
+++ src/components/layout/LatestPosts.vue
@@ -1,11 +1,49 @@
 <template>
-	<div>
-		Latests posts
+	<div class="latest__post">
+		<post
+			v-for="post in posts"
+			:title="post.title"
+			:slug="post.slug"
+			:bodyMarkdown="post.body_markdown"
+			:key="post.post_id"
+		/>
 	</div>
 </template>
 
 <script>
+// packages
+import axios from "axios";
+
+// components
+import Post from "../layout/Post";
+
 export default {
-	name: "LatestPosts"
+	name: "LatestPosts",
+	data() {
+		return {
+			posts: []
+		};
+	},
+	components: {
+		Post
+	},
+	methods: {
+		getPosts() {
+			const urlQuery = new URLSearchParams();
+			urlQuery.append("sort", "latest");
+
+			axios
+				.get(`${process.env.VUE_APP_SEVER_URL}/api/v1/posts`, urlQuery)
+				.then(response => {
+					this.posts = response.data.posts;
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
+	},
+	mounted() {
+		this.getPosts();
+	}
 };
 </script>
