f46c75e8b06efdfce0e1a8741f1238e83709f66b
Yashu Mittal <mittalyashu77@gmail.com>
2020-02-09 14:08:30 +0530

    use knex - sql query buider (#27)
    * Install knex@0.20.8 package
    
    * use knex query builder for db connection
    
    * use knex for database interactions
    
    Update codebase by replacing native sql queries with js using knex package.

--- server/controllers/post/updatePostById.js
+++ server/controllers/post/updatePostById.js
@@ -5,8 +5,6 @@ exports.updatePostById = (req, res, next) => {
 	const postTitle = req.body.title;
 	const slugId = req.body.slugId;
 	const bodyMarkdown = req.body.bodyMarkdown;
-	const categoryId = req.body.categoryId;
-	const statusId = req.body.statusId;
 
 	const slug = `${
 		postTitle
@@ -21,29 +19,30 @@ exports.updatePostById = (req, res, next) => {
    * note: do not add memberId and postId
    * inside this query
    */
-	database.query(`
-    UPDATE post
-    SET
-      title = '${postTitle}',
-      slug = '${slug}',
-      body_markdown = '${bodyMarkdown}',
-      category_id = '${categoryId}',
-      status_id = '${statusId}',
-      updated_at = current_timestamp
-    WHERE
-      post_id = '${postId}'
-    RETURNING *
-  ;`).then(post => {
-		const postData = post.rows[0];
-
-		res.status(200).send({
-			status: {
-				code: 200,
-				type: "success"
-			},
-			post: postData
+	database
+		.update({
+			title: postTitle,
+			slug,
+			slug_id: slugId,
+			body_markdown: bodyMarkdown,
+			updated_at: new Date()
 		})
-	}).catch(error => {
-		console.error(error);
-	});
+		.from("post")
+		.where({
+			post_id: postId
+		})
+		.returning('*')
+		.then(post => {
+			const postData = post[0];
+
+			res.status(200).send({
+				status: {
+					code: 200,
+					type: "success"
+				},
+				post: postData
+			})
+		}).catch(error => {
+			console.error(error);
+		});
 }
