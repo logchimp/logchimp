7a7335f36516c0e6ca14d522d2a98ac13476a7a8
Yashu Mittal <mittalyashu77@gmail.com>
2020-06-02 08:37:17 +0530

    Import vue-moment in Vue instance

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./routes";
+import moment from "vue-moment";
 
 import "@/assets/css/main.sass";
 
 Vue.config.productionTip = false;
+
+Vue.use(moment);
 
 new Vue({
 	render: h => h(App),
