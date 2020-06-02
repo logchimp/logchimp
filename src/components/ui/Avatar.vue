4fe806d0d200ef2d3ec438e18404e40a64033099
Yashu Mittal <mittalyashu77@gmail.com>
2020-06-02 08:33:56 +0530

    Create avatar component
    Use image as default option else use first name initials to create avatar.

---
+++ src/components/ui/Avatar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="avatar">
+		<div v-if="src" class="avatar__image">
+			<img :src="src" :alt="fullName" class="avatar__image-img" />
+		</div>
+		<div v-else class="avatar__initials">
+			{{ initals }}
+		</div>
+	</div>
+</template>
+
+<script>
+// icons
+export default {
+	name: "Avatar",
+	props: {
+		src: {
+			type: String,
+			default: ""
+		},
+		firstName: {
+			type: String,
+			required: true,
+			default: ""
+		},
+		lastName: {
+			type: String,
+			default: ""
+		}
+	},
+	computed: {
+		fullName() {
+			return this.firstName + " " + this.lastName;
+		},
+		initals() {
+			return this.firstName.slice(0, 1);
+		}
+	}
+};
+</script>
