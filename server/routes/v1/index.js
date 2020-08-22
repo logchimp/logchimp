95cb69394dc4274d2ffaea690e62a8a018c533f3
Yashu Mittal <mittalyashu77@gmail.com>
2020-08-22 18:33:01 +0530

    Create user API route

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -5,8 +5,10 @@ const router = express.Router();
 // requiring all routes
 const auth = require("./auth");
 const posts = require("./posts");
+const user = require("./user");
 
 router.use("/api/v1", auth);
 router.use("/api/v1", posts);
+router.use("/api/v1", user);
 
 module.exports = router;
