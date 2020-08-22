cb5eebfad311d387961bd478a5a8bef3bfca60fe
Yashu Mittal <mittalyashu77@gmail.com>
2020-08-22 11:57:16 +0530

    Button component (#60)
    * Create button component
    
    A common button component which will support all 3 different types (Text, Outline and Primary)
    
    * Update button for login & join in header
    
    * Update 'Button' component import path
    
    * Delete LPrimaryButton.vue

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="button" :class="[`button__${type}`]">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Button",
+	props: {
+		type: {
+			type: String,
+			required: true
+		}
+	}
+};
+</script>
