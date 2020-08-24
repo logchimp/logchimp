39cb3d68a667ea4984887362d22db5151f5fa32e
Yashu Mittal <mittalyashu77@gmail.com>
2020-08-24 09:23:30 +0530

    Update filterPost API route (latest and oldest)
    Reference: https://github.com/logchimp/logchimp/pull/65/commits/b7c41f4011071aa873fdd7dcb7b086404f5096e7

--- src/components/layout/OldestPosts.vue
+++ src/components/layout/OldestPosts.vue
@@ -1,11 +1,50 @@
 <template>
-	<div>
-		Oldest posts
+	<div class="latest__post">
+		<post
+			v-for="post in posts"
+			:title="post.title"
+			:slug="post.slug"
+			:description="post.contentMarkdown || ''"
+			:key="post.postId"
+		/>
 	</div>
 </template>
 
 <script>
+// packages
+import axios from "axios";
+
+// components
+import Post from "../layout/Post";
+
 export default {
-	name: "OldestPosts"
+	name: "OldestPosts",
+	data() {
+		return {
+			posts: []
+		};
+	},
+	components: {
+		Post
+	},
+	methods: {
+		getPosts() {
+			const url = new URL(
+				`${process.env.VUE_APP_SEVER_URL}/api/v1/posts?created=asc`
+			);
+
+			axios
+				.get(url)
+				.then(response => {
+					this.posts = response.data.posts;
+				})
+				.catch(error => {
+					console.log(error);
+				});
+		}
+	},
+	created() {
+		this.getPosts();
+	}
 };
 </script>
