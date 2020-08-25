3ecd5e9c05d90559883e0800c3505d53e5529b06
Yashu Mittal <mittalyashu77@gmail.com>
2020-08-25 13:27:46 +0530

    Add label to input field

--- src/components/ui/input/LText.vue
+++ src/components/ui/input/LText.vue
@@ -1,22 +1,34 @@
 <template>
-	<input
-		:type="type"
-		class="input__field input__text"
-		:class="{
-			'input__field-disabled': disabled
-		}"
-		@input="$emit('input', $event.target.value)"
-		:disabled="disabled"
-	/>
+	<div class="input">
+		<p v-if="label" class="input__field-label">{{ label }}</p>
+		<input
+			:type="type"
+			class="input__field input__text"
+			:class="{
+				'input__field-disabled': disabled
+			}"
+			@input="$emit('input', $event.target.value)"
+			:placeholder="placeholder"
+			:disabled="disabled"
+		/>
+	</div>
 </template>
 
 <script>
 export default {
 	name: "LText",
 	props: {
+		label: {
+			type: String,
+			default: ""
+		},
 		type: {
 			type: String,
 			default: "text"
+		},
+		placeholder: {
+			type: String,
+			default: ""
 		},
 		disabled: {
 			type: Boolean,
--- src/components/ui/input/LTextarea.vue
+++ src/components/ui/input/LTextarea.vue
@@ -1,23 +1,32 @@
 <template>
-	<textarea
-		:value="value"
-		class="input__field input__textarea"
-		:class="{
-			'input__field-disabled': disabled
-		}"
-		@input="$emit('input', $event.target.value)"
-	/>
+	<div>
+		<p v-if="label" class="input__field-label">{{ label }}</p>
+		<textarea
+			:value="value"
+			class="input__field input__textarea"
+			:class="{
+				'input__field-disabled': disabled
+			}"
+			@input="$emit('input', $event.target.value)"
+			:placeholder="placeholder"
+			:disabled="disabled"
+		/>
+	</div>
 </template>
 
 <script>
 export default {
 	name: "LTextarea",
 	props: {
-		type: {
+		label: {
 			type: String,
-			default: "text"
+			default: ""
 		},
 		value: {
+			type: String,
+			default: ""
+		},
+		placeholder: {
 			type: String,
 			default: ""
 		},
