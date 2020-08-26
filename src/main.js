2683171d53df637103f528654bccfdbf28fe606d
Yashu Mittal <mittalyashu77@gmail.com>
2020-08-26 10:21:47 +0530

    Import "vue-infinite-loading" as plugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
+import moment from "vue-moment";
+import infiniteLoading from "vue-infinite-loading";
+
 import App from "./App.vue";
 import router from "./routes";
-import moment from "vue-moment";
 import store from "./store";
 
 import "@/assets/css/main.sass";
@@ -9,6 +11,7 @@ import "@/assets/css/main.sass";
 Vue.config.productionTip = false;
 
 Vue.use(moment);
+Vue.use(infiniteLoading);
 
 new Vue({
 	render: h => h(App),
