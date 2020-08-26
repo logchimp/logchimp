8c2d818f0fbcfb664278b5c571ebded191c2dcc0
Yashu Mittal <mittalyashu77@gmail.com>
2020-08-26 15:09:54 +0530

    Create Vote component
    Split Vote (component) from Post component.

--- src/components/layout/Post.vue
+++ src/components/layout/Post.vue
@@ -1,12 +1,9 @@
 <template>
 	<div class="post">
-		<div class="post__voters" @click="changeVote">
-			<arrow-icon
-				class="post__voters-arrow"
-				:class="{ 'post__voters-vote': isVoted }"
-			/>
-			{{ voteCount }}
-		</div>
+		<Vote
+			:post-id="post.postId"
+			:voters="post.voters"
+		/>
 		<div class="post__content">
 			<router-link class="post__content-link" :to="`post/${post.slug}`">
 				<h5 class="post__content-title">{{ post.title }}</h5>
@@ -17,17 +14,11 @@
 </template>
 
 <script>
-// packages
-import axios from "axios";
-
-// icons
-import ArrowIcon from "../../assets/images/icons/arrow";
+// components
+import Vote from "../Vote";
 
 export default {
 	name: "post",
-	components: {
-		ArrowIcon
-	},
 	props: {
 		post: {
 			type: Object,
@@ -35,10 +26,10 @@ export default {
 			default: () => {}
 		}
 	},
+	components: {
+		Vote
+	},
 	computed: {
-		voteCount() {
-			return this.post.voters.length;
-		},
 		sliceContentMarkdown() {
 			if (this.post.contentMarkdown) {
 				return (
@@ -48,81 +39,9 @@ export default {
 			} else {
 				return "";
 			}
-		},
-		isVoted() {
-			const userId = this.$store.getters["user/getUserId"];
-			return this.post.voters.find(item => {
-				return item.userId === userId;
-			});
 		}
 	},
 	methods: {
-		changeVote() {
-			const userId = this.$store.getters["user/getUserId"];
-			const postId = this.post.postId;
-			const token = this.$store.getters["user/getAuthToken"];
-
-			if (this.isVoted) {
-				const voteId = this.post.voters.find(item => {
-					return item.userId === userId;
-				});
-
-				axios({
-					method: "delete",
-					url: `${process.env.VUE_APP_SEVER_URL}/api/v1/votes`,
-					data: {
-						voteId: voteId.voteId,
-						postId
-					},
-					headers: {
-						Authorization: `Bearer ${token}`
-					}
-				})
-					.then(response => {
-						this.post.voters = response.data.voters;
-					})
-					.catch(error => {
-						const err = { ...error };
-
-						if (err.response.data.error.code === "token_invalid") {
-							this.$store.dispatch("alerts/add", {
-								title: "Hold on! ✋",
-								description: "You need to login to submit feature request.",
-								type: "error",
-								timeout: 5000
-							});
-							this.$router.push("/login");
-						}
-					});
-			} else {
-				axios({
-					method: "post",
-					url: `${process.env.VUE_APP_SEVER_URL}/api/v1/votes`,
-					data: {
-						userId,
-						postId
-					},
-					headers: {
-						Authorization: `Bearer ${token}`
-					}
-				})
-					.then(response => {
-						this.post.voters = response.data.voters;
-					})
-					.catch(error => {
-						const err = { ...error };
-
-						if (err.response.data.error.code === "token_invalid") {
-							this.$store.dispatch("alerts/add", {
-								title: "Hold on! ✋",
-								description: "You need to login to submit feature request.",
-								type: "error",
-								timeout: 5000
-							});
-							this.$router.push("/join");
-						}
-					});
-			}
 		}
 	}
 };
