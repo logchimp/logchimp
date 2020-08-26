e2d1fab3696d06396929179504bf6853263ed111
Yashu Mittal <mittalyashu77@gmail.com>
2020-08-26 10:21:07 +0530

    Add pagination to filterPost controller

--- server/controllers/post/filterPost.js
+++ server/controllers/post/filterPost.js
@@ -1,30 +1,34 @@
 const database = require("../../database");
 
-exports.filterPost = (req, res) => {
+exports.filterPost = async (req, res) => {
 	/**
 	 * top, latest, oldest, trending
 	 */
 	const created = req.query.created;
+	const page = req.query.page - 1;
+	const limit = 10;
 
-	database
+	const posts = await database
 		.select("postId", "title", "slug", "contentMarkdown", "createdAt")
 		.from("posts")
+		.limit(limit)
+		.offset(limit * page)
 		.orderBy([
 			{
 				column: "createdAt",
 				order: created
 			}
-		])
-		.then(response => {
-			res.status(200).send({
-				status: {
-					code: 200,
-					type: "success"
-				},
-				posts: response
-			});
-		})
-		.catch(error => {
-			console.error(error);
+		]);
+
+	try {
+		res.status(200).send({
+			status: {
+				code: 200,
+				type: "success"
+			},
+			posts
 		});
+	} catch (error) {
+		console.error(error);
+	}
 };
