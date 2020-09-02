44191b59115fa53004db8277b86d20d47cfa51ae
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-02 18:04:31 +0530

    Create "/boards" API
    Create boards API with POST method for adding new board to database.

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -7,10 +7,12 @@ const auth = require("./auth");
 const posts = require("./posts");
 const votes = require("./votes");
 const users = require("./users");
+const boards = require("./boards");
 
 router.use("/api/v1", auth);
 router.use("/api/v1", posts);
 router.use("/api/v1", votes);
 router.use("/api/v1", users);
+router.use("/api/v1", boards);
 
 module.exports = router;
