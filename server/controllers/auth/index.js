be94eda1a03631e431787a307ebcffd725499109
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-02 16:50:34 +0530

    create isSetup route
    "/auth/isSetup` route validates the if there is an user with "isOwner" premission.

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,7 +1,9 @@
 const login = require("./login");
 const signup = require("./signup");
+const isSetup = require("./isSetup");
 
 module.exports = {
 	...login,
-	...signup
+	...signup,
+	...isSetup
 };
