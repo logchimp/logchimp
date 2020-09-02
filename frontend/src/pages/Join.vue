0caa9728fc1ba1d2e2e22f65f7e7a9c6787210d8
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-02 18:07:55 +0530

    Redirect to homepage or redirect query path
    Update navigation route to login and join page by adding support for redirect query.

--- frontend/src/pages/Join.vue
+++ frontend/src/pages/Join.vue
@@ -114,7 +114,11 @@ export default {
 								createdAt: response.data.user.createdAt,
 								updatedAt: response.data.user.updatedAt
 							});
-							this.$router.push("/");
+							if (this.$route.query.redirect) {
+								this.$router.push(this.$route.query.redirect);
+							} else {
+								this.$router.push("/");
+							}
 						}
 					})
 					.catch(error => {
