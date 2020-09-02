90b5142ebca201fa9896f7d769596ab68a1409c2
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-02 17:05:36 +0530

    Move button styles inside "Button" component

--- frontend/src/components/ui/Button.vue
+++ frontend/src/components/ui/Button.vue
@@ -15,3 +15,29 @@ export default {
 	}
 };
 </script>
+
+<style lang="sass">
+.button
+	position: relative
+	padding: .875rem 1.5rem
+	border-radius: $border-radius
+	font-weight: 500
+	display: flex
+	justify-content: center
+	cursor: pointer
+	user-select: none
+
+.button__primary
+	background-color: $brand-color
+	color: $white
+
+.button__outline
+	border: 1px solid rgba($white, .5)
+
+	&:hover
+		border-color: $white
+
+.button__primary-disabled
+	opacity: .7
+	cursor: default
+</style>
