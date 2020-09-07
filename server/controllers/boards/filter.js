64c8aa1445ab93097efbbeda14ae653173b8498a
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-07 09:27:06 +0530

    Create GET filter boards API

---
+++ server/controllers/boards/filter.js
@@ -0,0 +1,33 @@
+const database = require("../../database");
+
+exports.filter = async (req, res) => {
+	const created = req.query.created;
+	const page = req.query.page - 1;
+	const limit = 10;
+
+	const response = await database
+		.select()
+		.from("boards")
+		.limit(limit)
+		.offset(limit * page)
+		.orderBy([
+			{
+				column: "createdAt",
+				order: created
+			}
+		]);
+
+	try {
+		const boards = response;
+
+		res.status(200).send({
+			status: {
+				code: 200,
+				type: "success"
+			},
+			boards
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
