baeda56d7296440dff1b7a0552c964a6113cb180
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-12 14:50:49 +0530

    Revert #86 PR
    Reverting changes back due to pass userId to query secretKey for validating authToken everytime.
    
    For now, using `SECRET_KEY` as environment variable.
    
    Reopen #78

--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -27,7 +27,7 @@ exports.login = (req, res) => {
 					.then(validatePassword => {
 						if (validatePassword) {
 							// generate authToken
-							const authToken = jwt.sign(user, user.secretKey, {
+							const authToken = jwt.sign(user, process.env.SECRET_KEY, {
 								expiresIn: "2d"
 							});
 
