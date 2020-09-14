ee906233cec59cfa1c60f66fd352a9d8a84590c4
Yashu Mittal <mittalyashu77@gmail.com>
2020-09-14 16:27:16 +0530

    Create password.unit.test.js

---
+++ server/utils/password.unit.test.js
@@ -0,0 +1,33 @@
+// modules
+const generatePassword = require("omgopass");
+
+// utils
+const { hashPassword, validatePassword } = require("./password");
+
+test("Hash empty string as password", () => {
+	const hash = hashPassword("");
+
+	expect(hash).toBeUndefined();
+});
+
+test("Validate hash random password", async () => {
+	const password = generatePassword();
+	const hash = hashPassword(password);
+	const validPassword = await validatePassword(password, hash);
+
+	expect(validPassword).toBeTruthy();
+});
+
+test("Validate hash random password with missing hash", async () => {
+	const password = generatePassword();
+	const validPassword = await validatePassword(password);
+
+	expect(validPassword).toBeUndefined();
+});
+
+test("Validate hash random password with missing password", async () => {
+	const hash = hashPassword("");
+	const validPassword = await validatePassword(hash);
+
+	expect(validPassword).toBeUndefined();
+});
