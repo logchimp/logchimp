3c8459c683a27efa869347f5fed4c5f2a74290aa
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-04 16:14:40 +0530

    Add link to settings page

--- frontend/src/components/dashboard/Sidebar.vue
+++ frontend/src/components/dashboard/Sidebar.vue
@@ -8,33 +8,34 @@
 				<div class="dashboard-sidebar-navbar-icon">
 					<dashboard-icon />
 				</div>
-				<div class="dashboard-sidebar-navbar-content">
-					Dashbaord
-				</div>
+				<div class="dashboard-sidebar-navbar-content">Dashbaord</div>
 			</router-link>
 			<router-link to="/dashboard/boards" class="dashboard-sidebar-navbar-item">
 				<div class="dashboard-sidebar-navbar-icon">
 					<board-icon />
 				</div>
-				<div class="dashboard-sidebar-navbar-content">
-					Boards
-				</div>
+				<div class="dashboard-sidebar-navbar-content">Boards</div>
 			</router-link>
 			<router-link to="/dashboard/posts" class="dashboard-sidebar-navbar-item">
 				<div class="dashboard-sidebar-navbar-icon">
 					<post-icon />
 				</div>
-				<div class="dashboard-sidebar-navbar-content">
-					Posts
-				</div>
+				<div class="dashboard-sidebar-navbar-content">Posts</div>
 			</router-link>
 			<router-link to="/dashboard/users" class="dashboard-sidebar-navbar-item">
 				<div class="dashboard-sidebar-navbar-icon">
 					<users-icon />
 				</div>
-				<div class="dashboard-sidebar-navbar-content">
-					Users
+				<div class="dashboard-sidebar-navbar-content">Users</div>
+			</router-link>
+			<router-link
+				to="/dashboard/settings"
+				class="dashboard-sidebar-navbar-item"
+			>
+				<div class="dashboard-sidebar-navbar-icon">
+					<settings-icon />
 				</div>
+				<div class="dashboard-sidebar-navbar-content">Settings</div>
 			</router-link>
 		</nav>
 		<footer
@@ -104,6 +105,7 @@ import DashboardIcon from "../../components/icons/Dashboard";
 import BoardIcon from "../../components/icons/Board";
 import PostIcon from "../../components/icons/Post";
 import UsersIcon from "../../components/icons/Users";
+import SettingsIcon from "../../components/icons/Settings";
 import InfoIcon from "../../components/icons/Info";
 import TwitterIcon from "../../components/icons/Twitter";
 import LogoutIcon from "../../components/icons/Logout";
@@ -127,6 +129,7 @@ export default {
 		BoardIcon,
 		PostIcon,
 		UsersIcon,
+		SettingsIcon,
 		InfoIcon,
 		TwitterIcon,
 		LogoutIcon
