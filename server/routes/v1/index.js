a34700e6875d32c12cc1956828dadf3cd639f9be
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-04 20:10:33 +0530

    Serve static "content/images" from disk

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,5 +1,7 @@
 // modules
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
 // requiring all routes
@@ -16,6 +18,22 @@ router.use("/api/v1", votes);
 router.use("/api/v1", users);
 router.use("/api/v1", boards);
 router.use("/api/v1", settings);
+
+// content
+router.use("/content/images", (req, res) => {
+	fs.readFile(
+		path.join(__dirname, "../../../content/images") + req.url,
+		(err, data) => {
+			if (err) {
+				res.sendStatus(404);
+			}
+			if (data) {
+				res.writeHead(200);
+				res.end(data);
+			}
+		}
+	);
+});
 
 router.get("/api", (req, res) => {
 	res.send("👍");
