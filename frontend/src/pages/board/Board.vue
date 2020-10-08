89157bfa1a8772c5502a2f1e69f203562acafdea
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-08 15:51:45 +0530

    Add loader to board and show message on board not found

--- frontend/src/pages/board/Board.vue
+++ frontend/src/pages/board/Board.vue
@@ -1,36 +1,46 @@
 <template>
-	<div class="homepage">
-		<main class="homepage-posts">
-			<tab>
-				<tab-item
-					@click="updateTab('latest')"
-					:class="{
-						'tab-item-active': tab === 'latest'
-					}"
-				>
-					<template v-slot:icon>
-						<sort-desc-icon />
-					</template>
-					Latest
-				</tab-item>
-				<tab-item
-					@click="updateTab('oldest')"
-					:class="{
-						'tab-item-active': tab === 'oldest'
-					}"
-				>
-					<template v-slot:icon>
-						<sort-asc-icon />
-					</template>
-					Oldest
-				</tab-item>
-			</tab>
+	<div v-if="!board.loading">
+		<div v-if="isBoardExist">
+			<div class="homepage">
+				<main class="homepage-posts">
+					<tab>
+						<tab-item
+							@click="updateTab('latest')"
+							:class="{
+								'tab-item-active': tab === 'latest'
+							}"
+						>
+							<template v-slot:icon>
+								<sort-desc-icon />
+							</template>
+							Latest
+						</tab-item>
+						<tab-item
+							@click="updateTab('oldest')"
+							:class="{
+								'tab-item-active': tab === 'oldest'
+							}"
+						>
+							<template v-slot:icon>
+								<sort-asc-icon />
+							</template>
+							Oldest
+						</tab-item>
+					</tab>
 
-			<component :is="activeTab"></component>
-		</main>
-		<aside class="homepage-sidebar">
-			<create-post :board-id="board.boardId" />
-		</aside>
+					<component :is="activeTab"></component>
+				</main>
+				<aside class="homepage-sidebar">
+					<create-post :board-id="board.boardId" />
+				</aside>
+			</div>
+		</div>
+		<p v-else>
+			There is no such board.
+		</p>
+	</div>
+	<div v-else class="loader-container">
+		<loader />
 	</div>
 </template>
 
@@ -40,6 +50,7 @@ import axios from "axios";
 import InfiniteLoading from "vue-infinite-loading";
 
 // components
+import Loader from "../../components/Loader";
 import Tab from "../../components/tab/Tab";
 import TabItem from "../../components/tab/TabItem";
 import LatestPosts from "../../components/post/LatestPosts";
@@ -55,7 +66,10 @@ export default {
 	data() {
 		return {
 			tab: "latest",
-			board: {}
+			board: {
+				loading: false
+			},
+			isBoardExist: true
 		};
 	},
 	components: {
@@ -63,6 +77,7 @@ export default {
 		InfiniteLoading,
 
 		// components
+		Loader,
 		Tab,
 		TabItem,
 		LatestPosts,
@@ -89,6 +104,7 @@ export default {
 			this.tab = tabValue;
 		},
 		getBoard() {
+			this.board.loading = true;
 			const slug = this.$route.params.slug;
 
 			axios({
@@ -97,9 +113,15 @@ export default {
 			})
 				.then(response => {
 					this.board = response.data.board;
+
+					this.board.loading = false;
 				})
 				.catch(error => {
-					console.error(error);
+					if (error.response.data.code === "BOARD_NOT_FOUND") {
+						this.isBoardExist = false;
+					}
+
+					this.board.loading = false;
 				});
 		}
 	},
