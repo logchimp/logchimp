0635874679a354282a097e9eca40a7d1e738e04c
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-08 16:35:41 +0530

    Pass data directly instead of spreading object in user vuex store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -66,23 +66,9 @@ export default {
 			this.getSiteSettings();
 		}
 
-		const user = JSON.parse(localStorage.getItem("user"));
+		const user = localStorage.getItem("user");
 		if (user) {
-			this.$store.dispatch("user/login", {
-				authToken: user.authToken,
-				userId: user.userId,
-				firstname: user.firstname,
-				lastname: user.lastname,
-				emailAddress: user.emailAddress,
-				username: user.username,
-				avatar: user.avatar,
-				isVerified: user.isVerified,
-				isBlocked: user.isBlocked,
-				isModerator: user.isModerator,
-				isOwner: user.isOwner,
-				createdAt: user.createdAt,
-				updatedAt: user.updatedAt
-			});
+			this.$store.dispatch("user/login", JSON.parse(user));
 		}
 	}
 };
