7a2e91ca6971b5b84e11d138aa3cb1eccb23074a
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-08 11:55:28 +0530

    add "express-rate-limit" 15 minutes with each IP to 100 requests per windowMs

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,8 +1,7 @@
 // modules
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const router = express.Router();
+const rateLimit = require("express-rate-limit");
 
 // requiring all routes
 const auth = require("./auth");
@@ -12,6 +11,8 @@ const users = require("./users");
 const boards = require("./boards");
 const settings = require("./settings");
 
+const serveImages = require("../../controllers/serveImages");
+
 router.use("/api/v1", auth);
 router.use("/api/v1", posts);
 router.use("/api/v1", votes);
@@ -19,21 +20,16 @@ router.use("/api/v1", users);
 router.use("/api/v1", boards);
 router.use("/api/v1", settings);
 
-// content
-router.use("/content/images", (req, res) => {
-	fs.readFile(
-		path.join(__dirname, "../../../content/images") + req.url,
-		(err, data) => {
-			if (err) {
-				res.sendStatus(404);
-			}
-			if (data) {
-				res.writeHead(200);
-				res.end(data);
-			}
-		}
-	);
+// server images
+/**
+ * limit rate limit for 15 minutes
+ * each IP to 100 requests per windowMs
+ */
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100
 });
+router.use("/content/images", limiter, serveImages);
 
 router.get("/api", (req, res) => {
 	res.send("👍");
