e0e8050bb18ab8cdd94fff9667c0de41424d997d
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-10 16:30:05 +0530

    Log server startup message with logger

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 require("dotenv").config({ path: "../.env" });
 
+const startTime = Date.now();
+
 // modules
 const express = require("express");
 const cors = require("cors");
@@ -11,6 +13,9 @@ const app = express();
 
 const database = require("./database");
 
+// utils
+const logger = require("./utils/logger");
+
 database
 	.raw("select 1+1 as result")
 	.then(() => {
@@ -20,7 +25,7 @@ database
 				database.seed
 					.run()
 					.then(() => {
-						console.log("Database setup complete");
+						logger.info("Database migration complete");
 
 						// start express server at SERVER_PORT
 						app.listen(process.env.SERVER_PORT, () => {
@@ -33,17 +38,39 @@ database
 							// importing all routes modules
 							app.use(routes);
 
-							console.log(`Listening at port: ${process.env.SERVER_PORT}`);
+							logger.info(`LogChimp is running in ${process.env.NODE_ENV}...`);
+							logger.info(`Listening on port: ${process.env.SERVER_PORT}`);
+							logger.info("Ctrl+C to shut down");
+							logger.info(`LogChimp boot ${(Date.now() - startTime) / 1000}s`);
 						});
 					})
-					.catch(error => {
-						console.error(error);
+					.catch(err => {
+						logger.error({
+							code: "DATABASE_SEEDS",
+							message: "Seeding failed",
+							err
+						});
 					});
 			})
-			.catch(error => {
-				console.error(error);
+			.catch(err => {
+				logger.error({
+					code: "DATABASE_MIGRATIONS",
+					message: "Migrations failed",
+					err
+				});
 			});
 	})
-	.catch(error => {
-		console.error(error);
+	.catch(err => {
+		if (err.message === "SSL/TLS required") {
+			logger.error({
+				code: "DATABASE_CONNECTION",
+				message: "Enable SSL/TLS on your database connection"
+			});
+		}
+
+		logger.error({
+			code: "DATABASE_CONNECTION",
+			message: `Connecting to database at ${err.address}:${err.port}`,
+			err
+		});
 	});
