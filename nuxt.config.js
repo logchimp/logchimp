0a9d02395e56a60af40757db7f347acbcd3bb94f
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-11 10:02:52 +0530

    Add sass styles and sass variable support
    Install sass-loader, node-sass and @nuxtjs/style-resources packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,11 @@ export default {
 		host: "0.0.0.0",
 		timing: true
 	},
+	css: ["@/assets/styles/main.sass"],
+	buildModules: ["@nuxtjs/style-resources"],
+	styleResources: {
+		sass: "@/assets/styles/variables.sass"
+	},
 	modules: [
 		// https://axios.nuxtjs.org
 		"@nuxtjs/axios"
