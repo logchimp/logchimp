38fed2a8c6c2900f7480a803ae98abd932a8d289
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-11 17:15:33 +0530

    Board view page

--- frontend/pages/board/_url.vue
+++ frontend/pages/board/_url.vue
@@ -39,27 +39,29 @@
 			There is no such board.
 		</p>
 	</div>
-	<div v-else class="loader-container">
-		<loader />
+	<div v-else class="loader-container loader">
+		<loader-icon />
 	</div>
 </template>
 
 <script>
 // packages
-import axios from "axios";
-import InfiniteLoading from "vue-infinite-loading";
+import { SortDesc, SortAsc } from "lucide";
 
 // components
-import Loader from "../../components/Loader";
-import Tab from "../../components/tab/Tab";
-import TabItem from "../../components/tab/TabItem";
-import LatestPosts from "../../components/post/LatestPosts";
-import OldestPosts from "../../components/post/OldestPosts";
-import CreatePost from "../../components/post/CreatePost";
+import Tab from "@/components/tab/Tab";
+import TabItem from "@/components/tab/TabItem";
+import LatestPosts from "@/components/post/LatestPosts";
+import OldestPosts from "@/components/post/OldestPosts";
+import CreatePost from "@/components/post/CreatePost";
+
+// mixins
+import lucideIcon from "@/mixins/lucideIcon.js";
 
 // icons
-import SortDescIcon from "../../components/icons/SortDesc";
-import SortAscIcon from "../../components/icons/SortAsc";
+const SortDescIcon = lucideIcon("SortDesc", SortDesc);
+const SortAscIcon = lucideIcon("SortAsc", SortAsc);
+import LoaderIcon from "@/components/icons/Loader";
 
 export default {
 	name: "Board",
@@ -73,11 +75,7 @@ export default {
 		};
 	},
 	components: {
-		// packages
-		InfiniteLoading,
-
 		// components
-		Loader,
 		Tab,
 		TabItem,
 		LatestPosts,
@@ -86,7 +84,8 @@ export default {
 
 		// icons
 		SortDescIcon,
-		SortAscIcon
+		SortAscIcon,
+		LoaderIcon
 	},
 	computed: {
 		activeTab() {
@@ -97,50 +96,46 @@ export default {
 				default:
 					return LatestPosts;
 			}
-		},
-		getSiteSittings() {
-			return this.$store.getters["settings/get"];
 		}
+		// getSiteSittings() {
+		// 	return this.$store.getters["settings/get"];
+		// }
 	},
 	methods: {
 		updateTab(tabValue) {
 			this.tab = tabValue;
 		},
-		getBoard() {
+		async getBoard() {
 			this.board.loading = true;
-			const slug = this.$route.params.slug;
+			const url = this.$route.params.url;
 
-			axios({
-				method: "post",
-				url: `${process.env.VUE_APP_SEVER_URL}/api/v1/boards/${slug}`
-			})
-				.then(response => {
-					this.board = response.data.board;
+			try {
+				const response = await this.$axios.$post(`/api/v1/boards/${url}`);
 
-					this.board.loading = false;
-				})
-				.catch(error => {
-					if (error.response.data.code === "BOARD_NOT_FOUND") {
-						this.isBoardExist = false;
-					}
+				this.board = response.board;
+				this.board.loading = false;
+			} catch (error) {
+				if (error.response.data.code === "BOARD_NOT_FOUND") {
+					this.isBoardExist = false;
+				}
 
-					this.board.loading = false;
-				});
+				this.board.loading = false;
+			}
 		}
 	},
 	created() {
 		this.getBoard();
-	},
-	metaInfo() {
-		return {
-			title: `${this.board.name} · Board`,
-			meta: [
-				{
-					name: "og:title",
-					content: `${this.board.name} · Board · ${this.getSiteSittings.title}`
-				}
-			]
-		};
 	}
+	// metaInfo() {
+	// 	return {
+	// 		title: `${this.board.name} · Board`,
+	// 		meta: [
+	// 			{
+	// 				name: "og:title",
+	// 				content: `${this.board.name} · Board · ${this.getSiteSittings.title}`
+	// 			}
+	// 		]
+	// 	};
+	// }
 };
 </script>
