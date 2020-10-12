0da4ddfddec11e53c4d6ccf8f411d628732327ac
marko <markomiljkovic2@gmail.com>
2020-10-12 23:59:27 +0200

    Resolve "no-mutate-props"

--- frontend/src/components/post/Post.vue
+++ frontend/src/components/post/Post.vue
@@ -1,36 +1,26 @@
 <template>
-	<div class="post">
-		<Vote
-			:post-id="post.postId"
-			:voters="post.voters"
-			@update-voters="updateVoters"
-		/>
-		<div class="post-content">
-			<router-link
-				class="post-content-link"
-				:to="`${dashboardUrl}/post/${post.slug}`"
-			>
-				<h5 class="post-content-title">{{ post.title }}</h5>
-			</router-link>
-			<p class="post-content-description" v-html="sliceContentMarkdown" />
-			<router-link
-				:to="`${dashboardUrl}/board/${post.board.url}`"
-				v-if="post.board"
-			>
-				<div class="post-board">
-					<div
-						class="board-color"
-						:style="{
-							backgroundColor: `#${post.board.color}`
+  <div class="post">
+    <Vote :post-id="postData.postId" :voters="postData.voters" @update-voters="updateVoters" />
+    <div class="post-content">
+      <router-link class="post-content-link" :to="`${dashboardUrl}/post/${postData.slug}`">
+        <h5 class="post-content-title">{{ postData.title }}</h5>
+      </router-link>
+      <p class="post-content-description" v-html="sliceContentMarkdown" />
+      <router-link :to="`${dashboardUrl}/board/${postData.board.url}`" v-if="postData.board">
+        <div class="post-board">
+          <div
+            class="board-color"
+            :style="{
+							backgroundColor: `#${postData.board.color}`
 						}"
-					/>
-					<div class="post-board-name">
-						<p>{{ post.board.name }}</p>
-					</div>
-				</div>
-			</router-link>
-		</div>
-	</div>
+          />
+          <div class="post-board-name">
+            <p>{{ postData.board.name }}</p>
+          </div>
+        </div>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -38,41 +28,53 @@
 import Vote from "./Vote";
 
 export default {
-	name: "post",
-	props: {
-		post: {
-			type: Object,
-			required: true,
-			default: () => {}
-		},
-		dashboard: {
-			type: Boolean,
-			default: false
-		}
-	},
-	components: {
-		Vote
-	},
-	computed: {
-		sliceContentMarkdown() {
-			if (this.post.contentMarkdown) {
-				return (
-					this.post.contentMarkdown.slice(0, 120) +
-					(this.post.contentMarkdown.length > 120 ? "..." : "")
-				);
-			} else {
-				return "";
-			}
-		},
-		dashboardUrl() {
-			return this.dashboard ? "/dashboard" : "";
-		}
-	},
-	methods: {
-		updateVoters(voters) {
-			// eslint-disable-next-line vue/no-mutating-props
-			this.post.voters = voters;
-		}
-	}
+  name: "post",
+  props: {
+    post: {
+      type: Object,
+      required: true,
+      default: () => {}
+    },
+    dashboard: {
+      type: Boolean,
+      default: false
+    }
+  },
+  components: {
+    Vote
+  },
+  data: () => {
+    return {
+      postData: this.post
+    };
+  },
+  watch: {
+    post: {
+      handler(newValue, oldValue) {
+        this.postData = newValue;
+      }
+    }
+  },
+  computed: {
+    sliceContentMarkdown() {
+      if (this.postData.contentMarkdown) {
+        return (
+          this.postData.contentMarkdown.slice(0, 120) +
+          (this.postData.contentMarkdown.length > 120 ? "..." : "")
+        );
+      } else {
+        return "";
+      }
+    },
+    dashboardUrl() {
+      return this.dashboard ? "/dashboard" : "";
+    }
+  },
+  methods: {
+    updateVoters(voters) {
+      // other solution is to emit data to parent component
+      this.postData.voters = voters;
+    }
+  }
 };
 </script>
