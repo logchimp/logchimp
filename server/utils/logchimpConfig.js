9a98f8551ba9b485b88eed1d6c696bfe0166f35b
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-12 10:59:37 +0530

    Read logchimp config file from filesystem

---
+++ server/utils/logchimpConfig.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const fs = require("fs-extra");
+
+const config = () => {
+	try {
+		const result = fs.readJsonSync(
+			path.resolve(__dirname, "../../logchimp.config.json")
+		);
+
+		return result;
+	} catch (error) {
+		return false;
+	}
+};
+
+module.exports = config;
