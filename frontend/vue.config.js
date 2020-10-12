ce405b0675987e20b27e3becf9a78b3b7578db47
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-12 06:08:36 +0530

    Add devServer proxy at `http://localhost:${serverPort}`
    Default serverPort set at 3000 and use "VUE_APP_SERVER_PORT" environment variable.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
+const serverPort = process.env.VUE_APP_SERVER_PORT || 3000;
+
 module.exports = {
+	devServer: {
+		proxy: {
+			'/api': {
+				target: `http://localhost:${serverPort}`
+			}
+		}
+	},
 	css: {
 		loaderOptions: {
 			sass: {
