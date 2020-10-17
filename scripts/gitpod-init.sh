da7ac5894a620c75788ccf32a383d6f9ef76372b
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-17 15:57:43 +0530

    Create logchimp config in gitpod-init

--- scripts/gitpod-init.sh
+++ scripts/gitpod-init.sh
@@ -5,3 +5,12 @@ psql -c "CREATE DATABASE logchimp"
 
 # Install packages
 yarn install
+
+# Create logchimp.config.json file
+PG_DATABASE="logchimp" \
+PG_HOST="localhost" \
+PG_USER="gitpod" \
+PG_PASSWORD="" \
+PG_PORT=5432 \
+SECRET_KEY="" \
+sh ./scripts/create-config.sh
