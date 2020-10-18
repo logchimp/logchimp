4700c1252484dae308ae7b1f5bf1544203915b88
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-18 09:03:30 +0530

    disableHostCheck for IS_GITPOD environment variable

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,6 +5,7 @@ const serverPort = process.env.VUE_APP_SERVER_PORT || 3000;
 module.exports = {
 	outputDir: path.resolve(__dirname, "../server/public"),
 	devServer: {
+		disableHostCheck: process.env.IS_GITPOD ? true : false,
 		proxy: {
 			'/api': {
 				target: `http://localhost:${serverPort}`
