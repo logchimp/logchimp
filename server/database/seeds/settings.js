4318674222681560345a49c3834737eca6222762
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-21 21:20:26 +0530

    update settings seed logo url to cdn.logchimp

--- server/database/seeds/settings.js
+++ server/database/seeds/settings.js
@@ -15,9 +15,9 @@ exports.seed = function(knex) {
 							description: "Track user feedback to build better products",
 							accentColor: "484d7c",
 							logo:
-								"https://logchimp.codecarrot.net/images/logchimp-logo_circle.png",
+								"https://cdn.logchimp.codecarrot.net/logchimp_circular_logo.png",
 							icon:
-								"https://logchimp.codecarrot.net/images/logchimp-logo_circle.png",
+								"https://cdn.logchimp.codecarrot.net/logchimp_circular_logo.png",
 							isPoweredBy: true
 						}
 					])
