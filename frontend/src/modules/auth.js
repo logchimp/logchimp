d38af9a17f2b10e0831ff8ab34cd7954bdaa9684
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-24 18:25:32 +0530

    Create signIn module

--- frontend/src/modules/auth.js
+++ frontend/src/modules/auth.js
@@ -5,6 +5,23 @@ import axios from "axios";
 import store from "../store";
 
 /**
+ * Sign in to user account
+ *
+ * @param {emailAddress} string user email address
+ * @param {password} string user password
+ */
+export const signin = async (emailAddress, password) => {
+	return await axios({
+		method: "post",
+		url: "/api/v1/auth/login",
+		data: {
+			emailAddress,
+			password
+		}
+	});
+};
+
+/**
  * Create user account
  *
  * @param {emailAddress} string user email address
