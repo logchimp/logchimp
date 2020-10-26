891ff917df0d990e301941c98899d159c0258648
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-26 13:48:13 +0530

    Store email verification token in database

--- server/services/auth/verifyEmail.js
+++ server/services/auth/verifyEmail.js
@@ -1,34 +1,54 @@
+// database
+const database = require("../../database");
+
 // services
 const mail = require("../../services/mail");
 
 // utils
 const { createToken } = require("../../utils/token");
 const logchimpConfig = require("../../utils/logchimpConfig");
+const logger = require("../../utils/logger");
 const config = logchimpConfig();
 
 const verifyEmail = async (siteUrl, emailAddress) => {
 	const secretKey = config.server.secretKey;
-	const authToken = createToken({ emailAddress }, secretKey, {
+	const token = createToken({ emailAddress }, secretKey, {
 		expiresIn: "2h"
 	});
 
-	const onboardingMailContent = await mail.generateContent("verify", {
-		siteUrl,
-		verificationLink: `${siteUrl}/verify/email/${authToken}`
-	});
+	try {
 
-	const email = new mail.Mail();
-	const noReplyEmail = `noreply@${siteUrl}`;
+		const userEmailVerificationToken = await database
+			.insert({
+				emailAddress,
+				token,
+				createdAt: new Date().toJSON()
+			})
+			.into("emailVerification")
+			.returning("*");
 
-	await email.send({
-		from: noReplyEmail,
-		to: emailAddress,
-		subject: "LogChimp - Please confirm your email",
-		text: onboardingMailContent.text,
-		html: onboardingMailContent.html
-	});
+		const onboardingMailContent = await mail.generateContent("verify", {
+			siteUrl,
+			verificationLink: `${siteUrl}/verify/email/${token}`
+		});
 
-	return authToken;
+		const email = new mail.Mail();
+		const noReplyEmail = `noreply@${siteUrl}`;
+
+		await email.send({
+			from: noReplyEmail,
+			to: emailAddress,
+			subject: "LogChimp - Please confirm your email",
+			text: onboardingMailContent.text,
+			html: onboardingMailContent.html
+		});
+
+		console.log(userEmailVerificationToken);
+
+		return userEmailVerificationToken[0];
+	} catch (err) {
+		logger.error(err.message);
+	}
 };
 
 module.exports = verifyEmail;
