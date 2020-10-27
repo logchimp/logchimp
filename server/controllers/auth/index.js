57592282cc93e743320390b4141061be2aa13f83
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-27 09:41:38 +0530

    Email verification APIs (#180)
    * Install nodemailer, loadash, html-to-text package
    
    * Create Mail class for sending email
    
    * generateContent function to inject dynamic data
    
    generateContent function injects data inside html template files.
    
    * Verify email html templates
    
    * Send email verification in signup
    
    * Create send verifyEmail auth servicesSplit sending email verification into separate service.
    
    * Return authToken from verifyEmail services
    
    * Send email verification (route & controller)
    
    * Create emailVerification table migration
    
    * Store email verification token in database
    
    * Delete older email verification token before creating new one
    
    * Validate email verification token (route & controller)
    
    * Update verify email design
    
    * Remove console statement in verifyEmail mail service

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,9 +1,11 @@
 const login = require("./login");
 const signup = require("./signup");
 const isSetup = require("./isSetup");
+const email = require("./email");
 
 module.exports = {
 	...login,
 	...signup,
-	...isSetup
+	...isSetup,
+	email
 };
