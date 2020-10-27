6795fb4e1ba20c1de59106f354e7ac69af583e01
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-27 16:48:39 +0530

    Check board exists before continue boardPost request
    Send board data along with board posts in response.

--- server/controllers/boards/boardPosts.js
+++ server/controllers/boards/boardPosts.js
@@ -22,67 +22,54 @@ exports.boardPosts = async (req, res) => {
 	try {
 		const board = await getBoardBySlug(slug);
 
-		if (board) {
-			try {
-				const posts = await database
-					.select("postId", "title", "slug", "contentMarkdown", "createdAt")
-					.from("posts")
-					.where({
-						boardId: board.boardId
-					})
-					.limit(limit)
-					.offset(limit * page)
-					.orderBy([
-						{
-							column: "createdAt",
-							order: created
-						}
-					]);
-
-				const postVoters = [];
-
-				for (let i = 0; i < posts.length; i++) {
-					const postId = posts[i].postId;
-
-					try {
-						const voters = await getVotes(postId);
-
-						postVoters.push({
-							...posts[i],
-							voters
-						});
-					} catch (err) {
-						logger.log({
-							level: "error",
-							message: err
-						});
-					}
-				}
-
-				res.status(200).send({
-					status: {
-						code: 200,
-						type: "success"
-					},
-					posts: postVoters
-				});
-			} catch (err) {
-				logger.log({
-					level: "error",
-					message: err
-				});
-			}
-		} else {
-
+		if (!board) {
 			res.status(404).send({
-				code: "BOARD_NOT_FOUND",
-				message: error.api.boards.boardNotFound
+				message: error.api.boards.boardNotFound,
+				code: "BOARD_NOT_FOUND"
 			});
 		}
+
+		try {
+			const posts = await database
+				.select("postId", "title", "slug", "contentMarkdown", "createdAt")
+				.from("posts")
+				.where({
+					boardId: board.boardId
+				})
+				.limit(limit)
+				.offset(limit * page)
+				.orderBy([
+					{
+						column: "createdAt",
+						order: created
+					}
+				]);
+
+			const postVoters = [];
+
+			for (let i = 0; i < posts.length; i++) {
+				const postId = posts[i].postId;
+
+				try {
+					const voters = await getVotes(postId);
+
+					postVoters.push({
+						...posts[i],
+						voters
+					});
+				} catch (err) {
+					logger.error(err);
+				}
+			}
+
+			res.status(200).send({
+				board,
+				posts: postVoters
+			});
+		} catch (err) {
+			logger.error(err);
+		}
 	} catch (err) {
-		logger.log({
-			level: "error",
-			message: err
-		});
+		logger.error(err);
 	}
 };
