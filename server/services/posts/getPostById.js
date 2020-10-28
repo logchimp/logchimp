ca82ee68559d36928258607aec0af5d3f9e7b3e4
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-28 11:42:17 +0530

    Create getPostById service

---
+++ server/services/posts/getPostById.js
@@ -0,0 +1,37 @@
+const database = require("../../database");
+
+// utils
+const logger = require("../../utils/logger");
+const error = require("../../errorResponse.json");
+
+/**
+ * Get post by ID
+ *
+ * @param {string} postId Post UUID
+ */
+const getPostById = async postId => {
+	if (!postId) {
+		return {
+			message: error.api.posts.postIdMissing,
+			code: "MISSING_POST_ID"
+		};
+	}
+
+	try {
+		const posts = await database
+			.select()
+			.from("posts")
+			.where({
+				postId
+			})
+			.limit(1);
+
+		const post = posts[0];
+
+		return post;
+	} catch (err) {
+		logger.error(err);
+	}
+};
+
+module.exports = getPostById;
