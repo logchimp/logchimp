ee8b68b677cd5f61eb6eb18b3d474c2d12fb5067
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-30 09:20:34 +0530

    Set title and postId as required and getPost before updating

--- server/controllers/post/updatePost.js
+++ server/controllers/post/updatePost.js
@@ -1,22 +1,51 @@
 const database = require("../../database");
 
+// services
+const getPostById = require("../../services/posts/getPostById");
+
 // utils
 const logger = require("../../utils/logger");
+const error = require("../../errorResponse.json");
 
 exports.updatePost = async (req, res) => {
 	const postId = req.params.postId;
 	const title = req.body.title;
 	const contentMarkdown = req.body.contentMarkdown;
-	const slugId = req.body.slugId;
 
-	const slug = `${title
-		.replace(/[^\w\s]/gi, "")
-		.replace(/\s\s+/gi, " ")
-		.toLowerCase()
-		.split(" ")
-		.join("-")}-${slugId}`;
+	if (!title) {
+		res.status(400).send({
+			message: error.api.posts.titleMissing,
+			code: "MISSING_POST_TITLE"
+		});
+		return;
+	}
+
+	if (!postId) {
+		res.status(400).send({
+			message: error.api.posts.postIdMissing,
+			code: "MISSING_POST_ID"
+		});
+		return;
+	}
 
 	try {
+		const getPost = await getPostById(postId);
+
+		if (!getPost) {
+			res.status(404).send({
+				message: error.api.posts.postNotFound,
+				code: "POST_NOT_FOUND"
+			});
+			return;
+		}
+
+		const slug = `${title
+			.replace(/[^\w\s]/gi, "")
+			.replace(/\s\s+/gi, " ")
+			.toLowerCase()
+			.split(" ")
+			.join("-")}-${getPost.slugId}`;
+
 		const posts = await database
 			.update({
 				title,
@@ -32,13 +61,7 @@ exports.updatePost = async (req, res) => {
 
 		const post = posts[0];
 
-		res.status(200).send({
-			status: {
-				code: 200,
-				type: "success"
-			},
-			post
-		});
+		res.status(200).send(post);
 	} catch (err) {
 		logger.log({
 			level: "error",
