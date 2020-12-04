9e047e21f945fdb1bcde1d196b3b99b5cf9d2a19
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-04 11:51:36 +0530

    Remove boardPosts controller require
    Reference: 1524bb1a66d9a0fd5265b24f2bc3c82aec207eb9

--- server/controllers/boards/index.js
+++ server/controllers/boards/index.js
@@ -1,13 +1,11 @@
 const create = require("./create");
 const filter = require("./filter");
 const boardBySlug = require("./boardBySlug");
-const boardPosts = require("./boardPosts");
 const deleteBoard = require("./deleteBoard");
 
 module.exports = {
 	...create,
 	...filter,
 	...boardBySlug,
-	...boardPosts,
 	...deleteBoard
 };
