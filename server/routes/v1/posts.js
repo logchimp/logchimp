ab1a368ecf89edebde868fc8c4ee676e08aecb1a
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-04 13:28:06 +0530

    Add apiAuth middleware to create post route

--- server/routes/v1/posts.js
+++ server/routes/v1/posts.js
@@ -7,12 +7,14 @@ const post = require("../../controllers/post");
 
 // middleware
 const token = require("../../middlewares/token");
+const middleware = require("../../middlewares");
 const validateUserAccess = require("../../middlewares/validateUserAccess");
 
-router.post("/posts", token, post.create);
 router.get("/posts", post.filterPost);
 router.delete("/post/delete", token, post.deleteById);
 router.get("/posts/:slug", post.postBySlug);
+
+router.post("/posts", middleware.apiAuth, post.create);
 router.patch("/posts/:postId", token, validateUserAccess, post.updatePost);
 
 module.exports = router;
