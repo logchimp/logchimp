2e044c2673628bf43872bb632f518a03c2c32b1f
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-06 11:26:37 +0530

    Add apiAuth & postExists middleware to update post
    Remove validateUserAccess middleware

--- server/routes/v1/posts.js
+++ server/routes/v1/posts.js
@@ -6,16 +6,14 @@ const router = express.Router();
 const post = require("../../controllers/post");
 
 // middleware
-const token = require("../../middlewares/token");
 const middleware = require("../../middlewares");
-const validateUserAccess = require("../../middlewares/validateUserAccess");
 const exists = require("../../middlewares/posts/exists");
 
 router.post("/posts/get", post.filterPost);
 router.get("/posts/:slug", post.postBySlug);
 
 router.post("/posts", middleware.apiAuth, post.create);
-router.patch("/posts/:postId", token, validateUserAccess, post.updatePost);
+router.patch("/posts", middleware.apiAuth, exists, post.updatePost);
 
 router.delete("/posts", middleware.apiAuth, exists, post.deleteById);
 
