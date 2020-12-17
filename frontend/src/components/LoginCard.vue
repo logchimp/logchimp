60e866983cd8694ceb4072c168929e0376ccfbb4
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-17 17:06:32 +0530

    Create LoginCard component

---
+++ frontend/src/components/LoginCard.vue
@@ -0,0 +1,39 @@
+<template>
+	<Form>
+		<div class="site-setup-card-header">
+			<h4 class="site-setup-card-header-heading">Login</h4>
+			<p class="site-setup-card-header-label">
+				Log in to your account to give feedback
+			</p>
+		</div>
+		<div class="site-setup-card-button">
+			<Button type="primary" @click="loginRedirect">
+				Login
+			</Button>
+		</div>
+	</Form>
+</template>
+
+<script>
+// components
+import Form from "./Form";
+import Button from "./Button";
+
+export default {
+	name: "LoginCard",
+	components: {
+		Form,
+		Button
+	},
+	methods: {
+		loginRedirect() {
+			this.$router.push({
+				path: "/login",
+				query: {
+					redirect: this.$route.fullPath
+				}
+			});
+		}
+	}
+};
+</script>
