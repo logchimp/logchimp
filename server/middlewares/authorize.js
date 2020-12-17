0df2021ad2c448d0a14423d68dd277f614c52395
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-17 17:32:05 +0530

    Remove user ban check in authorize middleware
    Allow user with ban role to continue with new check

--- server/middlewares/authorize.js
+++ server/middlewares/authorize.js
@@ -9,14 +9,6 @@ const authorize = (req, res, next) => {
 		if (hasRoles) {
 			const roles = req.user.roles[0];
 
-			// user is ban
-			if (roles.name === "ban") {
-				return res.status(403).send({
-					message: error.middleware.user.userBan,
-					code: "USER_BAN"
-				});
-			}
-
 			// user is spam
 			if (roles.name === "spam") {
 				return res.status(403).send({
