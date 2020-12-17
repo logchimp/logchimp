725e14e8012c1a159082fde44248584bc7273ea2
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-17 17:31:10 +0530

    Remove login controller from if else conditional blocks

--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -14,49 +14,46 @@ exports.login = async (req, res) => {
 	const password = req.body.password;
 
 	try {
-		const users = await database
+		const user = await database
 			.select("userId", "name", "email", "password", "avatar")
 			.from("users")
 			.where({
 				email
 			})
-			.limit(1);
+			.first();
 
-		const user = users[0];
-
-		if (user) {
-			const validateUserPassword = await validatePassword(
-				password,
-				user.password
-			);
-
-			if (validateUserPassword) {
-				delete user.password;
-
-				// generate authToken
-				const secretKey = config.server.secretKey;
-				const authToken = createToken(user, secretKey, {
-					expiresIn: "2d"
-				});
-
-				res.status(200).send({
-					user: {
-						...user,
-						authToken
-					}
-				});
-			} else {
-				res.status(403).send({
-					message: error.middleware.user.incorrectPassword,
-					code: "INCORRECT_PASSWORD"
-				});
-			}
-		} else {
-			res.status(404).send({
+		if (!user) {
+			return res.status(404).send({
 				message: error.middleware.user.userNotFound,
 				code: "USER_NOT_FOUND"
 			});
 		}
+
+		const validateUserPassword = await validatePassword(
+			password,
+			user.password
+		);
+		if (!validateUserPassword) {
+			return res.status(403).send({
+				message: error.middleware.user.incorrectPassword,
+				code: "INCORRECT_PASSWORD"
+			});
+		}
+
+		delete user.password;
+
+		// generate authToken
+		const secretKey = config.server.secretKey;
+		const authToken = createToken(user, secretKey, {
+			expiresIn: "2d"
+		});
+
+		res.status(200).send({
+			user: {
+				...user,
+				authToken
+			}
+		});
 	} catch (err) {
 		logger.log({
 			level: "error",
