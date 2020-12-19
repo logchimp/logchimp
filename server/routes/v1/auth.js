90d8d5c9d3e7efb67959f1cfeae7bc56c9552e9a
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-19 10:43:19 +0530

    Split token validation & user exists into separate middleware
    Splitting the token validation process and user exists into different middleware to verify the user email address.

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -7,6 +7,7 @@ const auth = require("../../controllers/auth");
 
 // middleware
 const exists = require("../../middlewares/userExists");
+const validateEmailToken = require("../../middlewares/validateEmailToken");
 
 router.post("/auth/signup", auth.signup);
 router.post("/auth/login", auth.login);
@@ -15,8 +16,13 @@ router.post("/auth/setup", auth.setup);
 router.get("/auth/setup", auth.isSiteSetup);
 
 // email
-router.post("/auth/email/validate", auth.email.validate);
 router.post("/auth/email/verify", exists, auth.email.verify);
+router.post(
+	"/auth/email/validate",
+	validateEmailToken,
+	exists,
+	auth.email.validate
+);
 
 // password
 router.post("/auth/password/reset", auth.password.reset);
