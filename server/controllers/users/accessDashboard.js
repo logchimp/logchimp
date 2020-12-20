3f3d932740f361d0f6ae5e8ade58bd536539f29b
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-20 09:44:11 +0530

    Check "dashboard:read" permission for authenticate user for accessing dashboard

--- server/controllers/users/accessDashboard.js
+++ server/controllers/users/accessDashboard.js
@@ -1,54 +1,20 @@
-// database
-const database = require("../../database");
+// utils
+const error = require("../../errorResponse.json");
 
 exports.accessDashboard = (req, res) => {
-	const userId = req.params.userId;
+	const permissions = req.user.permissions;
 
-	database
-		.select("userId", "isOwner", "isModerator", "isBlocked")
-		.from("users")
-		.where({
-			userId
-		})
-		.limit(1)
-		.then(response => {
-			const user = response[0];
-
-			if (!user.isBlocked) {
-				if (user.isOwner || user.isModerator) {
-					res.status(200).send({
-						status: {
-							code: 200,
-							type: "success"
-						},
-						access: true
-					});
-				} else {
-					res.status(403).send({
-						status: {
-							code: 403,
-							type: "error"
-						},
-						error: {
-							code: "user_unauthorise",
-							message: "Not authorise to access dashboard"
-						}
-					});
-				}
-			} else {
-				res.status(403).send({
-					status: {
-						code: 403,
-						type: "error"
-					},
-					error: {
-						code: "user_blocked",
-						message: "User account is blocked"
-					}
-				});
-			}
-		})
-		.catch(error => {
-			console.error(error);
+	const accessDashboardPermission = permissions.find(
+		item => item === "dashboard:read"
+	);
+	if (!accessDashboardPermission) {
+		return res.status(403).send({
+			message: error.api.posts.notEnoughPermission,
+			code: "NOT_ENOUGH_PERMISSION"
 		});
+	}
+
+	res.status(200).send({
+		access: true
+	});
 };
