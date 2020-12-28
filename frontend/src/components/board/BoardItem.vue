131a13627f49cd8efc61156125dc8a69e2e79ed2
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-28 20:33:15 +0530

    create BoardItem component
    Create separate component BoardItem from Boards

---
+++ frontend/src/components/board/BoardItem.vue
@@ -0,0 +1,44 @@
+<template>
+	<router-link data-test="board-item" :to="`/board/${url}`" class="board-item">
+		<div
+			class="color-dot"
+			data-test="board-item-color"
+			:style="{
+				backgroundColor: `#${color}`
+			}"
+		/>
+		<div class="board-item-name-and-posts">
+			<div data-test="board-item-name" class="board-item-name">
+				{{ name }}
+			</div>
+			<div data-test="board-item-postcount" class="board-item-postcount">
+				{{ postCount }}
+			</div>
+		</div>
+	</router-link>
+</template>
+
+<script>
+export default {
+	name: "BoardItem",
+	props: {
+		name: {
+			type: String,
+			required: true
+		},
+		color: {
+			type: String,
+			validator: value => {
+				return value.length === 6;
+			}
+		},
+		url: {
+			type: String
+		},
+		postCount: {
+			type: Number,
+			default: 0
+		}
+	}
+};
+</script>
