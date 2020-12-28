592ae13434763fb0ac6a8e74b7f944655ec609f1
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-28 21:42:54 +0530

    create board item unit test

---
+++ frontend/tests/unit/boardItem.spec.js
@@ -0,0 +1,30 @@
+import BoardItem from "../../src/components/board/BoardItem.vue";
+import { mount } from "@vue/test-utils";
+
+describe("board item", () => {
+	const wrapper = mount(BoardItem, {
+		propsData: {
+			name: "Feature requests",
+			color: "abcabc",
+			url: "feature-requests",
+			postCount: 30
+		},
+		stubs: ["router-link"]
+	});
+
+	it("color is 'rgb(171, 202, 188)'", () => {
+		expect(
+			wrapper.find("[data-test=board-item-color]").attributes("style")
+		).toBe("background-color: rgb(171, 202, 188);");
+	});
+
+	it("name is 'Feature requests'", () => {
+		expect(wrapper.find("[data-test=board-item-name]").text()).toBe(
+			"Feature requests"
+		);
+	});
+
+	it("post count is '30'", () => {
+		expect(wrapper.find("[data-test=board-item-postcount]").text()).toBe("30");
+	});
+});
