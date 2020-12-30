29c4fa1f5568d72c121733b2599d4b48c63e28c1
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-30 17:24:03 +0530

    Create PostCard component for roadmap view

---
+++ frontend/src/components/post/PostCard.vue
@@ -0,0 +1,112 @@
+<template>
+	<div class="post-card">
+		<div class="post-card-main">
+			<Vote
+				:post-id="post.postId"
+				:votes-count="post.voters.votesCount"
+				:is-voted="post.voters.viewerVote"
+				@update-voters="updateVoters"
+			/>
+			<div style="width: 100%">
+				<div class="post-card-section">
+					<div>
+						<router-link :to="`/post/${post.slug}`">
+							<h5>{{ post.title }}</h5>
+						</router-link>
+						<span v-if="!isExpanded" class="post-card-board">{{
+							post.board.name
+						}}</span>
+						<time
+							v-else
+							:datetime="post.createdAt"
+							:title="post.createdAt | moment('dddd, DD MMMM YYYY hh:mm')"
+							class="post-date"
+						>
+							{{ post.createdAt | moment("from") }}
+						</time>
+					</div>
+					<div
+						@click="isExpanded = !isExpanded"
+						:style="{
+							transform: isExpanded ? 'rotateX(180deg)' : ''
+						}"
+						class="post-card-toggle"
+					>
+						<arrow-top />
+					</div>
+				</div>
+				<p v-if="isExpanded" class="post-card-description">
+					{{ sliceContentMarkdown }}
+				</p>
+			</div>
+		</div>
+		<div v-if="isExpanded" class="post-card-extra">
+			<div class="post-card-voters">
+				<img
+					v-for="voters in post.voters.votes"
+					:key="voters.userId"
+					:src="voters.avatar"
+					:alt="voters.username"
+				/>
+				<div v-if="moreVotersCount" class="more-voters">
+					{{ "+" + moreVotersCount }}
+				</div>
+			</div>
+			<board-badge
+				:show-board="true"
+				:name="post.board.name"
+				:color="post.board.color"
+				:url="post.board.url"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+// components
+import Vote from "./Vote";
+import BoardBadge from "../board/BoardBadge";
+
+// icons
+import ArrowTop from "../icons/ArrowTop";
+
+export default {
+	name: "PostCard",
+	data() {
+		return {
+			postData: this.post,
+			isExpanded: false
+		};
+	},
+	props: {
+		post: {
+			type: Object,
+			required: true
+		}
+	},
+	components: {
+		Vote,
+		BoardBadge,
+		ArrowTop
+	},
+	computed: {
+		sliceContentMarkdown() {
+			return (
+				this.post.contentMarkdown.slice(0, 120) +
+				(this.post.contentMarkdown.length > 120 ? "..." : "")
+			);
+		},
+		moreVotersCount() {
+			return this.post.voters.votesCount > 9
+				? this.post.voters.votesCount - 6
+				: 0;
+		}
+	},
+	methods: {
+		updateVoters(voters) {
+			this.postData.voters.votesCount = voters.votesCount;
+			this.postData.voters.viewerVote = voters.viewerVote;
+		}
+	}
+};
+</script>
