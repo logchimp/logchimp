af171ec1b1e94de9b65e632a7d79315ba2d30b64
Yashu Mittal <mittalyashu77@gmail.com>
2020-12-30 16:48:50 +0530

    tests: post component

---
+++ frontend/tests/unit/post.spec.js
@@ -0,0 +1,38 @@
+import Post from "../../src/components/post/Post";
+import { shallowMount, RouterLinkStub } from "@vue/test-utils";
+
+describe("post", () => {
+	const wrapper = shallowMount(Post, {
+		propsData: {
+			post: {
+				// random UUID
+				postId: "69136892-b8c8-41c7-9e8f-a2eb212e5311",
+				title: "Post title",
+				contentMarkdown: "What's this feature is all about?",
+				voters: {
+					isVoted: true,
+					voteCount: 120
+				},
+				board: {
+					name: "Feature requests",
+					color: "abcabc",
+					url: "feature-requests",
+					showBoard: true
+				}
+			}
+		},
+		stubs: {
+			RouterLink: RouterLinkStub
+		}
+	});
+
+	it("title", () => {
+		expect(wrapper.find("[data-test=post-title]").text()).toBe("Post title");
+	});
+
+	it("description", () => {
+		expect(wrapper.find("[data-test=post-description]").text()).toBe(
+			"What's this feature is all about?"
+		);
+	});
+});
