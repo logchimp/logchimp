140927770b32b6530676afcee7a87ebfe0ee85bc
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-03 10:03:48 +0530

    Add mailConfigExists middleware
    APIs route using mail service to send email, add mailConfigExists middleware to check for mail configuration.

---
+++ server/middlewares/mailConfigExists.js
@@ -0,0 +1,13 @@
+// services
+const { mail } = require("../services/mail");
+
+module.exports = async (req, res, next) => {
+	if (!mail) {
+		return res.status(501).send({
+			message: "Mail configuration missing",
+			code: "MAIL_CONFIG_MISSING"
+		});
+	}
+
+	next();
+};
