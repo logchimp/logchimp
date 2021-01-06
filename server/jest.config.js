847ccfcc147eee7e11dea4ed96351b12969b1c5a
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-06 14:51:11 +0530

    Remove testTimeout property from server/jest configuration

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,6 +3,5 @@ const jestConfig = require("../jest.config.js");
 module.exports = {
 	...jestConfig,
 	displayName: "server",
-	testTimeout: 20000,
 	setupFilesAfterEnv: ["./tests/setupTest.js"]
 };
