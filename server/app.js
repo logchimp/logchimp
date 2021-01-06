47c7f64dc9a3147c48e9640dbc4f7c8f95ac19ef
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-06 14:43:23 +0530

    Split express app instance & listen on port

--- server/app.js
+++ server/app.js
@@ -1,16 +1,12 @@
-const startTime = Date.now();
-
-// modules
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require("path");
 
 const routes = require("./routes");
+const database = require("./database");
 
 const app = express();
-
-const database = require("./database");
+app.set("x-powered-by", false);
 
 // utils
 const logger = require("./utils/logger");
@@ -27,46 +23,19 @@ if (!config) {
 // Set the default environment to be `development`
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-database
-	.raw("select 1+1 as result")
+// contains key-value pairs of data submitted in the request body
+app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// import all routes
+app.use(routes);
+
+database.migrate
+	.latest()
 	.then(() => {
-		database.migrate
-			.latest()
-			.then(() => {
-				logger.info("Database migration complete");
-
-				// contains key-value pairs of data submitted in the request body
-				app.use(bodyParser.json());
-
-				// enable all CORS requests
-				app.use(cors());
-
-				// importing all routes modules
-				app.use(routes);
-
-				// Serve vue app
-				if (process.env.NODE_ENV === "production") {
-					app.use(express.static(path.resolve(__dirname, "public")));
-					app.get(/.*/, (req, res) =>
-						res.sendFile(path.resolve(__dirname, "public/index.html"))
-					);
-				}
-
-				// start express server at SERVER_PORT
-				const port = config.server.port || 3000;
-				app.listen(port, () => {
-					logger.info(`LogChimp is running in ${process.env.NODE_ENV}...`);
-					logger.info(`Listening on port: ${port}`);
-					logger.info("Ctrl+C to shut down");
-					logger.info(`LogChimp boot ${(Date.now() - startTime) / 1000}s`);
-				});
-			})
-			.catch(err => {
-				logger.error({
-					code: "DATABASE_MIGRATIONS",
-					message: err
-				});
-			});
+		logger.info("Database migration complete");
 	})
 	.catch(err => {
 		if (err.message === "SSL/TLS required") {
@@ -74,11 +43,18 @@ database
 				code: "DATABASE_CONNECTION",
 				message: "Enable SSL/TLS on your database connection"
 			});
+
+			logger.error({
+				code: "DATABASE_CONNECTION",
+				message: `Connecting to database at ${err.address}:${err.port}`,
+				err
+			});
 		}
 
 		logger.error({
-			code: "DATABASE_CONNECTION",
-			message: `Connecting to database at ${err.address}:${err.port}`,
-			err
+			code: "DATABASE_MIGRATIONS",
+			message: err
 		});
 	});
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const startTime = Date.now();
+
+const path = require("path");
+const express = require("express");
+
+const app = require("./app");
+
+// utils
+const logger = require("./utils/logger");
+const logchimpConfig = require("./utils/logchimpConfig");
+const config = logchimpConfig();
+
+// Serve vue app
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.resolve(__dirname, "public")));
+	app.get(/.*/, (req, res) =>
+		res.sendFile(path.resolve(__dirname, "public/index.html"))
+	);
+}
+
+// start express server at SERVER_PORT
+const port = config.server.port || 3000;
+app.listen(port, () => {
+	logger.info(`LogChimp is running in ${process.env.NODE_ENV}...`);
+	logger.info(`Listening on port: ${port}`);
+	logger.info("Ctrl+C to shut down");
+	logger.info(`LogChimp boot ${(Date.now() - startTime) / 1000}s`);
+});
