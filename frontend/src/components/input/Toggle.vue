cd7fc22d2f0d85a65b082302f4be03a6fcbd2be5
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-07 17:03:54 +0530

    Create toggle component

---
+++ frontend/src/components/input/Toggle.vue
@@ -0,0 +1,39 @@
+<template>
+	<div
+		class="toggle"
+		:style="{
+			backgroundColor: checked ? `var(--brand-color)` : `#999`,
+			justifyContent: checked ? `flex-end` : ''
+		}"
+	>
+		<div class="toggle-slider" />
+		<input
+			type="checkbox"
+			:checked="checked"
+			:disabled="disabled"
+			@input="click"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Toggle",
+	props: {
+		checked: {
+			type: Boolean,
+			default: false
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		}
+	},
+	methods: {
+		click(value) {
+			if (this.disabled) return;
+			this.$emit("input", value.target.checked);
+		}
+	}
+};
+</script>
