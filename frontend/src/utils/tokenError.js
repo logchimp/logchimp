a1a185b24a5163f82995bccda6a873f421d1fad9
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-08 16:18:41 +0530

    Handle "INVALID_JWT" error to tokenError utility function

--- frontend/src/utils/tokenError.js
+++ frontend/src/utils/tokenError.js
@@ -2,20 +2,19 @@ import store from "../store";
 import router from "../routes";
 
 const tokenError = error => {
+	store.dispatch("user/logout");
+
 	if (error.response.data.code === "USER_NOT_FOUND") {
-		store.dispatch("user/logout");
 		if (router.currentRoute.fullPath !== "/") router.push("/");
 	}
 
-	// invalid token
-	if (error.response.data.code === "INVALID_TOKEN") {
-		store.dispatch("user/logout");
+	// invalid token or invalid JWT
+	if (["INVALID_TOKEN", "INVALID_JWT"].includes(error.response.data.code)) {
 		router.push("/login");
 	}
 
 	// invalid auth header format
 	if (error.response.data.code === "INVALID_AUTH_HEADER_FORMAT") {
-		store.dispatch("user/logout");
 		router.push({
 			path: "/login",
 			query: {
