f7602967a7dd4652c167c0f26b89694885e61544
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-09 19:48:45 +0530

    tests: Add user state in local vuex store & create updateUser, updatePermission mutation
    * Move wrapper variable outside describe function
    * Re-order tests in order to match with local vuex state
    
    Reference: f5b4fd755d55ba8a321cc55ab3cd073bc6b114a3

--- frontend/tests/unit/vote.spec.js
+++ frontend/tests/unit/vote.spec.js
@@ -7,61 +7,45 @@ localVue.use(Vuex);
 
 const store = new Vuex.Store({
 	state: {
+		user: {
+			// random UUID
+			userId: "d33e4e05-9c8b-4255-9838-b59085d081a0"
+		},
 		permissions: ["vote:create", "vote:destroy"]
 	},
 	getters: {
+		"user/getUserId": state => state.user.userId,
 		"user/getPermissions": state => state.permissions
+	},
+	mutations: {
+		updateUser(state, payload) {
+			state.user = payload;
+		},
+		updatePermissions(state, payload) {
+			state.permissions = payload;
+		}
+	}
+});
+
+const wrapper = mount(Vote, {
+	store,
+	localVue,
+	propsData: {
+		// random UUID
+		postId: "69136892-b8c8-41c7-9e8f-a2eb212e5311"
 	}
 });
 
 describe("vote", () => {
-	it("Disable vote without 'vote:create' permission", () => {
-		const store = new Vuex.Store({
-			state: {
-				permissions: []
-			},
-			getters: {
-				"user/getPermissions": state => state.permissions
-			}
-		});
-		const wrapper = mount(Vote, {
-			store,
-			localVue,
-			propsData: {
-				// random UUID
-				postId: "69136892-b8c8-41c7-9e8f-a2eb212e5311"
-			}
-		});
-
-		expect(
-			wrapper.find("[data-test=vote]").classes("post-voters-disabled")
-		).toBe(true);
-	});
-
 	it("Enable vote with 'vote:create' permission", () => {
-		const wrapper = mount(Vote, {
-			store,
-			localVue,
-			propsData: {
-				// random UUID
-				postId: "69136892-b8c8-41c7-9e8f-a2eb212e5311"
-			}
-		});
-
 		expect(
 			wrapper.find("[data-test=vote]").classes("post-voters-disabled")
 		).toBe(false);
 	});
 
-	it("Vote is voted", () => {
-		const wrapper = mount(Vote, {
-			store,
-			localVue,
-			propsData: {
-				// random UUID
-				postId: "69136892-b8c8-41c7-9e8f-a2eb212e5311",
-				isVoted: true
-			}
+	it("Vote is voted", async () => {
+		await wrapper.setProps({
+			isVoted: true
 		});
 
 		expect(
@@ -69,15 +53,9 @@ describe("vote", () => {
 		).toBe(true);
 	});
 
-	it("Vote is not voted", () => {
-		const wrapper = mount(Vote, {
-			store,
-			localVue,
-			propsData: {
-				// random UUID
-				postId: "69136892-b8c8-41c7-9e8f-a2eb212e5311",
-				isVoted: false
-			}
+	it("Vote is not voted", async () => {
+		await wrapper.setProps({
+			isVoted: false
 		});
 
 		expect(
@@ -85,17 +63,27 @@ describe("vote", () => {
 		).toBe(false);
 	});
 
-	it("Vote count is '120' votes", () => {
-		const wrapper = mount(Vote, {
-			store,
-			localVue,
-			propsData: {
-				// random UUID
-				postId: "69136892-b8c8-41c7-9e8f-a2eb212e5311",
-				votesCount: 120
-			}
+	it("Vote count is '120' votes", async () => {
+		await wrapper.setProps({
+			votesCount: 120
 		});
 
 		expect(wrapper.find("[data-test=vote-count]").text()).toBe("120");
+	});
+
+	it("disable without 'vote:create' permission", async () => {
+		await store.commit("updatePermissions", []);
+
+		expect(
+			wrapper.find("[data-test=vote]").classes("post-voters-disabled")
+		).toBe(true);
+	});
+
+	it("enabled with user logged out", async () => {
+		await store.commit("updateUser", {});
+
+		expect(
+			wrapper.find("[data-test=vote]").classes("post-voters-disabled")
+		).toBe(false);
 	});
 });
