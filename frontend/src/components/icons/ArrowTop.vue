b892c455dd5592ad19846141758efc2e9c516c2b
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-14 11:07:18 +0530

    Minify SVG icons (#284)
    * Minify svg icons, remove extra svg html attributes
    
    * Set base svg styles
    
    * stroke-width: 2
    * stroke-linecap: round
    * stroke-linejoin: round

--- frontend/src/components/icons/ArrowTop.vue
+++ frontend/src/components/icons/ArrowTop.vue
@@ -1,14 +1,3 @@
 <template>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	>
-		<polyline points="18 15 12 9 6 15"></polyline>
-	</svg>
+	<svg width="24" height="24" viewBox="0 0 24 24"><polyline points="18 15 12 9 6 15"></polyline></svg>
 </template>
