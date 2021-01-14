3550f32686efe4f8a9103cb140adf92f86941449
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-14 11:07:18 +0530

    Minify SVG icons (#284)
    * Minify svg icons, remove extra svg html attributes
    
    * Set base svg styles
    
    * stroke-width: 2
    * stroke-linecap: round
    * stroke-linejoin: round

--- frontend/src/components/icons/Success.vue
+++ frontend/src/components/icons/Success.vue
@@ -1,31 +1,3 @@
 <template>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		:fill="fill"
-		:stroke="stroke"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
-	>
-		<path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-		<polyline points="22 4 12 14.01 9 11.01" />
-	</svg>
+	<svg width="24" height="24" viewBox="0 0 24 24"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" /><polyline points="22 4 12 14.01 9 11.01" /></svg>
 </template>
-
-<script>
-export default {
-	props: {
-		fill: {
-			type: String,
-			default: "none"
-		},
-		stroke: {
-			type: String,
-			default: ""
-		}
-	}
-};
-</script>
