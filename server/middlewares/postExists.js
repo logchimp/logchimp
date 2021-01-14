d3cd6eaf5263b7ede5f1513a8dd43fdc6299a3b5
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-14 14:16:06 +0530

    Rename post/exists to postExists middleware

--- server/middlewares/postExists.js
+++ server/middlewares/postExists.js
@@ -1,7 +1,7 @@
-const database = require("../../database");
+const database = require("../database");
 
 // utils
-const error = require("../../errorResponse.json");
+const error = require("../errorResponse.json");
 
 module.exports = async (req, res, next) => {
 	const id = req.body.id;
