76112bf04f5d892dbb40fb5334ce97657f7dce29
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-14 10:34:05 +0530

    Minify svg icons, remove extra svg html attributes

--- frontend/src/components/icons/Arrow.vue
+++ frontend/src/components/icons/Arrow.vue
@@ -1,7 +1,3 @@
 <template>
-	<svg viewBox="0 0 12 8" xmlns="http://www.w3.org/2000/svg">
-		<path
-			d="M5.2 1.067a1 1 0 011.6 0l4 5.333A1 1 0 0110 8H2a1 1 0 01-.8-1.6l4-5.333z"
-		/>
-	</svg>
+	<svg width="24" height="24" viewBox="0 0 12 8"><path d="M5.2 1.067a1 1 0 011.6 0l4 5.333A1 1 0 0110 8H2a1 1 0 01-.8-1.6l4-5.333z" /></svg>
 </template>
