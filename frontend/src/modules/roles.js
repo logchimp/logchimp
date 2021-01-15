bd6e6e0fab691efa38a225e335769ad29a11b0b2
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-15 20:56:38 +0530

    Create getAllRoles module function

--- frontend/src/modules/roles.js
+++ frontend/src/modules/roles.js
@@ -5,6 +5,23 @@ import axios from "axios";
 import store from "../store";
 
 /**
+ *	Get all roles
+ *
+ * @returns {object} response
+ */
+export const getAllRoles = async () => {
+	const token = store.getters["user/getAuthToken"];
+
+	return await axios({
+		method: "GET",
+		url: "/api/v1/roles",
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	});
+};
+
+/**
  *	Create role
  *
  * @returns {object} response
