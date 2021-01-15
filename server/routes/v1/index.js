30f36c02cc5a1f89e8520ea5191a5a6ca497bfb2
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-15 11:43:12 +0530

    Add create role API "/api/v1/roles"

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -10,7 +10,18 @@ const users = require("./users");
 const boards = require("./boards");
 const settings = require("./settings");
 const roadmaps = require("./roadmaps");
+const roles = require("./roles");
 
-router.use("/api/v1", auth, posts, votes, users, boards, settings, roadmaps);
+router.use(
+	"/api/v1",
+	auth,
+	posts,
+	votes,
+	users,
+	boards,
+	settings,
+	roadmaps,
+	roles
+);
 
 module.exports = router;
