d08714dab35e8f89eb2d5bf9391d6dc25b5840ba
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-16 11:26:33 +0530

    Get all roles (API + client) (#290)
    * Add get roles
    
    * Create getAllRoles module function
    
    * show all roles in settings/roles page

--- server/routes/v1/roles.js
+++ server/routes/v1/roles.js
@@ -8,6 +8,8 @@ const roles = require("../../controllers/roles");
 // middleware
 const middleware = require("../../middlewares");
 
+router.get("/roles", middleware.apiAuth, roles.get);
+
 router.post("/roles", middleware.apiAuth, roles.create);
 
 module.exports = router;
