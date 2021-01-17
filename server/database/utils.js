a83ba03506de11d7582a146f9709d9ebeba73c5a
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-17 23:02:33 +0530

    replace addPermissionToRole & addPermissionWithRoles with addPermission & removePermssion functions

--- server/database/utils.js
+++ server/database/utils.js
@@ -3,124 +3,57 @@ const { v4: uuidv4 } = require("uuid");
 // utils
 const logger = require("../utils/logger");
 
-const addPermission = async (database, permission) => {
-	const existingPermission = await database
+const permissionExists = async (database, permission) => {
+	return await database
 		.select()
-		.from("permissions")
-		.where(permission)
-		.first();
-
-	if (existingPermission) {
-		logger.warn({
-			message: `Permission for ${permission.type}:${permission.action} already added`
-		});
-		return;
-	}
-
-	logger.info(`Adding permission for ${permission.type}:${permission.action}`);
-
-	await database
-		.insert({
-			id: uuidv4(),
-			type: permission.type,
-			action: permission.action
-		})
-		.into("permissions");
-};
-
-const removePermission = async (database, permission) => {
-	const existingPermission = await database
-		.select()
-		.from("permissions")
-		.where(permission)
-		.first();
-
-	if (!existingPermission) {
-		logger.warn({
-			message: `Permission for ${permission.type}:${permission.action} already removed`
-		});
-		return;
-	}
-
-	logger.info(
-		`Removing permission for ${permission.type}:${permission.action}`
-	);
-
-	await database
-		.delete()
 		.from("permissions")
 		.where({
-			type: permission.type,
-			action: permission.action
-		});
+			...permission
+		})
+		.first();
 };
 
-const addPermissionToRole = async (database, permission, roles) => {
-	const getPermission = await database
-		.select()
-		.from("permissions")
-		.where(permission)
-		.first();
+exports.addPermission = (database, permissions) => {
+	return permissions.forEach(async permission => {
+		const type = permission.split(":")[0];
+		const action = permission.split(":")[1];
 
-	if (!getPermission) {
-		logger.error({
-			message: `Permission (${permission.type}:${permission.action}) does not exist`
+		const exists = await permissionExists(database, {
+			type,
+			action
 		});
-		return;
-	}
 
-	roles.forEach(async role => {
-		const getRole = await database
-			.select()
-			.from("roles")
-			.where({
-				name: role
-			})
-			.first();
-
-		if (!getRole) {
-			logger.error({
-				message: `Role (${getRole}) does not exists`
+		if (exists) {
+			return logger.warn({
+				message: `Permssion ${type}:${action} already added`
 			});
 		}
-
-		const existingRelation = await database
-			.select()
-			.from("permissions_roles")
-			.where({
-				permission_id: getPermission.id,
-				role_id: getRole.id
-			})
-			.first();
-
-		if (existingRelation) {
-			logger.warn({
-				message: `Adding permission(${permission.type}:${permission.action}) to role(${getRole.name}) - already exists`
-			});
-			return;
-		}
-
-		logger.warn({
-			message: `Adding permission(${permission.type}:${permission.action}) to role(${getRole.name})`
-		});
 
 		await database
 			.insert({
 				id: uuidv4(),
-				permission_id: getPermission.id,
-				role_id: getRole.id
+				type,
+				action
 			})
-			.into("permissions_roles");
+			.into("permissions");
+
+		logger.info(`Adding permssion ${type}:${action}`);
 	});
 };
 
-exports.addPermissionWithRoles = async (database, permission, roles) => {
-	return (
-		await addPermission(database, permission),
-		await addPermissionToRole(database, permission, roles)
-	);
-};
+exports.removePermission = async (database, permissions) => {
+	return await permissions.forEach(async permission => {
+		const type = permission.split(":")[0];
+		const action = permission.split(":")[1];
 
-exports.removePermissionWithRoles = (database, permission) => {
-	return removePermission(database, permission);
+		await database
+			.delete()
+			.from("permissions")
+			.where({
+				type,
+				action
+			});
+
+		logger.info(`Removing permssion ${type}:${action}`);
+	});
 };
