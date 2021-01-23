f077ee6f4c90a0da13a4c0281a1e0674d8016003
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-23 10:54:54 +0530

    Add createRoadmap module function

--- frontend/src/modules/roadmaps.js
+++ frontend/src/modules/roadmaps.js
@@ -1,6 +1,9 @@
 // packges
 import axios from "axios";
 
+// store
+import store from "../store";
+
 /**
  *	Get all roadmaps
  *
@@ -10,5 +13,22 @@ export const getAllRoadmaps = async () => {
 	return await axios({
 		method: "GET",
 		url: "/api/v1/roadmaps"
+	});
+};
+
+/**
+ * Create new roadmap
+ *
+ * @returns {object} response
+ */
+export const createRoadmap = async () => {
+	const token = store.getters["user/getAuthToken"];
+
+	return await axios({
+		method: "POST",
+		url: "/api/v1/roadmaps",
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
 	});
 };
