af494c69e33b58b48ac095ba138cdab3bca655ae
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-26 22:49:22 +0530

    Create DropdownWrapper component

---
+++ frontend/src/components/dropdown/DropdownWrapper.vue
@@ -0,0 +1,40 @@
+<template>
+	<div
+		v-on-clickaway="away"
+		class="dropdown-wrapper"
+		@click="click"
+	>
+		<slot name="toggle" />
+		<slot :active="active" />
+	</div>
+</template>
+
+<script>
+// packages
+import { mixin as clickaway } from "vue-clickaway";
+
+export default {
+	name: "DropdownWrapper",
+	mixins: [clickaway],
+	props: {
+		disabled: {
+			type: Boolean,
+			default: false
+		}
+	},
+	data() {
+		return {
+			active: false
+		};
+	},
+	methods: {
+		click() {
+			if (this.disabled) return;
+			this.active = !this.active;
+		},
+		away() {
+			this.active = false;
+		}
+	}
+};
+</script>
