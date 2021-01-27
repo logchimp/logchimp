2b6f1fbbf9bb5e2cbddcc2bb524dab616b3d6482
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-27 13:00:09 +0530

    Create BoardSuggestion component

---
+++ frontend/src/components/board/BoardSuggestion.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="board-suggestion" @click="$emit('click')">
+		<div
+			class="color-dot"
+			:style="{
+				backgroundColor: `#${board.color}`
+			}"
+		/>
+		<div class="board-suggestion-content">
+			<h5>{{ board.name }}</h5>
+			<span>{{ board.url }}</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "BoardSuggestion",
+	props: {
+		board: {
+			type: Object,
+			default: () => {}
+		}
+	}
+};
+</script>
