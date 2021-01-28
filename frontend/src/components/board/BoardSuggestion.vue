ed286e038b4e8aaa01669500e008255071493ceb
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-28 11:35:12 +0530

    write tests for BoardSuggestion component

--- frontend/src/components/board/BoardSuggestion.vue
+++ frontend/src/components/board/BoardSuggestion.vue
@@ -2,13 +2,16 @@
 	<div class="board-suggestion" @click="$emit('click')">
 		<div
 			class="color-dot"
+			data-test="board-suggestion-color"
 			:style="{
 				backgroundColor: `#${board.color}`
 			}"
 		/>
 		<div class="board-suggestion-content">
-			<h5>{{ board.name }}</h5>
-			<span>{{ board.url }}</span>
+			<h5 data-test="board-suggestion-name">
+				{{ board.name }}
+			</h5>
+			<span data-test="board-suggestion-url">{{ board.url }}</span>
 		</div>
 	</div>
 </template>
