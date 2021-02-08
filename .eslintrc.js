2706380dfc5156b345e826ff066b90863bafa71f
Yashu Mittal <mittalyashu77@gmail.com>
2021-02-08 17:59:21 +0530

    Separate client codebase (#316)
    * remove frontend codebase
    
    * remove frontend dependencies & scripts
    
    * remove "server:" prefix from script command
    
    * server:test -> test
    * server:dev -> dev
    
    * remove frontend directoy from test push path event trigger
    
    * split install pkg process
    
    * clone default logchimp theme in client task
    
    * install pkg with yarn production false flag in gitpod
    
    * install pkg & clone theme in gitpod setup script
    
    * cd to theme directory before y dev in gitpod.yml
    
    * remove frontend dir from testPathIgnorePatterns jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,11 @@
 module.exports = {
 	root: true,
 	env: {
-		browser: true,
 		node: true,
 		es6: true
 	},
-	plugins: ["vue", "prettier"],
-	extends: [
-		"eslint:recommended",
-		"plugin:prettier/recommended",
-		"plugin:vue/recommended"
-	],
-	rules: {
-		"vue/html-indent": [1, "tab"],
-		"vue/max-attributes-per-line": [
-			2,
-			{
-				singleline: 2,
-				multiline: {
-					max: 1,
-					allowFirstLine: false
-				}
-			}
-		]
-	},
+	plugins: ["prettier"],
+	extends: ["eslint:recommended", "plugin:prettier/recommended"],
 	overrides: [
 		{
 			files: ["**/*.test.js", "**/*.spec.js"],
