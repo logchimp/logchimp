aaf0e532389e70ef3c53fa77ca7ea07d5ba6fa5a
Yashu Mittal <mittalyashu77@gmail.com>
2021-06-20 17:02:03 +0530

    create comment in single transaction

--- server/controllers/post/comments/create.js
+++ server/controllers/post/comments/create.js
@@ -14,22 +14,49 @@ module.exports = async (req, res) => {
 	// check the auth user has permission to comment
 
 	try {
-		const comment = await database
-			.insert({
-				id: uuid(),
-				parent_id,
-				body,
-				author_id: userId,
-				post_id,
-				is_internal,
-				created_at: new Date().toJSON(),
-				updated_at: new Date().toJSON()
-			})
-			.into("posts_comments")
-			.returning("*");
+		const results = await database.transaction(async (trx) => {
+			// postActivityId will be shared b/w "posts_comments" and "posts_activity" table
+			const postActivityId = uuid();
+
+			const comments = await trx("posts_comments").insert(
+				{
+					id: uuid(),
+					parent_id,
+					body,
+					activity_id: postActivityId,
+					is_internal,
+					created_at: new Date().toJSON(),
+					updated_at: new Date().toJSON()
+				},
+				"*"
+			);
+
+			const comment = comments[0];
+			await database("posts_activity")
+				.insert({
+					id: postActivityId,
+					type: "comment",
+					posts_comments_id: comment.id,
+					post_id,
+					author_id: userId,
+					created_at: new Date().toJSON()
+				})
+				.transacting(trx);
+
+			const author = await database
+				.select("userId", "name", "username", "avatar")
+				.from("users")
+				.where({ userId })
+				.first();
+
+			return {
+				...comment,
+				author
+			}
+		});
 
 		res.status(201).send({
-			comment: comment[0]
+			comment: results
 		});
 	} catch (err) {
 		logger.log({
