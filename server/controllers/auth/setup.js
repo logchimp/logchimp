e08ea8e49e3af24cabb42935c69104438073feaa
Yashu Mittal <mittalyashu77@gmail.com>
2021-07-18 17:09:29 +0530

    Change error from noEmailProvided to invalidEmail
    Ref: a58d998

--- server/controllers/auth/setup.js
+++ server/controllers/auth/setup.js
@@ -13,8 +13,8 @@ module.exports = async (req, res, next) => {
 
 	if (!validEmail(email)) {
 		return res.status(400).send({
-			message: error.api.authentication.noEmailProvided,
-			code: "EMAIL_MISSING"
+			message: error.api.authentication.invalidEmail,
+			code: "EMAIL_INVALID"
 		});
 	}
 
