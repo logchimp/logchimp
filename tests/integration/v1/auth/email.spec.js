d05ae1f0a2366fd2a17277e50bca3a6e70c8c657
Yashu Mittal <mittalyashu77@gmail.com>
2021-08-08 15:17:25 +0530

    test: "should validate email token"

--- tests/integration/v1/auth/email.spec.js
+++ tests/integration/v1/auth/email.spec.js
@@ -3,6 +3,7 @@ const supertest = require("supertest");
 const app = require("../../../../server");
 const database = require("../../../../server/database");
 const { createToken } = require("../../../../server/services/token.service");
+const { user: generateUser } = require('../../../utils/generators');
 
 beforeAll(async () => {
 	return await database.migrate.latest();
@@ -22,6 +23,7 @@ describe("POST /api/v1/auth/email/validate", () => {
 	});
 
 	it("should throw error \"INVALID_TOKEN\"", async () => {
+    // generate token
     const tokenPayload = {
 			userId: "601db0cd-ba5b-480d-a62b-06c8dcb72267",
 			email: "mittalyashu77@gmail.com",
@@ -41,5 +43,44 @@ describe("POST /api/v1/auth/email/validate", () => {
 		expect(response.body.code).toBe("INVALID_TOKEN");
 	});
 
+  it("should validate email token", async () => {
+		const user = generateUser();
+		user.isVerified = false;
 
+    // generate and save token to database
+		await database.insert(user).into("users");
+
+    const tokenPayload = {
+			userId: user.userId,
+			email: user.email,
+			type: "emailVerification"
+		};
+
+    const token = createToken(tokenPayload, {
+      expiresIn: "2h"
+    });
+
+    await database.insert({
+      email: tokenPayload.email,
+      token
+    })
+    .into("emailVerification");
+
+    const response = await supertest(app).post("/api/v1/auth/email/validate").send({
+			token
+		});
+
+		expect(response.headers["content-type"]).toContain("application/json");
+		expect(response.status).toBe(200);
+		// expect(response.body.code).toBe("INVALID_TOKEN");
+		expect(response.body.verify.success).toBeTruthy();
+
+		// check if token still exists in database
+		const getToken = await database
+			.select()
+			.from("emailVerification")
+			.where({ email: user.email }).first();
+
+		expect(getToken).toBeUndefined();
+  })
 });
