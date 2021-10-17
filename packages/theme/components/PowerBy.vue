d034478f3e12eb8b6a61f29659330bea8ba90736
Yashu Mittal <mittalyashu77@gmail.com>
2021-10-17 19:50:23 +0530

    feat: create PowerBy component

---
+++ packages/theme/components/PowerBy.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="logchimp-poweredby">
+		<a
+			:href="`https://logchimp.codecarrot.net/?utm_source=${source}&utm_medium=powered&company=${getSiteSittings.title}`"
+		>
+			Powered by LogChimp
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "PowerBy",
+	computed: {
+		getSiteSittings() {
+			return this.$store.getters["settings/get"];
+		},
+		source() {
+			return this.$route.name;
+		}
+	}
+};
+</script>
+
+<style lang='sass'>
+.logchimp-poweredby
+	display: flex
+	justify-content: center
+	margin-top: 2rem
+
+	a
+		font-size: 0.875rem
+		color: var(--color-gray-70)
+</style>
