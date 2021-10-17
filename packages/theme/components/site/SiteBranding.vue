c189f60a8ce4e98840fccebdd5eaba5e94360c7d
Yashu Mittal <mittalyashu77@gmail.com>
2021-10-17 14:38:44 +0530

    refactor: pass settings props to SiteBranding component

--- packages/theme/components/site/SiteBranding.vue
+++ packages/theme/components/site/SiteBranding.vue
@@ -1,40 +1,31 @@
 <template>
-  <router-link :to="link" class="site-info">
-    <div class="logo-placeholder">
-      <img
-        class="site-logo"
-        :src="logo"
-        :alt="title"
-      >
-    </div>
-    <h5 v-if="title" class="site-name">
-      {{ title }}
-    </h5>
-  </router-link>
+	<router-link :to="link" class="site-info">
+		<div class="logo-placeholder">
+			<img class="site-logo" :src="settings.logo" :alt="settings.title" />
+		</div>
+		<h5 v-if="settings.title" class="site-name">
+			{{ settings.title }}
+		</h5>
+	</router-link>
 </template>
 
 <script>
 export default {
-  name: "SiteBranding",
-  props: {
-    dashboard: {
-      type: Boolean,
-      default: false
-    }
-  },
-  computed: {
-    getSettings() {
-      return this.$store.getters["settings/get"];
-    },
-    title() {
-      return this.getSettings.title;
-    },
-    logo() {
-      return this.getSettings.logo;
-    },
-    link() {
-      return this.dashboard ? "/dashboard" : "/";
-    }
-  }
+	name: "SiteBranding",
+	props: {
+		settings: {
+			type: Object,
+			required: true
+		},
+		dashboard: {
+			type: Boolean,
+			default: false
+		}
+	},
+	computed: {
+		link() {
+			return this.dashboard ? "/dashboard" : "/";
+		}
+	}
 };
 </script>
