7fd1adb9422c352dedf6a5b08cfe38651cfc4c0f
Yashu Mittal <mittalyashu77@gmail.com>
2021-10-17 14:22:04 +0530

    feat: create SiteBranding component
    showing Site logo + name

---
+++ packages/theme/components/site/SiteBranding.vue
@@ -0,0 +1,40 @@
+<template>
+  <router-link :to="link" class="site-info">
+    <div class="logo-placeholder">
+      <img
+        class="site-logo"
+        :src="logo"
+        :alt="title"
+      >
+    </div>
+    <h5 v-if="title" class="site-name">
+      {{ title }}
+    </h5>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "SiteBranding",
+  props: {
+    dashboard: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    getSettings() {
+      return this.$store.getters["settings/get"];
+    },
+    title() {
+      return this.getSettings.title;
+    },
+    logo() {
+      return this.getSettings.logo;
+    },
+    link() {
+      return this.dashboard ? "/dashboard" : "/";
+    }
+  }
+};
+</script>
