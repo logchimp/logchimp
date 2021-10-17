48a434560238c25a2a1bf13f04c8a442e86ab206
Yashu Mittal <mittalyashu77@gmail.com>
2021-10-17 21:54:23 +0530

    feat(layout): create viewer layout

---
+++ packages/theme/layouts/viewer.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<Header />
+		<div class="container container-view">
+			<Nuxt />
+			<power-by />
+		</div>
+	</div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+import PowerBy from "../components/PowerBy.vue";
+
+export default {
+	name: "ViewerLayout",
+	components: {
+		// components
+		Header,
+		PowerBy
+	},
+	mounted() {
+		if (!process.server) {
+			const user = localStorage.getItem("user");
+
+			if (user) {
+				this.$store.dispatch("user/login", JSON.parse(user));
+				this.$store.dispatch("user/updatePermissions");
+			}
+		}
+	}
+};
+</script>
