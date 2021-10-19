25cc02d4669947bb2673a9e5edbc54fd549663e9
Yashu Mittal <mittalyashu77@gmail.com>
2021-10-19 11:47:24 +0530

    refactor(Button): format the code

--- packages/theme/components/ui/Button.vue
+++ packages/theme/components/ui/Button.vue
@@ -1,18 +1,18 @@
 <template>
-  <div
-    class="button"
-    :class="[
-      `button-${type}`,
-      loading ? 'button-loading' : '',
-      disabled ? 'button-primary-disabled' : ''
-    ]"
-    @click="click"
-  >
-    <slot />
-    <div v-if="loading" class="button-loader">
-      <loader-icon />
-    </div>
-  </div>
+	<div
+		class="button"
+		:class="[
+			`button-${type}`,
+			loading ? 'button-loading' : '',
+			disabled ? 'button-primary-disabled' : ''
+		]"
+		@click="click"
+	>
+		<slot />
+		<div v-if="loading" class="button-loader">
+			<loader-icon />
+		</div>
+	</div>
 </template>
 
 <script>
@@ -20,31 +20,31 @@
 import LoaderIcon from "../icons/Loader";
 
 export default {
-  name: "Button",
-  components: {
-    LoaderIcon
-  },
-  props: {
-    type: {
-      type: String,
-      required: true
-    },
-    loading: {
-      type: Boolean,
-      default: false
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    }
-  },
-  methods: {
-    click() {
-      if (this.loading) return;
-      if (this.disabled) return;
-      this.$emit("click");
-    }
-  }
+	name: "Button",
+	components: {
+		LoaderIcon
+	},
+	props: {
+		type: {
+			type: String,
+			required: true
+		},
+		loading: {
+			type: Boolean,
+			default: false
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		}
+	},
+	methods: {
+		click() {
+			if (this.loading) return;
+			if (this.disabled) return;
+			this.$emit("click");
+		}
+	}
 };
 </script>
 
