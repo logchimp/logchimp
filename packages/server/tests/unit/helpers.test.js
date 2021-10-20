6b6fc36c03d4f7b685c998390dca9285813ea20c
Yashu Mittal <mittalyashu77@gmail.com>
2021-10-20 18:06:26 +0530

    feat: theme components unit tests (#571)
    * tests: PostItem
    
    * copy-paste all components unit tests
    
    * ci(test): run theme unit test
    
    * ci: add missing runs-on in cache job
    
    * ci: use cache in app steps
    
    * ci: use IDs to define jobs
    
    * ci: fix job id's
    
    * install vue-jest package
    
    * install vue-jest as devDependency
    
    * add babel configuration & install packages
    
    * @babel/preset-env
    * babel-plugin-transform-require-context
    
    * move API unit tests in packages/server directory
    
    * install babel-core@7.0.0-bridge.0 pkg
    
    * fix(tests): postCard
    
    * fix(tests): vote.spec.js
    
    * fix(tests): text.spec.js
    
    * install @vue/test-utils pkg
    
    * tests: fixed tests
    
    * stub nuxt-link tag
    
    * fix(tests): stub RoadmapPostCard in roadmapColumn.spec.js
    
    * ci: run tests pipeline for packages directory

---
+++ packages/server/tests/unit/helpers.test.js
@@ -0,0 +1,154 @@
+const _ = require("lodash");
+
+const {
+	validEmail,
+	validUUID,
+	generateHexColor
+} = require("../../helpers");
+
+describe("validate email", () => {
+	it('should be a valid email "yashu@codecarrot.net"', () => {
+		const response = validEmail("yashu@codecarrot.net");
+		expect(response).toBeTruthy();
+	});
+
+	it('should be a valid email "ya___u@codecarrot.net"', () => {
+		const response = validEmail("ya___u@codecarrot.net");
+		expect(response).toBeTruthy();
+	});
+
+	it('should be a valid email "ya___u@codecarrot.nt"', () => {
+		const response = validEmail("ya___u@codecarrot.nt");
+		expect(response).toBeTruthy();
+	});
+
+	it('should be a valid email "y@codecarrot.nt"', () => {
+		const response = validEmail("y@codecarrot.nt");
+		expect(response).toBeTruthy();
+	});
+
+	it('should be a valid email "y@codecarr.otn"', () => {
+		const response = validEmail("y@codecar.rotn");
+		expect(response).toBeTruthy();
+	});
+
+	it('should not be a valid email "y@codecarrot.n"', () => {
+		const response = validEmail("y@codecarrot.n");
+		expect(response).toBeFalsy();
+	});
+
+	it('should not be a valid email "y@codecarrotn"', () => {
+		const response = validEmail("y@codecarrotn");
+		expect(response).toBeFalsy();
+	});
+
+	it('should not be a valid email "y@codecarrotn@"', () => {
+		const response = validEmail("y@codecarrotn@");
+		expect(response).toBeFalsy();
+	});
+
+	it('should not be a valid email "y@codecarr.otn@"', () => {
+		const response = validEmail("y@codecar.rotn@");
+		expect(response).toBeFalsy();
+	});
+
+	it('should not be a valid email "y@code$%&$%carrot.n"', () => {
+		const response = validEmail("y@code$%&$%carrot.n");
+		expect(response).toBeFalsy();
+	});
+
+	it('should not be a valid email "^#^3y@code$%&$%carrot.n"', () => {
+		const response = validEmail("^#^3y@code$%&$%carrot.n");
+		expect(response).toBeFalsy();
+	});
+
+	it('should not be a valid email "___yashu@carrot.n"', () => {
+		const response = validEmail("___yashu@carrot.n");
+		expect(response).toBeFalsy();
+	});
+});
+
+describe("validate UUID", () => {
+	it("should return empty string with empty string", () => {
+		const res = validUUID("");
+
+		expect(res).toEqual("");
+		expect(typeof res).toEqual("string");
+	});
+
+	it("should return empty string with random string", () => {
+		const res = validUUID("&(9798709879");
+
+		expect(res).toEqual("");
+		expect(typeof res).toEqual("string");
+	});
+
+	it("should return empty string with SQL injection", () => {
+		const res = validUUID("DELETE * FROM posts;");
+
+		expect(res).toEqual("");
+		expect(typeof res).toEqual("string");
+	});
+
+	it("should return empty string with empty array", () => {
+		const res = validUUID([]);
+
+		expect(res).toEqual("");
+		expect(typeof res).toEqual("string");
+	});
+
+	it("should return empty string with invalid UUID in array", () => {
+		const res = validUUID(["sdfs", "tevedfsad", "*****"]);
+
+		expect(res).toEqual("");
+		expect(typeof res).toEqual("string");
+	});
+
+	it("should return empty string with SQL injection in array", () => {
+		const res = validUUID(['UPDATE posts SET title = "LOL"', "DELETE posts"]);
+
+		expect(res).toEqual("");
+		expect(typeof res).toEqual("string");
+	});
+
+	it("should return empty string with invalid UUID in array", () => {
+		const res = validUUID([
+			"48EACC70-6D5E-403F-8CC2-AD945A3F3C",
+			"E82739-FB69-4F06-9FEE-184CC834A492",
+			"7D663644-5714-42-8A34-C34971A48A45",
+			"BC8F29C0-5813-4162-94-7EB137CD2B13"
+		]);
+
+		expect(res).toEqual("");
+		expect(typeof res).toEqual("string");
+	});
+
+	it("should return array of UUID with array of valid UUID", () => {
+		const res = validUUID([
+			"12E67136-3A19-4F71-9B41-DA8CE61BC80F",
+			"D7573C0E-7909-448C-BA1D-6B29042F95C8"
+		]);
+
+		expect(res).toEqual([
+			"12E67136-3A19-4F71-9B41-DA8CE61BC80F",
+			"D7573C0E-7909-448C-BA1D-6B29042F95C8"
+		]);
+		expect(_.isArray(res)).toBeTruthy();
+	});
+
+	it("should return UUID with valid UUID", () => {
+		const res = validUUID("1A3578E2-9594-4DDD-8D98-ACC55B1D6F99");
+
+		expect(res).toEqual("1A3578E2-9594-4DDD-8D98-ACC55B1D6F99");
+		expect(typeof res).toEqual("string");
+	});
+});
+
+describe("generateHexColor", () => {
+	it("should generate 6 digit HEX color code", () => {
+		const color = generateHexColor();
+		const result = /^#([a-fA-F0-9]){3}$|[a-fA-F0-9]{6}$/gi.test(color);
+
+		expect(result).toBeTruthy();
+	});
+});
---
+++ packages/theme/tests/unit/text.spec.js
@@ -0,0 +1,81 @@
+import LText from "../../components/ui/LText";
+import { mount } from "@vue/test-utils";
+
+describe("text input", () => {
+  const wrapper = mount(LText);
+
+  it("should not have label", () => {
+    expect(wrapper.find("[data-test=input-field-label]").exists()).toBe(false);
+  });
+
+  it("should have label", async () => {
+    await wrapper.setProps({
+      label: "Input label"
+    });
+
+    expect(wrapper.find("[data-test=input-field-label]").text()).toBe(
+      "Input label"
+    );
+  });
+
+  it("Do not show any error message", () => {
+    expect(wrapper.find("[data-test=input-error-message]").exists()).toBe(
+      false
+    );
+  });
+
+  describe("Disable input", () => {
+    const wrapper = mount(LText, {
+      propsData: {
+        disabled: true
+      }
+    });
+
+    it("Has 'input-field-disabled' class", () => {
+      expect(
+        wrapper.find("[data-test=input-field]").classes("input-field-disabled")
+      ).toBe(true);
+    });
+
+    it("Has 'disabled' attribute in input", () => {
+      expect(
+        wrapper.find("[data-test=input-field]").attributes("disabled")
+      ).toBe("disabled");
+    });
+  });
+
+  describe("Error in input and message section", () => {
+    const wrapper = mount(LText, {
+      propsData: {
+        error: {
+          show: true
+        }
+      }
+    });
+
+    it("Red color border on input", () => {
+      expect(
+        wrapper.find("[data-test=input-field]").classes("input-error")
+      ).toBe(true);
+    });
+
+    it("Show empty error message", () => {
+      expect(wrapper.find("[data-test=input-error-message]").exists()).toBe(
+        true
+      );
+    });
+
+    it("Check error with message", async () => {
+      await wrapper.setProps({
+        error: {
+          show: true,
+          message: "Some error message"
+        }
+      });
+
+      expect(wrapper.find("[data-test=input-error-message]").text()).toBe(
+        "Some error message"
+      );
+    });
+  });
+});
