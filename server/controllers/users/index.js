cd05127205bf4dc858cd0f3594d0b4886cdeede0
Yashu Mittal <mittalyashu77@gmail.com>
2021-10-30 15:35:17 +0530

    feat: RUD user from dashboard (#537)
    * migration: add notes to users table
    
    * create APIs for getUserInfo
    
    * .
    
    * comment out routes for /users API
    
    NOTE: they're not ready, yet!

--- server/controllers/users/index.js
+++ server/controllers/users/index.js
@@ -3,11 +3,15 @@ const getProfile = require("./getProfile");
 const updateProfile = require("./updateProfile");
 const accessDashboard = require("./accessDashboard");
 const getUserPermissions = require("./getUserPermissions");
+const getUserInfo = require('./getUserInfo')
+const updateUserInfo = require('./updateUserInfo')
 
 module.exports = {
 	...filter,
 	getProfile,
 	updateProfile,
 	...accessDashboard,
-	getUserPermissions
+	getUserPermissions,
+	getUserInfo,
+	updateUserInfo
 };
