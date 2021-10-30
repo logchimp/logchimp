2c519c8ed98064c156fe67d121af82c326ea9b21
Yashu Mittal <mittalyashu77@gmail.com>
2021-10-30 15:29:55 +0530

    comment out routes for /users API
    NOTE: they're not ready, yet!

--- server/routes/v1/users.js
+++ server/routes/v1/users.js
@@ -16,8 +16,12 @@ router.get("/users/permissions", middleware.apiAuth, users.getUserPermissions);
 router.get("/users/dashboard", middleware.apiAuth, users.accessDashboard);
 
 // TODO: create permission for user:read, user:update, user:create, user:delete
-router.get("/users/get", middleware.apiAuth, users.accessDashboard);
-router.get("/users/:user_id/get", middleware.apiAuth, users.getUserInfo);
-router.patch("/users/:user_id/update", middleware.apiAuth, users.updateUserInfo);
+// router.get("/users/get", middleware.apiAuth, users.accessDashboard);
+// router.get("/users/:user_id/get", middleware.apiAuth, users.getUserInfo);
+// router.patch(
+// 	"/users/:user_id/update",
+// 	middleware.apiAuth,
+// 	users.updateUserInfo
+// );
 
 module.exports = router;
