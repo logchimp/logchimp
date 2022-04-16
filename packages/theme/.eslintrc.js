aac17a362b0be62c830d933880f00af21eae955c
Yashu Mittal <mittalyashu77@gmail.com>
2022-04-16 22:34:01 +0530

    chore: ESLint setup in theme package

---
+++ packages/theme/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  extends: [
+    'eslint:recommended',
+    '@nuxtjs'
+  ]
+}
