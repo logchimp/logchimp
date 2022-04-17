90a2cd1a416f0c4db32d66825f54f7b19a1ce195
Yashu Mittal <mittalyashu77@gmail.com>
2022-04-17 20:10:13 +0530

    fix: uncontrolled data used for path expression in serverImage controller

--- packages/server/controllers/serveImages.js
+++ packages/server/controllers/serveImages.js
@@ -2,9 +2,23 @@
 const fs = require("fs");
 const path = require("path");
 
+/**
+ * Following image formats are allowed (they're case sensitive):
+ * - jpg
+ * - jpeg
+ * - png
+ * - svg
+ */
 const serveImages = (req, res) => {
+	const imagePathRegex = /^\/(.+)(?:pn|jpe?|sv)g$/gim;
+	const filterImagePath = req.url.match(imagePathRegex);
+
+	if (filterImagePath === null) {
+		return res.sendStatus(400);
+	}
+
 	fs.readFile(
-		path.join(__dirname, "../../content/images", req.url),
+		path.join(__dirname, "../../../content/images", filterImagePath[0]),
 		(err, data) => {
 			if (err) {
 				res.sendStatus(404);
