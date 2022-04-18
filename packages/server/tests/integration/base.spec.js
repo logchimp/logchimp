3efd398eb4fcae96690f41ff64afea2d391cc07e
Yashu Mittal <mittalyashu77@gmail.com>
2022-04-18 22:05:07 +0530

    fix: uncontrolled data used for path expression in serverImage (#685)
    * fix: uncontrolled data used for path expression in serverImage controller
    
    * fix: response status code 404 for "null" filterImagePath
    
    * chore: install "avatar-generator" pkg
    
    * feat(test): write test for server image

--- packages/server/tests/integration/base.spec.js
+++ packages/server/tests/integration/base.spec.js
@@ -1,5 +1,8 @@
 const app = require("../../app");
 const supertest = require("supertest");
+const avatarGenerator = require("avatar-generator");
+const fs = require("fs");
+const path = require("path");
 
 test("GET /api", async () => {
 	const response = await supertest(app).get("/api");
@@ -15,3 +18,42 @@ test("GET /api", async () => {
 // 	expect(response.headers["content-type"]).toContain("application/json");
 // 	expect(response.body.code).toEqual("ROUTE_NOT_FOUND");
 // });
+
+describe("Server images", () => {
+	test("should return 404 for readme.md file", async () => {
+		const response = await supertest(app).get("/content/images/readme.md");
+
+		expect(response.status).toEqual(404);
+	});
+
+	test("should server 'png' image", async () => {
+		// generate ramdom image
+		const generateAvatar = new avatarGenerator({
+			imageExtension: ".png"
+		});
+
+		// Directory path for "content/image" directory
+		const contentImageDirectoryPath = path.join(
+			__dirname,
+			"../../../../content/images"
+		);
+		const randomAvatarImage = await generateAvatar.generate(
+			"contact@codecarrot.net",
+			"male"
+		);
+
+		console.log(contentImageDirectoryPath);
+
+		await randomAvatarImage
+			.png()
+			.pipe(
+				fs.createWriteStream(`${contentImageDirectoryPath}/random-avatar.png`)
+			);
+
+		const response = await supertest(app).get(
+			"/content/images/random-avatar.png"
+		);
+
+		expect(response.status).toEqual(200);
+	});
+});
