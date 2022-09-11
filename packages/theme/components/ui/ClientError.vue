637bdc7c51852ce1cea5766c4faac75234ee3542
mittalyashu <mittalyashu77@gmail.com>
2022-09-11 10:12:11 +0000

    feat: create simple ClientError component

---
+++ packages/theme/components/ui/ClientError.vue
@@ -0,0 +1,18 @@
+<template>
+	<div :class="$style.error">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ClientError"
+}
+</script>
+
+<style module>
+.error {
+	text-align: center;
+}
+</style>
+
