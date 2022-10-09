4065bc25cd976d806a2a1997f27594fe3bd37954
mittalyashu <mittalyashu77@gmail.com>
2022-10-09 18:58:35 +0000

    feat(theme:refactor): initailise Pinia store directly in main.ts file

--- packages/theme/src/main.ts
+++ packages/theme/src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from 'vue'
 import { createHead } from '@vueuse/head';
+import { createPinia } from "pinia";
 
 import App from './App.vue'
 import {router} from "./router";
-import store from "./store";
 
 import "./styles/main.sass";
 
 const app = createApp(App)
+const store = createPinia()
 const head = createHead()
 
 app.use(router)
