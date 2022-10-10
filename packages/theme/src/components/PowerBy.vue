eb04bfdb65b7ab40cac1b821ad797b5b7abc3ea0
mittalyashu <mittalyashu77@gmail.com>
2022-10-10 17:54:37 +0000

    feat(theme): copy PowerBy component

--- packages/theme/src/components/PowerBy.vue
+++ packages/theme/src/components/PowerBy.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="logchimp-poweredby">
-    <a
-      :href="
-        `https://logchimp.codecarrot.net/?utm_source=${source}&utm_medium=powered&company=${siteSettings.title}`
-      "
-    >
-      Powered by LogChimp
-    </a>
-  </div>
+	<div class="logchimp-poweredby">
+		<a
+			:href="
+				`https://logchimp.codecarrot.net/?utm_source=${source}&utm_medium=powered&company=${siteSettings.title}`
+			"
+		>
+			Powered by LogChimp
+		</a>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -21,3 +21,14 @@ const { get: siteSettings } = useSettingStore()
 
 const source = computed(() => route.name?.toString())
 </script>
+
+<style lang='sass'>
+.logchimp-poweredby
+	display: flex
+	justify-content: center
+	margin-top: 2rem
+
+	a
+		font-size: 0.875rem
+		color: var(--color-gray-70)
+</style>
