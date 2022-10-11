1ee87d2756921a434dec4720b31bd55356f558e5
mittalyashu <mittalyashu77@gmail.com>
2022-10-11 18:18:05 +0000

    feat(theme): copy Vote component

--- packages/theme/src/components/vote/Vote.vue
+++ packages/theme/src/components/vote/Vote.vue
@@ -1,20 +1,20 @@
 <template>
-  <div
-    class="post-voters"
-    data-test="vote"
-    :class="[
-      loading ? 'post-voters-loading' : '',
-      disabled ? 'post-voters-disabled' : ''
-    ]"
-    @click="changeVote"
-  >
-    <arrow-icon
-      class="post-voters-arrow"
-      data-test="vote-arrow"
-      :class="{ 'post-voters-vote': isVoted }"
-    />
-    <span data-test="vote-count">{{ votesCount }}</span>
-  </div>
+	<div
+		class="post-voters"
+		data-test="vote"
+		:class="{
+			'post-voters-loading': loading,
+			'post-voters-disabled': disabled
+		}"
+		@click="changeVote"
+	>
+		<arrow-icon
+			class="post-voters-arrow"
+			data-test="vote-arrow"
+			:class="{ 'post-voters-vote': isVoted }"
+		/>
+		<span data-test="vote-count">{{ votesCount }}</span>
+	</div>
 </template>
 
 <script lang="ts">
@@ -70,18 +70,18 @@ const emit = defineEmits<{
 }>()
 
 const loading = ref<boolean>(false);
-const { getUserId, permissions } = useUserStore()
+const userStore = useUserStore()
 
 const disabled = computed(() => {
-	if (!getUserId) return false;
+	if (!userStore.getUserId) return false;
 
-	const checkPermission = permissions.includes("vote:create");
+	const checkPermission = userStore.permissions.includes("vote:create");
 	return !checkPermission;
 })
 
 async function changeVote() {
-	if (loading.value) return;
-	if (disabled.value) return;
+	if (loading.value || disabled.value) return;
+
 
 	loading.value = true;
 
@@ -90,9 +90,9 @@ async function changeVote() {
 			const response = await deleteVote(props.postId);
 
 			emit("update-voters", response.data.voters);
+			loading.value = false;
 		} catch (error) {
 			tokenError(error);
-		} finally {
 			loading.value = false;
 		}
 	} else {
@@ -100,11 +100,50 @@ async function changeVote() {
 			const response = await addVote(props.postId);
 
 			emit("update-voters", response.data.voters);
+			loading.value = false;
 		} catch (error) {
 			tokenError(error);
-		} finally {
 			loading.value = false;
 		}
 	}
 }
 </script>
+
+<style lang='sass'>
+.post-voters
+	margin-right: 1rem
+	display: flex
+	flex-direction: column
+	align-items: center
+	padding: 0.312rem 0.5rem 0.5rem
+	border: 1px solid var(--color-gray-90)
+	border-radius: var(--border-radius-default)
+	cursor: pointer
+	user-select: none
+
+	&:hover
+		border-color: var(--color-gray-80)
+
+.post-voters-arrow
+	margin-bottom: 0.185rem
+	fill: var(--color-gray-90)
+
+.post-voters-vote
+	fill: var(--color-brand-color)
+
+.post-voters-loading
+	opacity: 0.8
+	cursor: wait
+
+.post-voters-disabled
+	background-color: var(--color-gray-95)
+	border-color: var(--color-gray-95)
+	cursor: default
+	opacity: 0.7
+
+	&:hover
+		border-color: var(--color-gray-95)
+
+	.post-voters-arrow
+		fill: var(--color-gray-80)
+</style>
