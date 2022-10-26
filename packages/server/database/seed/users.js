eee5720e7f9cd447e392a1f8081dcea878e78a8b
mittalyashu <mittalyashu77@gmail.com>
2022-10-26 19:47:33 +0000

    feat(refactor): use knex seed built-in feature

--- packages/server/database/seed/users.js
+++ packages/server/database/seed/users.js
@@ -2,18 +2,26 @@ const { v4: uuidv4 } = require("uuid");
 
 const { hashPassword } = require("../../helpers");
 
-module.exports = [
-  {
-    userId: uuidv4(),
-    email: "user_exists@example.com",
-    password: hashPassword("strongPassword"),
-    username: "user_exists",
-  },
-  {
-    userId: uuidv4(),
-    email: "user_blocked@example.com",
-    password: hashPassword("strongPassword"),
-    username: "user_blocked",
-    isBlocked: true,
-  },
-];
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("users").del();
+  await knex("users").insert([
+    {
+      userId: uuidv4(),
+      email: "user_exists@example.com",
+      password: hashPassword("strongPassword"),
+      username: "user_exists",
+    },
+    {
+      userId: uuidv4(),
+      email: "user_blocked@example.com",
+      password: hashPassword("strongPassword"),
+      username: "user_blocked",
+      isBlocked: true,
+    },
+  ]);
+};
