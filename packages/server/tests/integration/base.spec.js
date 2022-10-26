f7950dc0de808bec4c1670694435ee1ccb5936a7
mittalyashu <mittalyashu77@gmail.com>
2022-10-26 14:18:23 +0000

    feat: add "Server images" test

--- packages/server/tests/integration/base.spec.js
+++ packages/server/tests/integration/base.spec.js
@@ -16,3 +16,42 @@ it("GET /api", async () => {
 // 	expect(response.headers["content-type"]).toContain("application/json");
 // 	expect(response.body.code).toEqual("ROUTE_NOT_FOUND");
 // });
+
+// describe("Server images", () => {
+//   test("should return 404 for readme.md file", async () => {
+//     const response = await supertest(app).get("/content/images/readme.md");
+//
+//     expect(response.status).toEqual(404);
+//   });
+//
+//   test("should server 'png' image", async () => {
+//     // generate ramdom image
+//     const generateAvatar = new avatarGenerator({
+//       imageExtension: ".png",
+//     });
+//
+//     // Directory path for "content/image" directory
+//     const contentImageDirectoryPath = path.join(
+//       __dirname,
+//       "../../../../content/images",
+//     );
+//     const randomAvatarImage = await generateAvatar.generate(
+//       "contact@codecarrot.net",
+//       "male",
+//     );
+//
+//     console.log(contentImageDirectoryPath);
+//
+//     await randomAvatarImage
+//       .png()
+//       .pipe(
+//         fs.createWriteStream(`${contentImageDirectoryPath}/random-avatar.png`),
+//       );
+//
+//     const response = await supertest(app).get(
+//       "/content/images/random-avatar.png",
+//     );
+//
+//     expect(response.status).toEqual(200);
+//   });
+// });
