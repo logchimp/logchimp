6cc3cdf6db8e1994335f86f7f4ecaa9bdd5a80f5
mittalyashu <mittalyashu77@gmail.com>
2022-12-10 10:13:01 +0000

    feat(theme): setup tailwind

---
+++ packages/theme/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
