7b1a8705252a4cd85b582accbbd1ea06d5b28f05
mittalyashu <mittalyashu77@gmail.com>
2022-12-11 09:07:13 +0000

    feat: change Breadcrumb component to empty slot component with pre-styles classes

--- packages/theme/src/components/Breadcrumbs.vue
+++ packages/theme/src/components/Breadcrumbs.vue
@@ -1,40 +1,8 @@
 <template>
   <div class="breadcrumbs">
-    <div
-      v-for="(item, index) in items"
-      :key="index"
-      class="breadcrumbs"
-    >
-      <router-link
-        v-if="item.href"
-        :to="item.href"
-        class="breadcrum-item"
-      >
-        {{ item.text }}
-      </router-link>
-      <div v-else class="breadcrum-item">
-        {{ item.text }}
-      </div>
-      <div v-if="lastItem" class="breadcrum-divider">
-        /
-      </div>
-    </div>
+    <slot />
   </div>
 </template>
-
-<script lang="ts">
-interface BreadcrumbItemType {
-	href?: string
-	text: string
-}
-</script>
-
-<script setup lang="ts">
-defineProps<{
-	items: BreadcrumbItemType[]
-	lastItem?: boolean
-}>()
-</script>
 
 <style lang='sass'>
 .breadcrumbs
