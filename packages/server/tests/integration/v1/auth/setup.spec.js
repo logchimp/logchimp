998fba747b01a5c2729b36cb4b0abfa4d4d733da
Yashu Mittal <mittalyashu77@gmail.com>
2023-10-14 14:16:12 +0530

    feat(test): add setup site integration test

--- packages/server/tests/integration/v1/auth/setup.spec.js
+++ packages/server/tests/integration/v1/auth/setup.spec.js
@@ -1,4 +1,5 @@
 import { describe, it, expect } from "vitest";
+import { cleanDb } from "../../../utils/db";
 const supertest = require("supertest");
 
 const app = require("../../../../app");
@@ -21,4 +22,23 @@ describe("POST /api/v1/auth/setup", () => {
     expect(response.status).toBe(400);
     expect(response.body.code).toBe("PASSWORD_MISSING");
   });
+
+  it('should setup site', async () => {
+    await cleanDb();
+
+    const response = await supertest(app).post("/api/v1/auth/setup").send({
+      siteTitle: "test site setup",
+      name: "Admin",
+      email: "admin@example.com",
+      password: "password",
+    });
+
+    expect(response.headers["content-type"]).toContain("application/json");
+    expect(response.status).toEqual(201);
+
+    expect(response.body.user).toBeDefined()
+    expect(response.body.user.name).toEqual('Admin')
+    expect(response.body.user.email).toEqual('admin@example.com')
+    expect(response.body.user.authToken).toBeDefined()
+  })
 });
