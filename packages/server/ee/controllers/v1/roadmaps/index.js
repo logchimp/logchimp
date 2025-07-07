3b873932585eaef3ee0bf5234194505bd4ff4806
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-07 13:16:33 +0530

    .

---
+++ packages/server/ee/controllers/v1/roadmaps/index.js
@@ -0,0 +1,11 @@
+const create = require("./create");
+const updateRoadmap = require("./updateRoadmap");
+const sort = require("./sort");
+const deleteById = require("./deleteById");
+
+module.exports = {
+  create,
+  updateRoadmap,
+  sort,
+  deleteById,
+};
