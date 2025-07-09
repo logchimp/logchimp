8183ff76e03f722de360b612e603b26e01fa4d6e
Shivanshee sahu <smahi4771@gmail.com>
2025-07-09 20:20:01 +0530

    Required Changes applied,Apply blacklist function in reset.js

--- packages/server/controllers/auth/password/reset.js
+++ packages/server/controllers/auth/password/reset.js
@@ -4,11 +4,11 @@ const passwordResetEmail = require("../../../services/auth/passwordReset");
 // utils
 const logger = require("../../../utils/logger");
 const error = require("../../../errorResponse.json");
-//import blacklist function
+// Import blacklist function
 const { isDomainBlacklisted } = require("../domainBlacklist");
 exports.reset = async (req, res) => {
   const { userId, email } = req.user;
-// Check if email's domain is blacklisted
+ // Check if email's domain is blacklisted
   if (isDomainBlacklisted(email)) {
     return res.status(403).send({
       message: "The domain of the email is not allowed.",
