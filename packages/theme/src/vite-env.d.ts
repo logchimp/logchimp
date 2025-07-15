9c08c032ada8d4525d342360a3389a00322a1cb1
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-15 11:33:42 +0530

    feat(theme): add `VITE_API_URL` env to ImportMetaEnv TS interface

--- packages/theme/src/vite-env.d.ts
+++ packages/theme/src/vite-env.d.ts
@@ -7,3 +7,11 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
