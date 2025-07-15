4fd0b3d707399432afbda92d34ca205caa90c286
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-15 11:57:46 +0530

    feat(theme): inject app env safely into `<script>` using sh script

---
+++ packages/theme/inject-app-env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Safely build a <script> block assigning to window.__APP_ENV__
+ENV_SCRIPT="<script>
+  window.__APP_ENV__ = {
+    VITE_API_URL: '${VITE_API_URL}',
+  };
+</script>"
+
+# Escape newlines for safe sed usage
+ESCAPED_SCRIPT=$(printf "%s\n" "$ENV_SCRIPT" | sed ':a;N;$!ba;s/\n/\\n/g')
+
+# Inject just before the first <script type="module"> tag
+sed -i "s@<script type=\"module\"@${ESCAPED_SCRIPT}\n<script type=\"module\"@" /app/dist/index.html
