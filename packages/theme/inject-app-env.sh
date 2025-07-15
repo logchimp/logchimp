826d75e92edef4f7fe564fa2bf597c2f46b814c3
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-15 12:14:41 +0530

    fix(theme): run 'inject-app-env.sh' as ENTRYPOINT in Dockerfile

--- packages/theme/inject-app-env.sh
+++ packages/theme/inject-app-env.sh
@@ -12,3 +12,6 @@ ESCAPED_SCRIPT=$(printf "%s\n" "$ENV_SCRIPT" | sed ':a;N;$!ba;s/\n/\\n/g')
 
 # Inject just before the first <script type="module"> tag
 sed -i "s@<script type=\"module\"@${ESCAPED_SCRIPT}\n<script type=\"module\"@" /app/dist/index.html
+
+# Start your app (adjust as needed, e.g. serve or nginx)
+exec "$@"
