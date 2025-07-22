f3c07a1d601635f62d9681bf7343069912b7e0a1
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-22 13:23:41 +0530

    theme: auto create '.env' with `VITE_API_URL` env variable based on codespace configuration

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -3,3 +3,10 @@
 LC_SECRET=$(openssl rand -base64 12)
 
 echo "LOGCHIMP_SECRET_KEY=$LC_SECRET" > packages/server/.env
+
+echo "cs name: ${CODESPACE_NAME}"
+echo "forward domain: ${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+
+GH_CS_PUBLIC_HOST="https://${CODESPACE_NAME}-8000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+
+echo "VITE_API_URL=$GH_CS_PUBLIC_HOST" >> packages/theme/.env
