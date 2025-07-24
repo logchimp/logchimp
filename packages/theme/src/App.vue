b60034b0f0242308f914ba61ad2390bce30f9d5b
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-24 20:56:04 +0530

    feat(theme): bump vue-gtag v3.5.2 pkg
    Disable vue-gtag in `App.vue`

--- packages/theme/src/App.vue
+++ packages/theme/src/App.vue
@@ -23,7 +23,7 @@
 // packages
 import axios from "axios";
 import { computed, onMounted } from "vue";
-import gtag, { setOptions, bootstrap } from "vue-gtag";
+// import gtag, { setOptions, bootstrap } from "vue-gtag";
 import { useHead } from "@vueuse/head";
 
 import packageJson from "../package.json";
@@ -62,12 +62,12 @@ onMounted(async () => {
 
   // set google analytics
   if (settingsStore.get.googleAnalyticsId) {
-    setOptions({
-      config: {
-        id: settingsStore.get.googleAnalyticsId,
-      },
-    });
-
+    // TODO: Properly implement 'vue-gtag' feature
+    // setOptions({
+    //   config: {
+    //     id: settingsStore.get.googleAnalyticsId,
+    //   },
+    // });
     // bootstrap(gtag).then();
   }
 
