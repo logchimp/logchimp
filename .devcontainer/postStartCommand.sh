a2fe33a82ff9267d8bcf88dbf965a257f4f73ac3
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-26 20:54:39 +0530

    feat: wait for db to ready before starting logchimp backend server

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -2,5 +2,11 @@
 
 pnpm i;
 
+# Boot-up db and mail services
 docker-compose -f ./docker-compose.dev.yml \
-  up -d
+  up -d db mail
+
+# Wait for the database to be ready
+wait4x -t 60 -s localhost:5432 \
+  # Start the API service
+  exec 'docker-compose -f ./docker-compose.dev.yml up -d logchimp'
