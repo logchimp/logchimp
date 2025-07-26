0e02aaa4657c9fe4b03fdc2a7e227a665450c0e1
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-27 00:14:41 +0530

    Configure codespaces for vs desktop (#986)
    * chore: update the files based on the container lifecycle
    
    * update script to remove duplicated variable and ensure each variable is only defined once
    
    * feat: wait for db to ready before starting logchimp backend server
    
    * fix: install Wait4X CLI
    
    * feat: add tasks to run theme in codespaces
    
    * fix: improve database readiness check before starting LogChimp API service
    
    * fix: update sed command in accordance to linux OS
    
    * fix: streamline Wait4X CLI installation process in postCreateCommand.sh

--- .devcontainer/initializeCommand.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LC_SECRET=$(openssl rand -base64 12)
-
-echo "LOGCHIMP_SECRET_KEY=$LC_SECRET" >> packages/server/.env
-
-GH_CS_PUBLIC_HOST="https://${CODESPACE_NAME}-8000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
-
-echo "VITE_API_URL=$GH_CS_PUBLIC_HOST" >> packages/theme/.env
