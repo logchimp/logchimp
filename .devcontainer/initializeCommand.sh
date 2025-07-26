c78f7036e610a1707a54decc9e78d73e38e44531
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-26 11:43:08 +0530

    fix: append "LOGCHIMP_SECRET_KEY" variable to `packages/server/.env` file

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -2,10 +2,7 @@
 
 LC_SECRET=$(openssl rand -base64 12)
 
-echo "LOGCHIMP_SECRET_KEY=$LC_SECRET" > packages/server/.env
-
-echo "cs name: ${CODESPACE_NAME}"
-echo "forward domain: ${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+echo "LOGCHIMP_SECRET_KEY=$LC_SECRET" >> packages/server/.env
 
 GH_CS_PUBLIC_HOST="https://${CODESPACE_NAME}-8000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
 
