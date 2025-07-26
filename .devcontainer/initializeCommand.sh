419b83c74b46336e13c7d0152260109661f20ff3
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-26 12:06:18 +0530

    Feat/improve codespaces setup (#971)
    * devcontainer: use lightweight base dev container image for codespaces
    
    * theme: auto create '.env' with `VITE_API_URL` env variable based on codespace configuration
    
    * fix: use `typescript-node:22` as base docker image for devcontainers
    
    * fix: add docker-in-docker feature in devcontainers
    
    * fix: first install packages, then boot-up docker services
    
    * fix: append "LOGCHIMP_SECRET_KEY" variable to `packages/server/.env` file

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -2,4 +2,8 @@
 
 LC_SECRET=$(openssl rand -base64 12)
 
-echo "LOGCHIMP_SECRET_KEY=$LC_SECRET" > packages/server/.env
+echo "LOGCHIMP_SECRET_KEY=$LC_SECRET" >> packages/server/.env
+
+GH_CS_PUBLIC_HOST="https://${CODESPACE_NAME}-8000.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+
+echo "VITE_API_URL=$GH_CS_PUBLIC_HOST" >> packages/theme/.env
