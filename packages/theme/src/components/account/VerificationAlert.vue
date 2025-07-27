7eb55ea07f1dfe443ed19c82987a6ef0fbc923ac
Yashu Mittal <mittalyashu77@gmail.com>
2025-07-27 18:37:19 +0530

    feat(theme/settings): show "Email verification" request status

--- packages/theme/src/components/account/VerificationAlert.vue
+++ packages/theme/src/components/account/VerificationAlert.vue
@@ -6,20 +6,40 @@
     class="mb-8"
   >
     <template #cta>
-      <Button
-        type="primary"
-        :loading="loading"
-        @click="sendEmailVerificationHandler"
-      >
-        Resend
-      </Button>
+      <div class="flex items-center gap-x-4">
+        <Button
+          type="primary"
+          :loading="loading"
+          @click="sendEmailVerificationHandler"
+        >
+          Resend
+        </Button>
+
+        <div :class="['flex items-center gap-x-2 transition-opacity', requestStatus.isShown ? 'opacity-100' : 'opacity-0']">
+          <!-- Success -->
+          <template v-if="requestStatus.status === 'success'">
+            <CheckCircle2Icon aria-hidden="true" class="size-5 stroke-green-600" />
+            <span class="font-medium">Email sent</span>
+          </template>
+
+          <!-- Error -->
+          <template v-if="requestStatus.status === 'error'">
+            <XCircleIcon aria-hidden="true" class="size-5 stroke-red-500" />
+            <span class="font-medium">
+              Request failed
+            </span>
+          </template>
+        </div>
+
+      </div>
     </template>
   </alert>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 import axios from "axios";
+import { CheckCircle2Icon, XCircleIcon } from "lucide-vue";
 
 import { Alert } from "../ui/Alert";
 import Button from "../ui/Button.vue";
@@ -28,6 +48,15 @@ import { useUserStore } from "../../store/user";
 
 const { getUser } = useUserStore();
 const loading = ref<boolean>(false);
+
+interface IRequestStatus {
+  isShown: boolean;
+  status?: "success" | "error";
+}
+
+const requestStatus = reactive<IRequestStatus>({
+  isShown: false,
+});
 
 async function sendEmailVerificationHandler() {
   loading.value = true;
@@ -41,9 +70,16 @@ async function sendEmailVerificationHandler() {
       },
     });
 
-    console.log("res");
-    console.log(res);
+    requestStatus.isShown = true;
+    if (res?.data?.verify?.success) {
+      requestStatus.status = "success";
+    } else {
+      requestStatus.status = "error";
+    }
   } catch (error: unknown) {
+    requestStatus.isShown = true;
+    requestStatus.status = "error";
+
     // if (error.response.data.code === "MAIL_CONFIG_MISSING") {
     //   serverError.value = true;
     // }
@@ -51,6 +87,26 @@ async function sendEmailVerificationHandler() {
     console.error(error);
   } finally {
     loading.value = false;
+    resetRequestStatus();
   }
+}
+
+// Hide request status after timeout
+function resetRequestStatus() {
+  const timerDuration = requestStatus.status === "success" ? 2500 : 4000;
+
+  setTimeout(() => {
+    requestStatus.isShown = false;
+  }, timerDuration);
+
+  // After request status is visually hidden from UI
+  setTimeout(
+    () => {
+      requestStatus.status = undefined;
+    },
+    timerDuration +
+      // transition opacity duration
+      150,
+  );
 }
 </script>
