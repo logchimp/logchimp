7f0bf92f3b471a9a7b480f78bde55a8fcf104bf5
Yashu Mittal <mittalyashu77@gmail.com>
2025-08-01 10:29:56 +0530

    feat(server): add `environment.d.ts` file

---
+++ packages/server/src/environment.d.ts
@@ -0,0 +1,27 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "production" | "staging" | "ci" | "testing" | "development";
+    readonly PORT: string | undefined;
+    // readonly ORIGIN: string | undefined;
+
+    readonly LOGCHIMP_SECRET_KEY: string | undefined;
+
+    // Database
+    readonly LOGCHIMP_DB_SSL: string | undefined;
+    readonly LOGCHIMP_DB_HOST: string | undefined;
+    readonly LOGCHIMP_DB_USER: string | undefined;
+    readonly LOGCHIMP_DB_PASSWORD: string | undefined;
+    readonly LOGCHIMP_DB_DATABASE: string | undefined;
+    readonly LOGCHIMP_DB_PORT: string | undefined;
+
+    // SMTP - Mail Configuration
+    /**
+     * @deprecated
+     */
+    readonly LOGCHIMP_MAIL_SERVICE: string | undefined;
+    readonly LOGCHIMP_MAIL_HOST: string | undefined;
+    readonly LOGCHIMP_MAIL_USER: string | undefined;
+    readonly LOGCHIMP_MAIL_PASSWORD: string | undefined;
+    readonly LOGCHIMP_MAIL_PORT: string | undefined;
+  }
+}
