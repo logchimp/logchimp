50bfc54defa0e992bb4b4a368cc234004f1e919b
Yashu Mittal <mittalyashu77@gmail.com>
2025-08-01 17:37:01 +0530

    feat(theme): update TS types in users store

--- packages/types/src/auth.ts
+++ packages/types/src/auth.ts
@@ -1,4 +1,13 @@
-export interface ICurrentUserProfile {
+export interface IAuthUser {
+  userId: string;
+  avatar: string;
+  email: string;
+  name: string;
+  username: string;
+  authToken: string;
+}
+
+export interface IAuthUserProfile {
   userId: string;
   name: string;
   username: string;
