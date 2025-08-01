49e761ade8b0745b69c784ce21895aab1330fe05
Yashu Mittal <mittalyashu77@gmail.com>
2025-08-01 23:21:45 +0530

    feat(theme/dashboard): improve sidebar HTML structure

---
+++ packages/theme/src/components/dashboard/Sidebar/SidebarItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { RouteLocationRaw } from "vue-router";
+
+defineProps<{
+  to: RouteLocationRaw;
+}>();
+</script>
+
+<template>
+  <router-link
+    :class="[
+      'flex items-center py-2 px-3 rounded-lg',
+      '[&>svg]:stroke-(--color-white) [&>svg]:size-4 [&>svg]:mr-1.5',
+      '[&>p]:text-(--color-white)',
+      // active
+      'aria-[current=page]:bg-white/10',
+      'aria-[current=page][&>p]:font-semibold',
+      'aria-[current=page][&>p]:text-pink-500',
+    ]"
+    :to="to"
+  >
+    <slot />
+  </router-link>
+</template>
---
+++ packages/theme/src/components/dashboard/Sidebar/SidebarListHeading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-xs font-medium uppercase text-white/80 tracking-widest px-3">
+    <slot />
+  </div>
+</template>
