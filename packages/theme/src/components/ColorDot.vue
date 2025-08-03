efb12035d0d43e43669655ce4b11b9b19882296a
Yashu Mittal <mittalyashu77@gmail.com>
2025-08-03 18:55:55 +0530

    fix(theme/ui): set min & max size to ColorDot.vue component

--- packages/theme/src/components/ColorDot.vue
+++ packages/theme/src/components/ColorDot.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
 defineProps({
-  color: {
-    type: String,
-    required: true,
-  },
+	color: {
+		type: String,
+		required: true,
+	},
 });
 </script>
 
 <template>
   <div
     data-test="color-dot"
-    class="min-w-2.5 min-h-2.5 rounded-full"
+    class="min-w-2.5 min-h-2.5 max-w-2.5 max-h-2.5 rounded-full"
     :style="{
       backgroundColor: `#${color}`
     }"
   />
 </template>
-
