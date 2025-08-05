12928d7b370fd9556737f86179f1b753f614ae2f
Agastya Khati <krishkhati0007king@gmail.com>
2025-08-05 23:43:48 +0530

    Update packages/server/src/routes/v1/roadmaps.ts
    Co-authored-by: Yashu Mittal <mittalyashu77@gmail.com>

--- packages/server/src/routes/v1/roadmaps.ts
+++ packages/server/src/routes/v1/roadmaps.ts
@@ -6,7 +6,6 @@ import * as roadmaps from "../../controllers/roadmaps";
 import * as middleware from "../../middlewares";
 import { roadmapExists } from "../../middlewares/roadmapExists";
 
-
 router.get("/roadmaps", roadmaps.filter);
 
 router.get("/roadmaps/:url", roadmapExists, roadmaps.roadmapByUrl);
