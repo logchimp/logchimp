49746a34aca62613e200e1f2f7580d48205aa6d3
Yashu Mittal <mittalyashu77@gmail.com>
2025-08-07 21:58:15 +0530

    feat(theme/ui): improve DashboardUsersTabularItem more options dropdown

---
+++ packages/theme/src/components/ui/DropdownV2/DropdownContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <DropdownMenuContent
+    :class="[
+      'p-1 w-40',
+      'shadow-sm border border-neutral-300 bg-white rounded-lg'
+    ]"
+  >
+    <slot />
+  </DropdownMenuContent>
+</template>
+
+<script setup lang="ts">
+import { DropdownMenuContent } from "reka-ui";
+
+defineOptions({
+  name: "DropdownV2Content",
+});
+</script>
---
+++ packages/theme/src/ee/components/dashboard/users/TabularItem/MoreOptionsDropdown.vue
@@ -0,0 +1,39 @@
+<template>
+  <DropdownV2 class="table-icon-group boards-table-icons">
+    <template #trigger>
+      <DropdownMenuTrigger
+        :class="[
+          'table-data table-data-icon',
+          'boards-table-icon-settings',
+          'dropdown-menu-icon',
+        ]"
+      >
+        <MoreHorizontalIcon aria-hidden="true" class="stroke-neutral-700 size-5"/>
+        <span class="sr-only">More options</span>
+      </DropdownMenuTrigger>
+    </template>
+
+    <DropdownV2Content
+      align="end"
+      side="bottom"
+      :loop="true"
+    >
+      <MoreOptionsDropdownContent :userId="userId" />
+    </DropdownV2Content>
+  </DropdownV2>
+</template>
+
+<script setup lang="ts">
+import { MoreHorizontalIcon } from "lucide-vue";
+import { DropdownMenuTrigger } from "reka-ui";
+
+import DropdownV2 from "../../../../../components/ui/DropdownV2/Dropdown.vue";
+import DropdownV2Content from "../../../../../components/ui/DropdownV2/DropdownContent.vue";
+import MoreOptionsDropdownContent from "./MoreOptionsDropdownContent.vue";
+
+interface Props {
+  userId: string;
+}
+
+defineProps<Props>();
+</script>
