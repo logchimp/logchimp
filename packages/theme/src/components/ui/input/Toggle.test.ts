72af80d6aebb11c75c45485da41b293b8cfebad2
Yashu Mittal <mittalyashu77@gmail.com>
2025-08-14 15:21:19 +0530

    fix(theme/ui): update Toggle.test.ts file

--- packages/theme/src/components/ui/input/Toggle.test.ts
+++ packages/theme/src/components/ui/input/Toggle.test.ts
@@ -1,22 +1,50 @@
+import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 
 import Toggle from "./Toggle.vue";
 
 describe("toggle", () => {
-  const wrapper = mount(Toggle);
+  it("should be unchecked with gray background color", () => {
+    const wrapper = mount(Toggle);
+    const component = wrapper.find("[data-test=toggle]");
 
-  it("unchecked with gray color", () => {
-    expect(wrapper.find("[data-test=toggle]").attributes("style")).toBe(
-      "background-color: rgb(153, 153, 153);",
-    );
+    expect(component.attributes("data-state")).toBe("unchecked");
+    // TODO: should check the CSS gray background color, not css class
+    // expect(component.classes()).toContain("data-[state=unchecked]:bg-neutral-300");
   });
 
-  // todo: error: Cannot read property '0' of undefined
-  // it("emit checkbox value", async () => {
-  // 	const checkbox = wrapper.find("[data-test=toggle-checkbox]");
-  // 	await checkbox.trigger("click");
+  it("should be checked", () => {
+    const wrapper = mount(Toggle, {
+      props: {
+        modelValue: true,
+      },
+    });
 
-  // 	const value = wrapper.emitted("input");
-  // 	expect(value[0][0]).toBe(true);
+    const component = wrapper.find("[data-test=toggle]");
+    expect(component.attributes("data-state")).toBe("checked");
+  });
+
+  // it.skip("should be checked with brand color", () => {
+  //   const component = wrapper.find("[data-test=toggle]");
+  //   console.log(component);
+  //   const styles = component.attributes("style");
+  //   console.log(styles);
+  //   expect(styles).toBe("background-color: rgb(153, 153, 153);");
   // });
+
+  it("should handle 'click' event to toggle", async () => {
+    const wrapper = mount(Toggle);
+    const component = wrapper.find("[data-test=toggle]");
+    await component.trigger("click");
+
+    // Event trigger
+    wrapper.emitted("update:modelValue");
+    const emitted = wrapper.emitted<boolean[]>("update:modelValue");
+    expect(emitted).not.toBeUndefined();
+    expect(emitted?.[0][0]).toBe(true);
+
+    // Toggle state updated
+    console.log(component.attributes("data-state"));
+    // expect(component.attributes("data-state")).toEqual("checked");
+  });
 });
