883f33de73c351bdb2e925caa1d7e3248e3d3515
Yashu Mittal <mittalyashu77@gmail.com>
2025-08-15 01:19:00 +0530

    update

--- packages/theme/src/components/ui/input/Toggle.test.ts
+++ packages/theme/src/components/ui/input/Toggle.test.ts
@@ -45,6 +45,7 @@ describe("toggle", () => {
 
     // Toggle state updated
     console.log(component.attributes("data-state"));
+    // TODO: should have to toggle state 'checked'
     // expect(component.attributes("data-state")).toEqual("checked");
   });
 });
