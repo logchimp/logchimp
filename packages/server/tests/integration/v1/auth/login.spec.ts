0a9d01b4835e913b67b0d8f8b730066e10a05950
Vedant005 <vedantkanekar909@gmail.com>
2025-08-18 23:39:26 +0530

    feat(test) : added EMAIL_DOMAIN_BLACKLISTED test case in login integration test

--- packages/server/tests/integration/v1/auth/login.spec.ts
+++ packages/server/tests/integration/v1/auth/login.spec.ts
@@ -7,6 +7,17 @@ import { verifyToken } from "../../../../src/services/token.service";
 import { createUser } from "../../../utils/seed/user";
 
 describe("POST /api/v1/auth/login", () => {
+  it("should throw EMAIL_DOMAIN_BLACKLISTED", async () => {
+    const response = await supertest(app).post("/api/v1/auth/login").send({
+      email: "test@test.com",
+      password: "password",
+    });
+
+    expect(response.headers["content-type"]).toContain("application/json");
+    expect(response.status).toBe(403);
+    expect(response.body.code).toBe("EMAIL_DOMAIN_BLACKLISTED");
+  });
+
   it('should throw error "EMAIL_INVALID"', async () => {
     const response = await supertest(app).post("/api/v1/auth/login");
 
