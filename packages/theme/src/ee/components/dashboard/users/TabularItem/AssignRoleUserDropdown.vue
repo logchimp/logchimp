0dce0eebc9d40d6c81a60eff67eba2ae2c018070
Yashu Mittal <mittalyashu77@gmail.com>
2025-08-21 23:49:47 +0530

    feat(theme): disable `AssignRoleUserDropdown` button with permission checks

--- packages/theme/src/ee/components/dashboard/users/TabularItem/AssignRoleUserDropdown.vue
+++ packages/theme/src/ee/components/dashboard/users/TabularItem/AssignRoleUserDropdown.vue
@@ -2,10 +2,12 @@
   <DropdownV2 :modal="true">
     <template #trigger>
       <DropdownMenuTrigger
+        v-show="!disabled"
         :class="[
           'group-hover:block group-focus:block data-[state=open]:block',
-          'hidden bg-neutral-300 rounded-md p-1 cursor-pointer'
+          'hidden bg-neutral-300 rounded-md p-1 cursor-pointer',
         ]"
+        :disabled="disabled"
       >
         <PlusIcon aria-hidden="true" class="stroke-neutral-700 size-5"/>
         <span class="sr-only">Add role</span>
@@ -24,15 +26,30 @@
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent } from "vue";
+import { computed, defineAsyncComponent } from "vue";
 import { PlusIcon } from "lucide-vue";
 import { DropdownMenuTrigger } from "reka-ui";
+import type { TPermission } from "@logchimp/types";
 
 import DropdownV2 from "../../../../../components/ui/DropdownV2/Dropdown.vue";
 import DropdownV2Content from "../../../../../components/ui/DropdownV2/DropdownContent.vue";
+import { useUserStore } from "../../../../../store/user";
 const AssignRoleUserDropdownContent = defineAsyncComponent(
   () => import("./AssignRoleUserDropdownContent.vue"),
 );
+
+const userStore = useUserStore();
+
+const REQUIRED_PERMISSIONS: TPermission[] = ["role:assign", "role:unassign"];
+const disabled = computed(() => {
+  if (!userStore.getUserId) return false;
+
+  const hasRequiredPermission = REQUIRED_PERMISSIONS.some((permission) =>
+    userStore.permissions.includes(permission),
+  );
+
+  return !hasRequiredPermission;
+});
 
 defineOptions({
   name: "AssignRoleUserDropdown",
