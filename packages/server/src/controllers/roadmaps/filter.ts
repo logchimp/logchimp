25a60455961b863ad8ade701df96aab15c13b562
Vedant005 <vedantkanekar909@gmail.com>
2025-08-26 17:44:12 +0530

    refactor: imported computePermissions function form authenticate middleware and used it for extracting authorization headers

--- packages/server/src/controllers/roadmaps/filter.ts
+++ packages/server/src/controllers/roadmaps/filter.ts
@@ -9,7 +9,7 @@ import database from "../../database";
 import logger from "../../utils/logger";
 import error from "../../errorResponse.json";
 import { getUserFromRequest } from "../../utils/getUserFromRequest";
-import { computePermissions } from "../../utils/computePermissions";
+import { computePermissions } from "../../middlewares/authenticate";
 import { GET_ROADMAPS_FILTER_COUNT } from "../../constants";
 
 const querySchema = z.object({
@@ -27,7 +27,8 @@ export async function filter(req: Request, res: Response<ResponseBody>) {
   try {
     const { first, after } = querySchema.parse(req.query);
 
-    const decoded = getUserFromRequest(req);
+    const decoded = getUserFromRequest(req.headers.authorization);
+
     //@ts-ignore
     const userId = decoded?.userId;
     let withPermissions = true;
