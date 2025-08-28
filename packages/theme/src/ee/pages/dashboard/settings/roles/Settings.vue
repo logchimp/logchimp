a1c31ed1cef5be5a9d643249df1b8f06f7eb757c
Vedant005 <vedantkanekar909@gmail.com>
2025-08-28 14:35:27 +0530

    feat: show alert in roles page when creating role with role: assign permission

--- packages/theme/src/ee/pages/dashboard/settings/roles/Settings.vue
+++ packages/theme/src/ee/pages/dashboard/settings/roles/Settings.vue
@@ -127,6 +127,13 @@
 
     <div class="form-section">
       <p class="form-section-title">Roles permissions</p>
+      <alert 
+       v-if="permissions.role.assign" 
+       title="Alert: Critical Security Issue" 
+       description="The `role:assign` permission can cause critical security and access issues if misused." 
+       type="error" 
+       class="mb-6"
+      />
       <div class="form-columns">
         <div class="form-column">
           <toggle-item v-model="permissions.role.read" label="Read" />
@@ -177,6 +184,7 @@ import Breadcrumbs from "../../../../../components/Breadcrumbs.vue";
 import BreadcrumbDivider from "../../../../../components/ui/breadcrumbs/BreadcrumbDivider.vue";
 import BreadcrumbItem from "../../../../../components/ui/breadcrumbs/BreadcrumbItem.vue";
 import DashboardPageHeader from "../../../../../components/dashboard/PageHeader.vue";
+import Alert from "../../../../../components/ui/Alert/Alert.vue";
 
 const { permissions: userPermissions } = useUserStore();
 const dashboardRoles = useDashboardRoles();
