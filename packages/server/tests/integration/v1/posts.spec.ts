03d9869a632af455888f38e419a699e13bebe40e
Daniel Idem <dinirichard@hotmail.com>
2025-08-29 15:17:57 +0100

    Added test for Posts Slug (/posts/slug)

--- packages/server/tests/integration/v1/posts.spec.ts
+++ packages/server/tests/integration/v1/posts.spec.ts
@@ -4,7 +4,11 @@ import { v4 as uuid } from "uuid";
 import { faker } from "@faker-js/faker";
 import app from "../../../src/app";
 import database from "../../../src/database";
-import { board as generateBoards } from "../../utils/generators";
+import {
+  post as generatePost,
+  board as generateBoards,
+  roadmap as generateRoadmap
+} from "../../utils/generators";
 import { createUser } from "../../utils/seed/user";
 import { cleanDb } from "../../utils/db";
 
@@ -107,6 +111,9 @@ describe("POST /api/v1/posts", () => {
   it('should throw error "POST_TITLE_MISSING"', async () => {
     const board = generateBoards();
     await database.insert(board).into("boards");
+
+    const roadmap = generateRoadmap();
+    await database.insert(roadmap).into("roadmaps");
 
     const { user: authUser } = await createUser({
       isVerified: true,
@@ -235,5 +242,64 @@ describe("POST /api/v1/posts", () => {
 
     expect(response.status).toBe(204);
     expect(response.body.code).toBeUndefined();
+  });
+});
+
+describe("POST /api/v1/posts/slug", () => {
+  afterEach(async () => {
+    await cleanDb();
+  });
+
+  it('should throw error "POST_NOT_FOUND"', async () => {
+    await cleanDb();
+
+    const response = await supertest(app)
+      .post("/api/v1/posts/slug")
+      .send(
+        {
+          slug: "dolores-ipsa-mKTAvagnq3xaZYaag2pU",
+          userId: ""
+        }
+      );
+
+    expect(response.headers["content-type"]).toContain("application/json");
+    expect(response.status).toBe(404);
+    expect(response.body.code).toEqual("POST_NOT_FOUND");
+  });
+
+  it('should get post with matching slug', async () => {
+    const board = generateBoards();
+    await database.insert(board).into("boards");
+
+    const roadmap = generateRoadmap();
+    await database.insert(roadmap).into("roadmaps");
+
+    const { user: authUser } = await createUser({
+      isVerified: true,
+    });
+
+    const post = generatePost({
+      user_id: authUser.userId,
+      board_id: board.boardId,
+      roadmap_id: roadmap.id,
+    });
+
+    await database.insert(post).into("posts");
+
+    const response = await supertest(app)
+      .post("/api/v1/posts/slug")
+      .send(
+        {
+          slug: post.slug,
+          userId: ""
+        }
+      );
+
+    expect(response.headers["content-type"]).toContain("application/json");
+    expect(response.status).toBe(200);
+    expect(response.body.post.slug).toEqual(post.slug);
+    expect(response.body.post.board.boardId).toEqual(board.boardId);
+    expect(response.body.post.roadmap.id).toEqual(roadmap.id);
+    expect(response.body.post.author.userId).toEqual(authUser.userId);
   });
 });
