62e377d67f4e30ab36d638cef874b7241ec2f2a0
Vedant Kanekar <111733870+Vedant005@users.noreply.github.com>
2025-08-31 19:40:16 +0530

    feat: New utility function to create role with permissions for tests (#1112)
    * feat: add new utility function for tests to create role with permissions
    
    * feat: used new createRoleWithPermissions utility function
    
    * refactor: removed unnecessary comments
    
    * refactor: arranged createRoleWithPermissions input according to priority and created role object with roleId and roleName properties
    
    * refactor: used proper inputs with changes in createRoleWithPermissions function changes
    
    * refactor: used createRoleWithPermissions in a test case to avoid duplicate code
    
    * refactor: input fields arranged in string
    
    * refactor: input fields aranged in string
    
    * refactor: used TPermission from logchimp types and truncate roleName and description to match db max length
    
    * refactor: used proper permission inputs for board:destroy test case
    
    * refactor: used appropriate name for role:read permission

--- packages/server/tests/integration/v1/boards.spec.ts
+++ packages/server/tests/integration/v1/boards.spec.ts
@@ -1,6 +1,5 @@
 import { describe, it, expect } from "vitest";
 import supertest from "supertest";
-import { v4 as uuid } from "uuid";
 import { faker } from "@faker-js/faker";
 
 import app from "../../../src/app";
@@ -8,6 +7,7 @@ import database from "../../../src/database";
 import { board as generateBoards } from "../../utils/generators";
 import { createUser } from "../../utils/seed/user";
 import { cleanDb } from "../../utils/db";
+import { createRoleWithPermissions } from "../../utils/createRoleWithPermissions";
 
 // Get all boards
 describe("GET /api/v1/boards", () => {
@@ -65,7 +65,6 @@ describe("GET /boards/search/:name", () => {
 
   it("should throw error not having 'board:read' permission", async () => {
     const { user: authUser } = await createUser();
-
     const response = await supertest(app)
       .get("/api/v1/boards/search/name")
       .set("Authorization", `Bearer ${authUser.authToken}`);
@@ -79,46 +78,9 @@ describe("GET /boards/search/:name", () => {
     const { user: authUser } = await createUser();
 
     // assign "board:read" permission to user
-    const newRoleId = uuid();
-
-    await database.transaction(async (trx) => {
-      await trx
-        .insert({
-          id: newRoleId,
-          name: "board:read",
-          description: "this role has 'board:read' permission",
-        })
-        .into("roles");
-
-      // find "board:read" permission
-      const findPermission = await trx
-        .select()
-        .from("permissions")
-        .where({
-          type: "board",
-          action: "read",
-        })
-        .first();
-
-      // assign 'board:read' permission to newly created role
-      await trx
-        .insert({
-          id: uuid(),
-          role_id: newRoleId,
-          permission_id: findPermission.id,
-        })
-        .into("permissions_roles");
-
-      // assign the role to newly created user
-      await trx
-        .insert({
-          id: uuid(),
-          role_id: newRoleId,
-          user_id: authUser.userId,
-        })
-        .into("roles_users");
+    await createRoleWithPermissions(authUser.userId, ["board:read"], {
+      roleName: "Board Reader",
     });
-
     const response = await supertest(app)
       .get("/api/v1/boards/search/name")
       .set("Authorization", `Bearer ${authUser.authToken}`);
@@ -193,44 +155,8 @@ describe("DELETE /api/v1/boards", () => {
     const { user: authUser } = await createUser();
 
     // assign "board:destroy" permission to user
-    const newRoleId = uuid();
-
-    await database.transaction(async (trx) => {
-      await trx
-        .insert({
-          id: newRoleId,
-          name: "board:destroy",
-          description: "this role has 'board:destroy' permission",
-        })
-        .into("roles");
-
-      // find "board:destroy" permission
-      const findPermission = await trx
-        .select()
-        .from("permissions")
-        .where({
-          type: "board",
-          action: "destroy",
-        })
-        .first();
-
-      // assign 'board:destroy' permission to newly created role
-      await trx
-        .insert({
-          id: uuid(),
-          role_id: newRoleId,
-          permission_id: findPermission.id,
-        })
-        .into("permissions_roles");
-
-      // assign the role to newly created user
-      await trx
-        .insert({
-          id: uuid(),
-          role_id: newRoleId,
-          user_id: authUser.userId,
-        })
-        .into("roles_users");
+    await createRoleWithPermissions(authUser.userId, ["board:destroy"], {
+      roleName: "Board destroyer",
     });
 
     const response = await supertest(app)
