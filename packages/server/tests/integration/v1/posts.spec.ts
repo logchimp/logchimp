a7c7fe8c77ec475fdb26528146723af188209dd1
Daniel Idem <dinirichard@hotmail.com>
2025-09-01 23:58:01 +0100

    format changes

--- packages/server/tests/integration/v1/posts.spec.ts
+++ packages/server/tests/integration/v1/posts.spec.ts
@@ -1,44 +1,44 @@
-import { describe, it, expect } from "vitest"
-import supertest from "supertest"
-import { faker } from "@faker-js/faker"
-import app from "../../../src/app"
-import database from "../../../src/database"
-import { board as generateBoards } from "../../utils/generators"
-import { createUser } from "../../utils/seed/user"
-import { createRoleWithPermissions } from "../../utils/createRoleWithPermissions"
+import { describe, it, expect } from "vitest";
+import supertest from "supertest";
+import { faker } from "@faker-js/faker";
+import app from "../../../src/app";
+import database from "../../../src/database";
+import { board as generateBoards } from "../../utils/generators";
+import { createUser } from "../../utils/seed/user";
+import { createRoleWithPermissions } from "../../utils/createRoleWithPermissions";
 
 // Create new posts
 describe("CREATE POST /api/v1/posts", () => {
   it('should throw error "INVALID_AUTH_HEADER"', async () => {
-    const response = await supertest(app).post("/api/v1/posts")
+    const response = await supertest(app).post("/api/v1/posts");
 
-    expect(response.headers["content-type"]).toContain("application/json")
-    expect(response.status).toBe(400)
-    expect(response.body.code).toEqual("INVALID_AUTH_HEADER")
-  })
+    expect(response.headers["content-type"]).toContain("application/json");
+    expect(response.status).toBe(400);
+    expect(response.body.code).toEqual("INVALID_AUTH_HEADER");
+  });
 
   it('should throw error "NOT_ENOUGH_PERMISSION"', async () => {
     const { user: authUser } = await createUser({
       isVerified: true,
-    })
+    });
 
     const response = await supertest(app)
       .post("/api/v1/boards")
-      .set("Authorization", `Bearer ${authUser.authToken}`)
+      .set("Authorization", `Bearer ${authUser.authToken}`);
 
-    expect(response.headers["content-type"]).toContain("application/json")
-    expect(response.status).toBe(403)
-    expect(response.body.code).toEqual("NOT_ENOUGH_PERMISSION")
-  })
+    expect(response.headers["content-type"]).toContain("application/json");
+    expect(response.status).toBe(403);
+    expect(response.body.code).toEqual("NOT_ENOUGH_PERMISSION");
+  });
 
   it('should throw error "BOARD_ID_MISSING"', async () => {
     const { user: authUser } = await createUser({
       isVerified: true,
-    })
+    });
 
     await createRoleWithPermissions(authUser.userId, ["post:create"], {
       roleName: "Post Creator",
-    })
+    });
 
     const response = await supertest(app)
       .post(`/api/v1/posts/`)
@@ -47,10 +47,10 @@ describe("CREATE POST /api/v1/posts", () => {
         title: faker.food.dish,
         contentMarkdown: faker.food.description,
         userId: authUser.userId,
-      })
+      });
 
-    expect(response.headers["content-type"]).toContain("application/json")
-    expect(response.status).toBe(400)
+    expect(response.headers["content-type"]).toContain("application/json");
+    expect(response.status).toBe(400);
     expect(response.body.errors).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
@@ -58,20 +58,20 @@ describe("CREATE POST /api/v1/posts", () => {
           code: "BOARD_ID_MISSING",
         }),
       ]),
-    )
-  })
+    );
+  });
 
   it('should throw error "POST_TITLE_MISSING"', async () => {
-    const board = generateBoards()
-    await database.insert(board).into("boards")
+    const board = generateBoards();
+    await database.insert(board).into("boards");
 
     const { user: authUser } = await createUser({
       isVerified: true,
-    })
+    });
 
     await createRoleWithPermissions(authUser.userId, ["post:create"], {
       roleName: "Post Creator",
-    })
+    });
 
     const response = await supertest(app)
       .post(`/api/v1/posts/`)
@@ -80,10 +80,10 @@ describe("CREATE POST /api/v1/posts", () => {
         contentMarkdown: faker.food.description,
         userId: authUser.userId,
         boardId: board.boardId,
-      })
+      });
 
-    expect(response.headers["content-type"]).toContain("application/json")
-    expect(response.status).toBe(400)
+    expect(response.headers["content-type"]).toContain("application/json");
+    expect(response.status).toBe(400);
     expect(response.body.errors).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
@@ -91,20 +91,20 @@ describe("CREATE POST /api/v1/posts", () => {
           code: "POST_TITLE_MISSING",
         }),
       ]),
-    )
-  })
+    );
+  });
 
   it("should create a post", async () => {
-    const board = generateBoards()
-    await database.insert(board).into("boards")
+    const board = generateBoards();
+    await database.insert(board).into("boards");
 
     const { user: authUser } = await createUser({
       isVerified: true,
-    })
+    });
 
     await createRoleWithPermissions(authUser.userId, ["post:create"], {
       roleName: "Post Creator",
-    })
+    });
 
     const response = await supertest(app)
       .post(`/api/v1/posts/`)
@@ -114,9 +114,9 @@ describe("CREATE POST /api/v1/posts", () => {
         contentMarkdown: faker.food.description(),
         userId: authUser.userId,
         boardId: board.boardId,
-      })
+      });
 
-    expect(response.status).toBe(201)
-    expect(response.body.code).toBeUndefined()
-  })
-})
+    expect(response.status).toBe(201);
+    expect(response.body.code).toBeUndefined();
+  });
+});
