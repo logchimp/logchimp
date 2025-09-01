c1d6c61688b335f3db27451cf6d02b6575a96068
Daniel Idem <dinirichard@hotmail.com>
2025-09-01 21:55:08 +0100

    filter tests complete

---
+++ packages/server/vitest.setup.integration.ts
@@ -0,0 +1,30 @@
+import { afterAll, beforeAll } from 'vitest';
+import database from "./src/database";
+
+export interface TableInserts {
+  tableName: string;
+  columnName: string;
+  uniqueValue: any;
+}
+
+declare global {
+  var tableInserts: TableInserts[];
+}
+
+beforeAll(async () => {
+  globalThis.tableInserts = [];
+});
+
+afterAll(async () => {
+  console.log("totalTables: ", globalThis.tableInserts.length);
+
+    while (globalThis.tableInserts.length > 0) {
+      const inserts = globalThis.tableInserts.shift();
+      if (inserts) {
+        await database
+        .table(inserts.tableName)
+        .where(inserts.columnName, inserts.uniqueValue)
+        .del();
+      }
+    }
+});
