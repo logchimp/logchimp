5ca0c7ee90e6c5379cadbd083f1ccc550473b49f
Yashu Mittal <mittalyashu77@gmail.com>
2025-09-01 13:14:30 +0530

    feat(theme/roles): create `DashboardRolesTabularItem.vue` using DropdownV2 UI component

--- packages/theme/src/ee/pages/dashboard/settings/roles/Index.vue
+++ packages/theme/src/ee/pages/dashboard/settings/roles/Index.vue
@@ -24,42 +24,7 @@
       </div>
       <div class="table-body">
         <div v-for="role in dashboardRoles.roles" :key="role.id" class="table-row">
-          <div class="table-data flex-1">
-            {{ role.name }}
-          </div>
-          <div class="table-icon-group boards-table-icons">
-            <router-link
-              :to="`/dashboard/settings/roles/${role.id}/settings`"
-              class="table-data table-data-icon"
-            >
-              <settings-icon />
-            </router-link>
-            <dropdown-wrapper v-if="settings.developer_mode">
-              <template #toggle>
-                <div
-                  class="
-                    table-data table-data-icon
-                    boards-table-icon-settings
-                    dropdown-menu-icon
-                  "
-                >
-                  <more-icon />
-                </div>
-              </template>
-              <template #default="dropdown">
-                <dropdown v-if="dropdown.active">
-                  <dropdown-item
-                    @click="useCopyText(role.id)"
-                  >
-                    <template #icon>
-                      <copy-icon />
-                    </template>
-                    Copy ID
-                  </dropdown-item>
-                </dropdown>
-              </template>
-            </dropdown-wrapper>
-          </div>
+          <DashboardRolesTabularItem :role="role" />
         </div>
 
         <infinite-scroll :on-infinite="dashboardRoles.fetchRoles" :state="dashboardRoles.state" />
@@ -72,31 +37,21 @@
 // packages
 import { computed, ref } from "vue";
 import { useHead } from "@vueuse/head";
-import {
-  Settings as SettingsIcon,
-  Clipboard as CopyIcon,
-  MoreHorizontal as MoreIcon,
-} from "lucide-vue";
 
 // modules
 import { router } from "../../../../../router";
-import { useSettingStore } from "../../../../../store/settings";
 import { useUserStore } from "../../../../../store/user";
 import { createRole } from "../../../../modules/roles";
-import { useCopyText } from "../../../../../hooks";
 import { useDashboardRoles } from "../../../../../store/dashboard/roles.ts";
 
 // components
 import InfiniteScroll from "../../../../../components/ui/InfiniteScroll.vue";
 import Button from "../../../../../components/ui/Button.vue";
-import DropdownWrapper from "../../../../../components/ui/dropdown/DropdownWrapper.vue";
-import Dropdown from "../../../../../components/ui/dropdown/Dropdown.vue";
-import DropdownItem from "../../../../../components/ui/dropdown/DropdownItem.vue";
 import Breadcrumbs from "../../../../../components/Breadcrumbs.vue";
 import DashboardPageHeader from "../../../../../components/dashboard/PageHeader.vue";
 import BreadcrumbItem from "../../../../../components/ui/breadcrumbs/BreadcrumbItem.vue";
+import DashboardRolesTabularItem from "../../../../components/dashboard/roles/TabularItem.vue";
 
-const { settings } = useSettingStore();
 const { permissions } = useUserStore();
 const dashboardRoles = useDashboardRoles();
 
