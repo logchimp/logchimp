4d299cf02105b11271379040e322fc0899273bf8
Yashu Mittal <mittalyashu77@gmail.com>
2025-09-04 19:05:22 +0530

    fix(SiteSettings): disable `/settings/update-logo` API-endpoint

--- packages/server/src/routes/v1/settings.ts
+++ packages/server/src/routes/v1/settings.ts
@@ -1,25 +1,6 @@
 // modules
-import path from "path";
 import express from "express";
 const router = express.Router();
-import multer from "multer";
-
-const storage = multer.diskStorage({
-  destination: (_req, _file, cb) => {
-    cb(null, path.resolve(__dirname, "../../../content/images"));
-  },
-  filename: (_req, file, cb) => {
-    const fileExtension =
-      file.originalname.split(".")[file.originalname.split(".").length - 1];
-    const fileName = Date.now().toString(32);
-
-    cb(null, `${fileName}.${fileExtension}`);
-  },
-});
-
-const upload = multer({
-  storage,
-});
 
 // controller
 import * as settings from "../../controllers/settings";
@@ -29,12 +10,12 @@ import { authRequired } from "../../middlewares/auth";
 
 router.get("/settings/site", settings.siteSettings);
 router.patch("/settings/site", authRequired, settings.update);
-router.post(
-  "/settings/update-logo",
-  authRequired,
-  upload.single("logo", 1),
-  settings.updateLogo,
-);
+// NOTE: Temporarily disable API-endpoint due to no file storage system implemented.
+// router.post(
+//   "/settings/update-logo",
+//   authRequired,
+//   settings.updateLogo,
+// );
 router.get("/settings/labs", settings.getLabs);
 router.patch("/settings/labs", authRequired, settings.updateLabs);
 
