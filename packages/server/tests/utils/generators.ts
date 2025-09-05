6e84346a13a4a2c1e6050a480d3c533730fac535
Daniel Idem <dinirichard@hotmail.com>
2025-09-05 13:09:29 +0100

    tests for filtering boards

--- packages/server/tests/utils/generators.ts
+++ packages/server/tests/utils/generators.ts
@@ -111,10 +111,21 @@ interface BoardArgs {
   color: string;
 }
 
+export interface BoardInsertRecord {
+  boardId: string;
+  name: string;
+  url: string;
+  color: string;
+  display: boolean;
+  view_voters: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
 const board = async (board?: Partial<BoardArgs>, insertToDb = false) => {
   const name = board?.name || faker.commerce.productName();
 
-  const obj = {
+  const obj: BoardInsertRecord = {
     boardId: uuid(),
     name,
     url: board?.url || `${sanitiseURL(name)}-${nanoid(10)}`,
