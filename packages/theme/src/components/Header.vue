554cad9c69d964e753bf18dcd22c6751135afa28
Vedant005 <vedantkanekar909@gmail.com>
2025-09-06 12:20:28 +0530

    feat(i18n): add language selection and localization support in header

--- packages/theme/src/components/Header.vue
+++ packages/theme/src/components/Header.vue
@@ -8,10 +8,21 @@
           text-color="white"
         />
 
+        <select
+          v-model="$i18n.locale"
+          class="ml-4 px-2 py-1 rounded bg-white text-black text-sm"
+        >
+          <option value="en">English</option>
+          <option value="fr">Français</option>
+          <option value="hi">हिन्दी</option>
+        </select>
+
         <nav>
           <HeaderAuthDropdown v-if="userStore.user.userId" />
           <div v-else class="flex items-center">
-            <Button type="primary" href="/login" size="small">Login</Button>
+            <Button type="primary" href="/login" size="small">
+              {{ $t("header.login") }}
+            </Button>
             <Button
               v-if="settingsStore.get.allowSignup"
               type="primary"
@@ -19,7 +30,7 @@
               href="/join"
               size="small"
             >
-              Create an account
+              {{ t("header.createAccount") }}
             </Button>
           </div>
         </nav>
@@ -33,6 +44,7 @@
 <script setup lang="ts">
 import { useSettingStore } from "../store/settings";
 import { useUserStore } from "../store/user";
+import { useI18n } from "vue-i18n";
 
 // components
 import Navbar from "./Navbar.vue";
@@ -42,4 +54,5 @@ import HeaderAuthDropdown from "./HeaderAuthDropdown.vue";
 
 const settingsStore = useSettingStore();
 const userStore = useUserStore();
+const { t } = useI18n();
 </script>
