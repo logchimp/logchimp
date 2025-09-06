cdd535a9c76049389c0189706296754301d6c19b
Yashu Mittal <mittalyashu77@gmail.com>
2025-09-06 13:42:50 +0530

    feat(e2e): update playwright.config.ts with projects based on `CI_BRANCH_NAME`

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -26,49 +26,71 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
+  projects:
+    process.env.CI_BRANCH_NAME === "master"
+      ? // CI
+        [
+          {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] },
+          },
+          // Test against branded browsers
+          {
+            name: "Microsoft Edge",
+            use: { ...devices["Desktop Edge"], channel: "msedge" },
+          },
+          {
+            name: "firefox",
+            use: { ...devices["Desktop Firefox"] },
+          },
+          {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+          },
+          // Mobile
+          {
+            name: "Mobile Chrome",
+            use: { ...devices["Pixel 5"] },
+          },
+          {
+            name: "Mobile Safari",
+            use: { ...devices["iPhone 13 Pro"] },
+          },
+        ]
+      : // Dev
+        [
+          {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] },
+          },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+          {
+            name: "firefox",
+            use: { ...devices["Desktop Firefox"] },
+          },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+          {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+          },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+          /* Test against mobile viewports. */
+          // {
+          //   name: 'Mobile Chrome',
+          //   use: { ...devices['Pixel 5'] },
+          // },
+          // {
+          //   name: 'Mobile Safari',
+          //   use: { ...devices['iPhone 12'] },
+          // },
+        ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
