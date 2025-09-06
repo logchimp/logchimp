67e2d09c4be97abb699240af47d27168f9d1a2e2
Vedant005 <vedantkanekar909@gmail.com>
2025-09-06 12:19:13 +0530

    feat(i18n): integrate vue-i18n for localization support

--- packages/theme/src/main.ts
+++ packages/theme/src/main.ts
@@ -1,18 +1,35 @@
 import { createApp } from "vue";
 import { createHead } from "@vueuse/head";
 import { createPinia } from "pinia";
+import { createI18n } from "vue-i18n";
 
 import App from "./App.vue";
 import { router } from "./router";
 
 import "./styles/main.sass";
 
+//locales
+import en from "./locales/en.json";
+import fr from "./locales/fr.json";
+import hi from "./locales/hi.json";
+
 const app = createApp(App);
 const store = createPinia();
 const head = createHead();
+const i18n = createI18n({
+  legacy: false,
+  locale: "en",
+  fallbackLocale: "en",
+  messages: {
+    en,
+    fr,
+    hi,
+  },
+});
 
 app.use(router);
 app.use(store);
 app.use(head);
+app.use(i18n);
 
 app.mount("body");
