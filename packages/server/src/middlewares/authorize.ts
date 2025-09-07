e50dadd3612fda6f376dd22926b1f802b215cb53
Yashu Mittal <mittalyashu77@gmail.com>
2025-09-07 22:41:32 +0530

    refactor(server/authorize): simplify `auth/authorize` middleware logic

--- packages/server/src/middlewares/authorize.ts
+++ packages/server/src/middlewares/authorize.ts
@@ -3,36 +3,32 @@ import error from "../errorResponse.json";
 
 const authorize = (req: Request, res: Response, next: NextFunction) => {
   // @ts-expect-error
-  const hasUser = req.user?.userId;
-
-  if (hasUser) {
-    // user is blocked
-    // @ts-expect-error
-    const isBlocked = req.user.isBlocked;
-    if (isBlocked) {
-      return res.status(403).send({
-        message: error.middleware.user.userBlocked,
-        code: "USER_BLOCK",
-      });
-    }
-
-    // @ts-expect-error
-    const hasPermissions = req.user.permissions.length > 0;
-    if (hasPermissions) {
-      return next();
-    } else {
-      // In case user doesn't have any permissions
-      return res.status(403).send({
-        message: error.middleware.auth.accessDenied,
-        code: "ACCESS_DENIED",
-      });
-    }
-  } else {
+  const user = req.user;
+  if (!user || !user.userId) {
     return res.status(401).send({
       message: error.middleware.auth.authorizationFailed,
       code: "AUTHORIZATION_FAILED",
     });
   }
+
+  // user is blocked
+  if (user.isBlocked) {
+    return res.status(403).send({
+      message: error.middleware.user.userBlocked,
+      code: "USER_BLOCK",
+    });
+  }
+
+  const hasPermissions =
+    Array.isArray(user.permissions) && user.permissions.length > 0;
+  if (!hasPermissions) {
+    return res.status(403).send({
+      message: error.middleware.auth.accessDenied,
+      code: "ACCESS_DENIED",
+    });
+  }
+
+  return next();
 };
 
 export { authorize };
