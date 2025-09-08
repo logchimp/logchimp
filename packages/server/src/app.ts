9b1b91bc0f691d0d10e690071ecd2727a9bfec19
Yashu Mittal <mittalyashu77@gmail.com>
2025-09-09 00:25:47 +0530

    fix: globally handle express decode URI errors

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,6 +1,7 @@
 // Set the default environment to be `development`
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+import type { Request, Response, NextFunction } from "express";
 import express from "express";
 import cors from "cors";
 
@@ -27,5 +28,16 @@ app.use(cors());
 
 // import all routes
 app.use(routes);
+
+app.use((err, _req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof URIError) {
+    res.status(404).send({
+      message: "bad URL",
+      code: "DECODE_URI_ERROR",
+    });
+    return;
+  }
+  next();
+});
 
 export default app;
