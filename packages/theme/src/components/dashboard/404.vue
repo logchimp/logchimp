b2713da9059f744f3a98edc35fd1d58f8861fefd
Yashu Mittal <mittalyashu77@gmail.com>
2025-09-14 19:18:49 +0530

    feat(ui/dashboard): display loading & error state in DashboardPostView page

---
+++ packages/theme/src/components/dashboard/404.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { FileSearchIcon } from "lucide-vue";
+
+defineOptions({
+  name: "Dashboard404",
+});
+</script>
+
+<template>
+  <div class="h-[inherit] flex flex-col items-center justify-center flex-grow-1 gap-y-5">
+    <FileSearchIcon aria-hidden="true" class="stroke-neutral-700 stroke-[1px] size-16" />
+    <div class="flex flex-col items-center justify-center gap-y-2">
+      <div  class="font-medium">
+        <slot>
+          Not found
+        </slot>
+      </div>
+      <p class="text-xs text-neutral-800">
+        We could not find the page you were looking for.
+      </p>
+    </div>
+  </div>
+</template>
--- packages/theme/src/pages/dashboard/posts/_slug/Index.vue
+++ packages/theme/src/pages/dashboard/posts/_slug/Index.vue
@@ -1,115 +1,32 @@
 <template>
-  <DashboardPageHeader>
-    <template v-slot:left>
-      <Breadcrumbs>
-        <BreadcrumbItem to="/dashboard/posts">
-          Posts
-        </BreadcrumbItem>
-
-        <!-- Show divider & title once data loaded -->
-        <template v-if="post.title">
-          <BreadcrumbDivider />
-          <BreadcrumbItem>
-            {{ post.title }}
-          </BreadcrumbItem>
-        </template>
-      </Breadcrumbs>
-    </template>
-
-    <Button
-      type="primary"
-      :loading="loading.updatePostButton"
-      :disabled="updatePostPermissionDisabled"
-      @click="updatePostHandler"
-    >
-      Save
-    </Button>
-  </DashboardPageHeader>
-
-  <div class="px-3 lg:px-6">
-    <div class="form-section">
-      <div class="form-columns">
-        <div class="form-column">
-          <l-text
-            v-model="post.title"
-            label="Title"
-            placeholder="Name of the feature"
-          />
-
-          <l-textarea
-            :model-value="post.contentMarkdown ?? undefined"
-            @update:model-value="(value) => post.contentMarkdown = value ?? null"
-            label="Description"
-            rows="4"
-            placeholder="What would you use it for?"
-          />
-        </div>
-
-        <div class="form-column">
-          <div>
-            <p class="input-field-label">
-              Preview
-            </p>
-            <div class="card">
-              <post-item v-if="!loading.post" :post="post" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="form-section">
-      <h6 class="form-section-title">
-        Other
-      </h6>
-      <div class="form-columns">
-        <div class="form-column">
-          <SearchBoardDropdown @selected="selectBoard" />
-        </div>
-
-        <div class="form-column">
-          <SearchRoadmapDropdown @selected="selectRoadmap" />
-        </div>
-      </div>
-    </div>
+  <div>
+    <LoaderContainer v-if="loading" />
+    <DashboardPostEditor
+      v-else-if="post.postId && !errorCode"
+      :post="post"
+    />
+    <Dashboard404 v-else-if="errorCode === 'POST_NOT_FOUND'">
+      Post not found
+    </Dashboard404>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, reactive } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import { useHead } from "@vueuse/head";
-import type {
-  IBoardPrivate,
-  IDashboardPost,
-  IRoadmapPrivate,
-} from "@logchimp/types";
+import type { IDashboardPost } from "@logchimp/types";
 
 // modules
 import { router } from "../../../../router";
-import { getPostBySlug, updatePost } from "../../../../modules/posts";
-import { useUserStore } from "../../../../store/user";
+import { getPostBySlug } from "../../../../modules/posts";
 
 // components
-import Button from "../../../../components/ui/Button.vue";
-import LText from "../../../../components/ui/input/LText.vue";
-import LTextarea from "../../../../components/ui/input/LTextarea.vue";
-import PostItem from "../../../../components/post/PostItem.vue";
-import Breadcrumbs from "../../../../components/Breadcrumbs.vue";
-import BreadcrumbDivider from "../../../../components/ui/breadcrumbs/BreadcrumbDivider.vue";
-import BreadcrumbItem from "../../../../components/ui/breadcrumbs/BreadcrumbItem.vue";
-import DashboardPageHeader from "../../../../components/dashboard/PageHeader.vue";
-import SearchRoadmapDropdown from "../../../../ee/components/dashboard/roadmap/SearchRoadmapDropdown/Dropdown.vue";
-import SearchBoardDropdown from "../../../../ee/components/dashboard/boards/SearchBoardDropdown/Dropdown.vue";
+import Dashboard404 from "../../../../components/dashboard/404.vue";
+import DashboardPostEditor from "../../../../components/dashboard/posts/PostEditor/index.vue";
+import LoaderContainer from "../../../../components/ui/LoaderContainer.vue";
 
-const { permissions } = useUserStore();
-
-const loading = reactive<{
-  post: boolean;
-  updatePostButton: boolean;
-}>({
-  post: false,
-  updatePostButton: false,
-});
+const errorCode = ref<string | undefined>();
+const loading = ref<boolean>(false);
 const post = reactive<IDashboardPost>({
   postId: "",
   title: "",
@@ -146,63 +63,25 @@ const post = reactive<IDashboardPost>({
   },
 });
 
-const updatePostPermissionDisabled = computed(() => {
-  const checkPermission = permissions.includes("post:update");
-  return !checkPermission;
-});
-
-async function updatePostHandler() {
-  loading.updatePostButton = true;
-
-  try {
-    const response = await updatePost({
-      id: post.postId,
-      title: post.title,
-      contentMarkdown: post.contentMarkdown,
-      slugId: post.slugId,
-      userId: post.author.userId,
-      boardId: post.board ? post.board.boardId : undefined,
-      roadmapId: post.roadmap ? post.roadmap.id : undefined,
-    });
-
-    if (response.status === 200) {
-      router.push("/dashboard/posts");
-    }
-  } catch (err) {
-    console.error(err);
-  } finally {
-    loading.updatePostButton = false;
-  }
-}
-
 async function postBySlug() {
-  loading.post = true;
-  const route = router.currentRoute.value;
+  loading.value = true;
+  errorCode.value = undefined;
 
+  const route = router.currentRoute.value;
   if (route.params.slug) {
     try {
       const slug = route.params.slug.toString();
       const response = await getPostBySlug(slug);
 
       Object.assign(post, response.data.post);
-      loading.post = false;
+      loading.value = false;
     } catch (err) {
       console.error(err);
-      loading.post = false;
+      // @ts-expect-error
+      errorCode.value = err.response.data.code;
+      loading.value = false;
     }
   }
-}
-
-function selectBoard(board: IBoardPrivate) {
-  Object.assign(post, {
-    board,
-  });
-}
-
-function selectRoadmap(roadmap: IRoadmapPrivate) {
-  Object.assign(post, {
-    roadmap,
-  });
 }
 
 onMounted(() => postBySlug());
