9c26ef9e31a9605eddd6c8166eb7b1c8fd4bee51
Vedant005 <vedantkanekar909@gmail.com>
2025-09-27 13:54:33 +0530

    refactor: made path changes for docker  in postStartCommand file

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -6,13 +6,13 @@ pnpm i;
 pnpm --filter="@logchimp/types" build
 
 # Boot-up db and mail services
-docker-compose -f ./docker-compose.dev.yml \
+docker-compose -f ./docker/local/docker-compose.dev.yml \
   up -d db mail
 
 # Wait for the database to be ready
 if wait4x tcp localhost:5432 --timeout 60s; then
   echo "Database is ready, starting LogChimp API service..."
-  docker-compose -f ./docker-compose.dev.yml up -d logchimp
+  docker-compose -f ./docker/local/docker-compose.dev.yml up -d logchimp
 else
   echo "Database did not start in time, exiting."
   exit 1
