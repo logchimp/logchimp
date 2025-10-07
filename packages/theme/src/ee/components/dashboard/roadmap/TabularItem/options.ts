69786f4e6be1873d1cb622990d5df0ec4b0af91d
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2025-10-08 00:58:52 +0530

    type fix

--- packages/theme/src/ee/components/dashboard/roadmap/TabularItem/options.ts
+++ packages/theme/src/ee/components/dashboard/roadmap/TabularItem/options.ts
@@ -1,7 +1,9 @@
-import type { InjectionKey } from "vue";
+import type { InjectionKey, ComputedRef } from "vue";
 import type { IRoadmapPrivate } from "@logchimp/types";
 
 /**
  * Symbol to exchange roadmap's data
  */
-export const roadmapKey = Symbol() as InjectionKey<IRoadmapPrivate>;
+export const roadmapKey = Symbol() as InjectionKey<
+  ComputedRef<IRoadmapPrivate>
+>;
