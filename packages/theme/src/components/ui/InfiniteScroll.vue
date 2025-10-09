4b380fe7ed34e11e0224a2fe4d5f0b708ea5e97d
aaviz mehar <aavizk526@gmail.com>
2025-10-09 20:06:15 +0530

    final

--- packages/theme/src/components/ui/InfiniteScroll.vue
+++ packages/theme/src/components/ui/InfiniteScroll.vue
@@ -77,20 +77,18 @@ watch(
 );
 
 function executeInfiniteScroll() {
-  if (!props.canLoadMore || props.state === "COMPLETED" || props.state === "ERROR") return;
+  if (
+    !props.canLoadMore ||
+    props.state === "COMPLETED" ||
+    props.state === "ERROR") return;
   props.onInfinite();
 }
-
 
 useInfiniteScroll(infiniteTrigger, executeInfiniteScroll, {
   distance: props.distance,
   direction: "bottom",
-  canLoadMore: () =>
-    props.canLoadMore &&
-    props.state !== "COMPLETED" &&
-    props.state !== "ERROR",
+  canLoadMore: () => props.canLoadMore && props.state !== "COMPLETED" && props.state !== "ERROR",
 });
-
 
 onMounted(async () => {
   await nextTick();
