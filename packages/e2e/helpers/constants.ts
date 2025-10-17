96b1a134d11dd17122e9f09d047af6a4cc588d6a
Yashu Mittal <mittalyashu77@gmail.com>
2025-10-17 19:03:34 +0530

    chore(e2e): update `SITE_NAME` constant in test configuration

--- packages/e2e/helpers/constants.ts
+++ packages/e2e/helpers/constants.ts
@@ -1,3 +1,3 @@
-export const SITE_NAME = "LogChimp e2e Test Site";
+export const SITE_NAME = "LogChimp";
 export const SITE_LOGO_URL =
   "https://cdn.logchimp.codecarrot.net/logchimp_circular_logo.png";
