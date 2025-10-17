31326474ac98e965575c15be369584dfc8d80de0
Yashu Mittal <mittalyashu77@gmail.com>
2025-10-17 19:48:49 +0530

    feat(e2e): log res status in user-account.ts

--- packages/e2e/fixtures/user-account.ts
+++ packages/e2e/fixtures/user-account.ts
@@ -18,18 +18,20 @@ export const test = baseTest.extend({
         requestContext,
         baseURL,
       });
+      console.log("res status:", res.status());
       const body = (await res.json()) as IAuthSignupResponseBody;
 
       const context = await browser.newContext();
       const page = await context.newPage();
       await page.goto(baseURL);
 
-      // Store the auth token in localStorage (or use cookies/sessionStorage based on your app)
+      // Store the auth user in localStorage (or use cookies/sessionStorage based on your app)
       await page.evaluate((payload: IAuthUser) => {
         localStorage.setItem("user", JSON.stringify(payload));
       }, body.user);
 
       await context.storageState({ path: fileName });
+      await context.close();
       await requestContext.dispose();
     }
 
