d81545356d9fa2ddca49140f6c86041011b6102e
Vedant005 <vedantkanekar909@gmail.com>
2025-10-17 22:47:55 +0530

    refactor: making colordot style similar to the component

--- packages/theme/src/ee/components/roadmap/RoadmapSkeleton.vue
+++ packages/theme/src/ee/components/roadmap/RoadmapSkeleton.vue
@@ -4,7 +4,7 @@
   >
     <div v-for="i in 3" :key="i" class="flex flex-col gap-2.5 animate-pulse">
       <header class="flex items-center gap-x-1.5">
-        <div class="size-2.5 h-3 w-3 rounded-full bg-gray-300"></div>
+        <div class="size-2.5 rounded-full bg-(--color-gray-95)"></div>
         <div class="h-3 w-24 bg-gray-300 rounded"></div>
       </header>
       <div class="rounded-md bg-gray-300 p-3 flex-grow space-y-2">
