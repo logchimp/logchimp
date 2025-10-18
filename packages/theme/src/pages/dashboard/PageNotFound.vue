f072e3a7b2df19908e21cc65e552ce97617d6316
Yashu Mittal <mittalyashu77@gmail.com>
2025-10-18 18:45:21 +0530

    feat(theme): improve 404 pages

---
+++ packages/theme/src/pages/dashboard/PageNotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <DashboardPageHeader />
+
+  <div class="h-full flex items-center justify-center">
+    <h1 class="text-4xl font-bold">
+      Page not found
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DashboardPageHeader from "../../components/dashboard/PageHeader.vue";
+
+defineOptions({
+  name: "PageNotFound",
+});
+</script>
