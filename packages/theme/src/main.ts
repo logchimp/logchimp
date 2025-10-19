474a4772486b0fc7744579d4356c109f5c3ceb52
Vedant005 <vedantkanekar909@gmail.com>
2025-10-19 19:58:52 +0530

    refactor: simplify i18n setup by removing unused locale imports and localStorage handling

--- packages/theme/src/main.ts
+++ packages/theme/src/main.ts
@@ -1,38 +1,16 @@
-import { createApp, watchEffect } from "vue";
+import { createApp } from "vue";
 import { createHead } from "@vueuse/head";
 import { createPinia } from "pinia";
-import { createI18n } from "vue-i18n";
+import i18n from "./plugins/i18n";
 
 import App from "./App.vue";
 import { router } from "./router";
 
 import "./styles/main.sass";
 
-//locales
-import en from "./locales/en.json";
-import fr from "./locales/fr.json";
-import hi from "./locales/hi.json";
-
 const app = createApp(App);
 const store = createPinia();
 const head = createHead();
-
-const savedLocale = localStorage.getItem("locale") || "en";
-
-const i18n = createI18n({
-  legacy: false,
-  locale: savedLocale,
-  fallbackLocale: "en",
-  messages: {
-    en,
-    fr,
-    hi,
-  },
-});
-
-watchEffect(() => {
-  localStorage.setItem("locale", i18n.global.locale.value);
-});
 
 app.use(router);
 app.use(store);
