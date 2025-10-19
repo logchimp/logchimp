dc84e1b32b23031bd4c399f3ce3c22331559bb80
Yashu Mittal <mittalyashu77@gmail.com>
2025-10-19 19:44:48 +0530

    feat(server/routes): add licenseGuard middleware to protect EE routes

--- packages/server/src/ee/routes/v1/posts.ts
+++ packages/server/src/ee/routes/v1/posts.ts
@@ -14,6 +14,7 @@ import * as post from "../../../ee/controllers/v1/posts";
 
 // middleware
 import { authRequired } from "../../../middlewares/auth";
+import { licenseGuard } from "../../middleware/licenseGuard";
 
 // post activity
 router.get<
@@ -21,24 +22,32 @@ router.get<
   unknown,
   unknown,
   IGetPostActivityRequestQuery
->("/posts/:post_id/activity", post.activity.get);
+>(
+  "/posts/:post_id/activity",
+  // @ts-expect-error
+  licenseGuard,
+  post.activity.get,
+);
 
 // post comment
 router.post<TCreatePostCommentRequestParam>(
   "/posts/:post_id/comments",
   // @ts-expect-error
+  licenseGuard,
   authRequired,
   post.comments.create,
 );
 router.put<IUpdatePostCommentRequestParam>(
   "/posts/:post_id/comments/:comment_id",
   // @ts-expect-error
+  licenseGuard,
   authRequired,
   post.comments.update,
 );
 router.delete<TDeletePostCommentRequestParam>(
   "/posts/:post_id/comments/:comment_id",
   // @ts-expect-error
+  licenseGuard,
   authRequired,
   post.comments.destroy,
 );
