e1ace372bab27856696c03c439533b09dfb6e27a
Tarana Khanna <taranakhanna5@gmail.com>
2025-10-21 22:17:49 +0530

    feat: character limit added to title in post creation form
    apply the recommended changes

--- packages/theme/src/components/ui/input/HelperText.vue
+++ packages/theme/src/components/ui/input/HelperText.vue
@@ -1,7 +1,21 @@
 <template>
-  <div
-    class="text-sm text-neutral-700"
-  >
+  <div class="text-sm text-right" :class="textColorClass">
+
     <slot />
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({
+  isError: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const textColorClass = computed(() =>
+  props.isError ? "text-red-600" : "text-neutral-700",
+);
+</script>
