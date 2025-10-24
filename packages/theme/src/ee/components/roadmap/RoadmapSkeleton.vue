7dfd1b0f877a1a9e0234fd1691ef0d734afc91f2
Vedant Kanekar <111733870+Vedant005@users.noreply.github.com>
2025-10-24 12:11:55 +0530

    Feat: Add loading skeleton on lazy-loading roadmaps on public page  (#1330)
    * feat: loading skeleton for lazy-loading in public roadmapas page
    
    * feat: implementing loading skeleton component
    
    * refactor: making colordot style similar to the component
    
    * refactor: made roadmap name skeleton similar to the actual view
    
    * refactor: made  roadmap postcard div simialr to the atcual component
    
    * refactor: modfied height to be compatible with tailwind and used tailwind color
    
    * refactor: update loading skeleton logic for cursor pagination to show skeleton for new incoming roadmaps
    
    * fix
    
    * feat(theme/roadmaps): replace `RoadmapSkeleton` with `LoaderContainer` for improved loading state handling
    
    ---------
    
    Co-authored-by: Yashu Mittal <mittalyashu77@gmail.com>

---
+++ packages/theme/src/ee/components/roadmap/RoadmapSkeleton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="overflow-x-auto h-[500px] overflow-y-hidden grid grid-flow-col gap-x-4 md:gap-x-6 auto-cols-[minmax(22rem,24rem)]"
+  >
+    <div v-for="i in 3" :key="i" class="flex flex-col gap-2.5 animate-pulse">
+      <header class="flex items-center gap-x-1.5 h-5">
+        <div class="size-2.5 rounded-full bg-(--color-gray-95)"></div>
+        <div class="h-3 w-24 bg-(--color-gray-95) rounded"></div>
+      </header>
+      <div class="rounded-md bg-(--color-gray-95) flex-grow min-h-0 overflow-y-auto" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "RoadmapSkeleton",
+});
+</script>
