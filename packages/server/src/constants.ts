24c9fbea7b96fd01ef7632cdd1eb86784cd3f4d5
Yashu Mittal <mittalyashu77@gmail.com>
2025-10-26 23:45:28 +0530

    feat(server/license): add dual memory caching to improve performance

--- packages/server/src/constants.ts
+++ packages/server/src/constants.ts
@@ -8,4 +8,4 @@ export const GET_ROADMAPS_FILTER_COUNT = 20;
 export const GET_ROLES_FILTER_COUNT = 10;
 
 // 1 day in seconds
-export const LOGCHIMP_LICENSE_CACHE_TTL_IN_SECONDS = 60 * 60 * 24;
+export const LOGCHIMP_LICENSE_CACHE_TTL_IN_SEC = 60 * 60 * 24;
