957e58aac31bb9d9230d298169cbf69091b3cea4
Yashu Mittal <mittalyashu77@gmail.com>
2025-10-26 21:59:38 +0530

    feat(server/license): validate license with encrypted payload

--- packages/server/src/ee/services/license/checkLicense.ts
+++ packages/server/src/ee/services/license/checkLicense.ts
@@ -1,49 +1,139 @@
 import type {
+  ICheckLicenseDecryptedPayload,
   ICheckLicenseRequestBody,
   ICheckLicenseResponseBody,
 } from "@logchimp/types";
+import jwt from "jsonwebtoken";
 
 import { configManager } from "../../../utils/logchimpConfig";
 import * as cache from "../../../cache";
 import { LOGCHIMP_LICENSE_CACHE_TTL_IN_SECONDS } from "../../../constants";
+import logger from "../../../utils/logger";
 
 const config = configManager.getConfig();
 
 const getLogChimpCacheKey = (licenseKey: string) =>
   `${config.cachePrefix}pilot:key:${licenseKey}`;
 
-export async function checkLicense(): Promise<boolean> {
+const deploymentProvider = getDeploymentProvider();
+
+const EMPTY_LICENSE_RESPONSE: ICheckLicenseDecryptedPayload = {
+  status: "",
+  response_nonce: "",
+  server_time: "",
+};
+
+export async function checkLicense(): Promise<ICheckLicenseDecryptedPayload> {
   const licenseKey = config.licenseKey;
   if (!licenseKey) {
-    // Invalid or missing license key in your environment variables or logchimp.config.json file
-    return false;
+    return EMPTY_LICENSE_RESPONSE;
   }
 
-  // let cachedData: unknown;
+  let encryptedPayload: string | null = null;
   if (cache.isActive) {
-    const cachedData = await cache.valkey.get(getLogChimpCacheKey(licenseKey));
-    if (cachedData) {
-      return (JSON.parse(cachedData) as ICheckLicenseResponseBody).status;
+    try {
+      encryptedPayload = await cache.valkey.get(
+        getLogChimpCacheKey(licenseKey),
+      );
+    } catch (e) {
+      logger.error({
+        message: "error get license encrypted payload from cache",
+        error: e,
+      });
     }
   }
 
+  if (!encryptedPayload) {
+    try {
+      const data = await pingLicenseServer();
+      if ("code" in data || !data.encrypted_payload) {
+        return EMPTY_LICENSE_RESPONSE;
+      }
+
+      encryptedPayload = data.encrypted_payload;
+
+      if (cache.isActive) {
+        try {
+          await cache.valkey.set(
+            getLogChimpCacheKey(licenseKey),
+            data.encrypted_payload,
+            "EX",
+            LOGCHIMP_LICENSE_CACHE_TTL_IN_SECONDS,
+          );
+        } catch (e) {
+          logger.error({
+            message: "error cache license encrypted payload response",
+            error: e,
+          });
+        }
+      }
+    } catch (e) {
+      logger.error({
+        message: "failed to ping license server",
+        error: e,
+      });
+      return EMPTY_LICENSE_RESPONSE;
+    }
+  }
+
+  try {
+    return decryptPayload(encryptedPayload);
+  } catch (e) {
+    logger.error({
+      message: "failed to decrypt license payload",
+      error: e,
+    });
+
+    if (cache.isActive) {
+      try {
+        await cache.valkey.del(getLogChimpCacheKey(licenseKey));
+      } catch (e) {
+        logger.error({
+          message: "error deleting invalid cached license payload",
+          error: e,
+        });
+      }
+    }
+
+    return EMPTY_LICENSE_RESPONSE;
+  }
+}
+
+async function pingLicenseServer() {
   const response = await fetch(`${config.licensePilotUrl}/v1/license/check`, {
     method: "POST",
     mode: "cors",
     body: JSON.stringify({
-      license_key: licenseKey,
-      machine_signature: config.machineSignature,
+      license_key: config.licenseKey,
+      metadata: {
+        version: config.version,
+        deploymentProvider,
+        // features_used
+      },
       timestamp: new Date().toISOString(),
     } as ICheckLicenseRequestBody),
   });
-  const data = (await response.json()) as ICheckLicenseResponseBody;
-  if (cache.isActive) {
-    await cache.valkey.set(
-      getLogChimpCacheKey(licenseKey),
-      JSON.stringify(data),
-      "EX",
-      LOGCHIMP_LICENSE_CACHE_TTL_IN_SECONDS,
-    );
+
+  return (await response.json()) as ICheckLicenseResponseBody;
+}
+
+function decryptPayload(
+  encryptedPayload: string,
+): ICheckLicenseDecryptedPayload {
+  return jwt.verify(
+    encryptedPayload,
+    config.licenseSignature,
+  ) as ICheckLicenseDecryptedPayload;
+}
+
+function getDeploymentProvider() {
+  if (process.env.VERCEL) {
+    return "vercel";
+  } else if (process.env.RENDER) {
+    return "render";
+  } else if (process.env.RAILWAY_SERVICE_ID) {
+    return "railway";
+  } else {
+    return "unknown";
   }
-  return data.status;
 }
