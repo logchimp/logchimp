1198601fd3eb65e5e2a704332495d8d191ce3839
Yashu Mittal <mittalyashu77@gmail.com>
2025-11-01 15:50:50 +0530

    feat(theme): use `hasValidLicense` from settings EE store

--- packages/theme/src/ee/pages/dashboard/settings/roles/Index.vue
+++ packages/theme/src/ee/pages/dashboard/settings/roles/Index.vue
@@ -6,7 +6,7 @@
       </Breadcrumbs>
     </template>
 
-    <UpgradeTooltip :has-valid-license="settingsStore.settings.hasValidLicense">
+    <UpgradeTooltip :has-valid-license="hasValidLicense">
       <Button
         type="primary"
         :loading="createRoleButtonLoading"
@@ -15,7 +15,7 @@
       >
         Create
         <PhCrownSimple
-          v-if="!settingsStore.settings.hasValidLicense"
+          v-if="!hasValidLicense"
           :size="20"
           weight="regular"
           class="fill-white"
@@ -42,7 +42,7 @@ import { router } from "../../../../../router";
 import { useUserStore } from "../../../../../store/user";
 import { createRole } from "../../../../modules/roles";
 import { useDashboardRoles } from "../../../../store/dashboard/roles";
-import { useSettingStore } from "../../../../../store/settings";
+import { useSettingsEEStore } from "../../../../store/settings";
 
 // components
 import Button from "../../../../../components/ui/Button.vue";
@@ -55,7 +55,7 @@ import UpgradeTooltip from "../../../../components/UpgradeTooltip.vue";
 
 const { permissions } = useUserStore();
 const dashboardRoles = useDashboardRoles();
-const settingsStore = useSettingStore();
+const { hasValidLicense } = useSettingsEEStore();
 
 const createRoleButtonLoading = ref(false);
 
--- packages/theme/src/ee/store/settings.ts
+++ packages/theme/src/ee/store/settings.ts
@@ -24,6 +24,6 @@ export const useSettingsEEStore = defineStore("settingsEE", () => {
   });
 
   return {
-    hasValidLicense: hasValidLicense.value,
+    hasValidLicense,
   };
 });
