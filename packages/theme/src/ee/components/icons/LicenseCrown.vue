d17f6f57a3c69db868e0f3471923b389369f96d0
Yashu Mittal <mittalyashu77@gmail.com>
2025-11-01 18:40:22 +0530

    feat(theme): create LicenseCrown.vue icon

---
+++ packages/theme/src/ee/components/icons/LicenseCrown.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { PhCrownSimple } from "@phosphor-icons/vue";
+</script>
+
+<template>
+  <PhCrownSimple
+    :size="20"
+    weight="regular"
+    class="fill-white"
+  />
+</template>
