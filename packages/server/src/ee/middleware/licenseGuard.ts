f58b8d4f204cb183e391b9833c48e34b19cfa1ff
Yashu Mittal <mittalyashu77@gmail.com>
2025-11-01 13:36:09 +0530

    feat(server/middleware): add try-catch handling errors in licenseGuard with logging

--- packages/server/src/ee/middleware/licenseGuard.ts
+++ packages/server/src/ee/middleware/licenseGuard.ts
@@ -3,20 +3,32 @@ import type { IApiErrorResponse } from "@logchimp/types";
 
 import error from "../../errorResponse.json";
 import { checkLicense } from "../services/license/checkLicense";
+import logger from "../../utils/logger";
 
 export async function licenseGuard(
   _: Request,
   res: Response<IApiErrorResponse>,
   next: NextFunction,
 ) {
-  const result = await checkLicense();
+  try {
+    const result = await checkLicense();
 
-  if ("code" in result || result.status !== "active") {
-    return res.status(403).send({
-      message: error.middleware.license.verification,
-      code: "LICENSE_VALIDATION_FAILED",
+    if ("code" in result || result.status !== "active") {
+      return res.status(403).send({
+        message: error.middleware.license.verification,
+        code: "LICENSE_VALIDATION_FAILED",
+      });
+    }
+
+    next();
+  } catch (e) {
+    logger.error({
+      message: e,
+    });
+
+    res.status(500).send({
+      message: error.general.serverError,
+      code: "SERVER_ERROR",
     });
   }
-
-  next();
 }
