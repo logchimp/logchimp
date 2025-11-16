6c3df938926c1544bb5af5c00ce9cee99a8c7eb6
Yashu Mittal <mittalyashu77@gmail.com>
2025-11-16 18:33:36 +0530

    fix(server/roadmap): access roadmap via middleware `req.roadmap` in roadmapByUrl controller

--- packages/server/src/controllers/roadmaps/roadmapByUrl.ts
+++ packages/server/src/controllers/roadmaps/roadmapByUrl.ts
@@ -1,15 +1,15 @@
-import type { Response } from "express";
+import type { Request, Response } from "express";
 import type {
   IGetRoadmapByUrlRequestParam,
   IGetRoadmapByUrlResponseBody,
 } from "@logchimp/types";
-import type { ExpressRequestContext } from "../../express";
 
 export async function roadmapByUrl(
-  req: ExpressRequestContext<IGetRoadmapByUrlRequestParam>,
+  req: Request<IGetRoadmapByUrlRequestParam>,
   res: Response<IGetRoadmapByUrlResponseBody>,
 ) {
-  const roadmap = req.ctx.roadmap;
+  // @ts-expect-error
+  const roadmap = req.roadmap;
 
   res.status(200).send({ roadmap });
 }
