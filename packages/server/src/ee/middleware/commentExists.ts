f715d0d7ec6ce1918a88394cb98184024648d59c
Yashu Mittal <mittalyashu77@gmail.com>
2025-11-24 12:51:26 +0530

    fix(server/commentExists): update middleware to retrieve `post_id` for a comment by adding a join between `posts_comments` and `posts_activity`

--- packages/server/src/ee/middleware/commentExists.ts
+++ packages/server/src/ee/middleware/commentExists.ts
@@ -28,12 +28,32 @@ export async function commentExists(
     });
   }
 
-  const comment = await database
-    .select()
-    .from("posts_comments")
-    .where({
-      id: id || null,
-    })
+  const comment = await database("posts_comments as pc")
+    .leftJoin("posts_activity as pa", "pa.posts_comments_id", "pc.id")
+    .select<{
+      id: string;
+      parent_id: string | null;
+      activity_id: string;
+      body: string;
+      is_edited: boolean;
+      is_spam: boolean;
+      is_internal: boolean;
+      created_at: Date;
+      updated_at: Date;
+      post_id: string | null;
+    }>([
+      "pc.id",
+      "pc.parent_id",
+      "pc.activity_id",
+      "pc.body",
+      "pc.is_edited",
+      "pc.is_spam",
+      "pc.is_internal",
+      "pc.created_at",
+      "pc.updated_at",
+      "pa.post_id",
+    ])
+    .where("pc.id", "=", id || null)
     .first();
 
   if (!comment) {
