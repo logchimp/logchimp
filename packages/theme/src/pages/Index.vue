e6530c6337c4e9ac213dda115f919f9fd170b41e
TaranaKhanna <taranakhanna5@gmail.com>
2025-12-21 03:32:56 +0000

    feat: add dialog for creating posts with title and description

--- packages/theme/src/pages/Index.vue
+++ packages/theme/src/pages/Index.vue
@@ -1,4 +1,49 @@
 <template>
+  <Dialog v-model:open="showDialog">
+    <l-text
+      v-model="title.value"
+      label="Title"
+      type="text"
+      name="Post title"
+      data-test="post-title"
+      placeholder="Name of the feature"
+      :error="title.error"
+      :disabled="createPostPermissionDisabled"
+      @keyup-enter="submitPost"
+      @hide-error="hideTitleError"
+    />
+    <l-textarea
+      v-model="description"
+      label="Description"
+      rows="4"
+      name="Post description"
+      placeholder="What would you use it for?"
+      :disabled="createPostPermissionDisabled"
+    />
+    <template #footer>
+       <Button
+        type="primary"
+        data-test="submit-post-button"
+        :loading="loading"
+        :disabled="createPostPermissionDisabled"
+        @click="submitPost"
+      >
+        Submit
+      </Button>
+    </template>
+
+  </Dialog>
+
+  <div class="flex justify-center mb-8">
+    <Button
+        type="primary"
+        data-test="create-post-button"
+        @click="showDialog = true"
+      >
+        Submit Feedback
+    </Button>
+  </div>
+
   <div class="flex flex-col-reverse lg:flex-row mb-16 lg:gap-x-8">
     <main class="grow-[2] shrink basis-0">
       <post-item
@@ -18,7 +63,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive, computed } from "vue";
 import { useHead } from "@vueuse/head";
 import type { IPost } from "@logchimp/types";
 
@@ -27,6 +72,8 @@ import { isSiteSetup } from "../modules/site";
 import { getPosts } from "../modules/posts";
 import { useSettingStore } from "../store/settings";
 import { useUserStore } from "../store/user";
+import { createPost } from "../modules/posts";
+import { router } from "../router";
 
 // components
 import InfiniteScroll, {
@@ -36,6 +83,50 @@ import PostItem from "../components/post/PostItem.vue";
 import SiteSetupCard from "../components/site/SiteSetupCard.vue";
 import LoginCard from "../components/auth/LoginCard.vue";
 import TopPublicBoardsList from "../ee/components/TopPublicBoardsList.vue";
+import Dialog from "../components/ui/Dialog/Dialog.vue";
+import LText from "../components/ui/input/LText.vue";
+import LTextarea from "../components/ui/input/LTextarea.vue";
+import Button from "../components/ui/Button.vue";
+import type { FormFieldErrorType } from "../components/ui/input/formBaseProps";
+
+// utils
+import validateUUID from "../utils/validateUUID";
+import tokenError from "../utils/tokenError";
+
+const { permissions } = useUserStore();
+
+const props = defineProps({
+  boardId: {
+    type: String,
+    required: true,
+    validator: validateUUID,
+  },
+  dashboard: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const showDialog = ref(false);
+const title = reactive({
+  value: "",
+  error: {
+    show: false,
+    message: "",
+  },
+});
+const description = ref<string>("");
+const loading = ref<boolean>(false);
+
+const dashboardUrl = computed(() => (props.dashboard ? "/dashboard" : ""));
+const createPostPermissionDisabled = computed(() => {
+  const checkPermission = permissions.includes("post:create");
+  return !checkPermission;
+});
+
+function hideTitleError(event: FormFieldErrorType) {
+  title.error = event;
+}
 
 const settingsStore = useSettingStore();
 const userStore = useUserStore();
@@ -94,4 +185,30 @@ useHead({
 defineOptions({
   name: "Homepage",
 });
+
+async function submitPost() {
+  if (!title.value) {
+    title.error.show = true;
+    title.error.message = "You forgot to enter a post title";
+    return;
+  }
+
+  loading.value = true;
+
+  try {
+    const response = await createPost({
+      title: title.value,
+      contentMarkdown: description.value,
+      boardId: props.boardId,
+    });
+
+    // redirect to post
+    const slug = response.data.post.slug;
+    router.push(`${dashboardUrl.value}/posts/${slug}`);
+  } catch (error) {
+    tokenError(error);
+  } finally {
+    loading.value = false;
+  }
+}
 </script>
