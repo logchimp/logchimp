e419c87364f6d912fca5ed76e824f63a055637aa
Yashu Mittal <mittalyashu77@gmail.com>
2026-02-13 19:49:41 +0530

    feat: singular point for license validation url (#1554)
    * feat(server/config): update logchimp pilot URL as singular point
    
    * feat: update do-not-remove module
    
    * feat(config): remove `DEFAULT_LOGCHIMP_PILOT_URL` from logchimpConfig
    
    * feat: update do-not-remove module

--- packages/server/src/utils/logchimpConfig.ts
+++ packages/server/src/utils/logchimpConfig.ts
@@ -8,7 +8,6 @@ import { isDevTestEnv } from "../helpers";
 const DEFAULT_SERVER_PORT = 8000;
 const DEFAULT_DATABASE_PORT = 5432;
 const DEFAULT_MAIL_PORT = 465;
-const DEFAULT_LOGCHIMP_PILOT_URL = "https://pilot.logchimp.codecarrot.net";
 
 interface Config {
   version: string;
@@ -134,7 +133,7 @@ class ConfigManager {
       // License
       licenseKey: config?.license?.key,
       licenseSignature: config?.license?.signature,
-      licensePilotUrl: config?.license?.pilotUrl || DEFAULT_LOGCHIMP_PILOT_URL,
+      licensePilotUrl: config?.license?.pilotUrl,
 
       // Database
       databaseUrl: config.database?.url,
@@ -194,8 +193,7 @@ class ConfigManager {
       // License
       licenseKey: process.env.LOGCHIMP_LICENSE_KEY,
       licenseSignature: process.env.LOGCHIMP_SIGNATURE_TOKEN,
-      licensePilotUrl:
-        process.env.LOGCHIMP_PILOT_URL || DEFAULT_LOGCHIMP_PILOT_URL,
+      licensePilotUrl: process.env.LOGCHIMP_PILOT_URL,
 
       // Database
       databaseUrl: process.env.LOGCHIMP_DB_URL,
