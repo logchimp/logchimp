a4accc099dc813b4028f3d3156b81f86c42ce1b3
Yashu Mittal <mittalyashu77@gmail.com>
2026-02-15 13:44:27 +0530

    fix(docker/db): replace inline migration command with script in docker-compose.dev.yml

--- packages/server/scripts/migrate.sh
+++ packages/server/scripts/migrate.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-pnpm knex --knexfile ./dist/database/config.js migrate:latest && \
+# Default path set to production
+CONFIG_PATH="dist/database/config.js"
+
+if [ "$NODE_ENV" = "development" ]
+then
+  CONFIG_PATH="src/database/config.ts"
+  echo "Using development config: $CONFIG_PATH"
+else
+  echo "Using production config: $CONFIG_PATH"
+fi
+
+pnpm knex --knexfile ./$CONFIG_PATH migrate:latest && \
   echo '=> Migration done'
