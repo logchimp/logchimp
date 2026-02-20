4ea7be9208d3166362f9d283ee4a1c80816673d8
Yashu Mittal <mittalyashu77@gmail.com>
2026-02-20 17:02:41 +0530

    feat(theme): handle license validation error for boards and roadmaps

--- packages/theme/src/pages/Roadmaps.vue
+++ packages/theme/src/pages/Roadmaps.vue
@@ -1,35 +1,49 @@
 <template>
   <roadmap-skeleton v-if="loading" />
-
-  <!-- Show roadmaps grid only when we have roadmaps -->
   <div
-    v-if="roadmaps.length > 0"
-    ref="roadmapElement"
-    :class="[
+    v-else-if="errorCode === 'LICENSE_VALIDATION_FAILED'"
+    class="text-center"
+  >
+    <p>
+      No roadmaps available
+    </p>
+  </div>
+  <template v-else>
+    <!-- Show roadmaps grid only when we have roadmaps -->
+    <div
+      v-if="roadmaps.length > 0"
+      ref="roadmapElement"
+      :class="[
       'overflow-x-auto h-[500px] overflow-y-hidden',
       'grid grid-flow-col gap-x-4 md:gap-x-6 auto-cols-[minmax(22rem,24rem)]',
     ]"
-  >
-    <roadmap-column
-      v-for="roadmap in roadmaps"
-      :key="roadmap.id"
-      :roadmap="roadmap"
-    />
-
-    <div
-      v-if="scrollLoading"
-      class="flex items-center justify-center"
     >
-      <LoaderContainer />
+      <roadmap-column
+        v-for="roadmap in roadmaps"
+        :key="roadmap.id"
+        :roadmap="roadmap"
+      />
+
+      <div
+        v-if="scrollLoading"
+        class="flex items-center justify-center"
+      >
+        <LoaderContainer />
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script setup lang="ts">
 import { ref, useTemplateRef, onMounted } from "vue";
 import { useHead } from "@vueuse/head";
 import { useInfiniteScroll } from "@vueuse/core";
-import type { IPaginatedRoadmapsResponse, IRoadmap } from "@logchimp/types";
+import type {
+  IApiErrorResponse,
+  IPaginatedRoadmapsResponse,
+  IRoadmap,
+} from "@logchimp/types";
+import type { AxiosError } from "axios";
 
 // modules
 import { getAllRoadmaps } from "../modules/roadmaps";
@@ -46,7 +60,7 @@ const roadmapElement = useTemplateRef<HTMLElement>("roadmapElement");
 const roadmaps = ref<IRoadmap[]>([]);
 const endCursor = ref<string | undefined>();
 const hasNextPage = ref<boolean>(false);
-
+const errorCode = ref<string>();
 const loading = ref(true);
 const scrollLoading = ref(false);
 
@@ -69,8 +83,11 @@ async function getRoadmaps(after: string | undefined, isScroll = false) {
     }
     endCursor.value = paginatedData.page_info.end_cursor || undefined;
     hasNextPage.value = paginatedData.page_info.has_next_page;
-  } catch (err) {
-    console.error("Error fetching roadmaps:", err);
+  } catch (error) {
+    const err = error as AxiosError<IApiErrorResponse>;
+    if (err.response?.data.code === "LICENSE_VALIDATION_FAILED") {
+      errorCode.value = err.response.data.code;
+    }
   } finally {
     loading.value = false;
     scrollLoading.value = false;
