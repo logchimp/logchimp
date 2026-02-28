d85d927892ebc78b1dc64f93de52b6b863221c54
Yashu Mittal <mittalyashu77@gmail.com>
2026-02-28 14:06:08 +0530

    fix(server/test): merge `baseConfig.test` properties to preserve base configuration

--- packages/server/vitest.config.unit.ts
+++ packages/server/vitest.config.unit.ts
@@ -5,6 +5,7 @@ import baseConfig from "./vitest.config.base";
 export default defineConfig({
   ...baseConfig,
   test: {
+    ...baseConfig.test,
     include: ["./tests/unit/**/*.test.ts", "./src/**/*.test.ts"],
   },
 });
