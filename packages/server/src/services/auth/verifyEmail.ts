339d6ccecbdef88d3e37275faaa232c54ccc6771
Yashu Mittal <mittalyashu77@gmail.com>
2026-02-28 14:07:08 +0530

    Fix: reset email branding - Part 2 (#1584)
    * Fix: reset email branding (#1461)
    
    * feat: added custom details
    
    * feat: added site logo and color to email content
    
    * refactor: change in template
    
    * refactor: change in raw template
    
    * fix: update color handling in reset email template
    
    * refactor: revert color change for anchor tag
    
    * refactor: added # to recognise a color value
    
    * refactor: site color value
    
    * fix(server/auth): add explicit type annotations for get siteInfo db query statement
    
    * fix: throw error if site settings is empty array
    
    * fix(server/auth): use LogChimp fallback brand color if site color is undefined
    
    * fix(mail/templates): inline site color style for reset password button
    
    Remove unused `--site-color` CSS variable references and apply `siteColor` directly as an inline style for the reset password button.
    
    * test(mail): add unit tests for `generateContent` function
    
    * test(server): expand unit test coverage to additional directories
    
    Update `vitest.config.unit.ts` to include `src/**/*.test.ts` directory, enhancing unit test coverage.
    
    * test: add test case "should fallback to default LogChimp brand color" in generateContent.test.ts
    
    * test(mail): add test for handling invalid template in generateContent
    
    * fix(mail): ensure fallback brand color is applied when site color is undefined
    
    * fix(mail/templates): dynamically fetch site logo and color for verify email template
    
    * test(mail): add tests for account verification email in generateContent.test.ts
    
    * chore: format the code
    
    * fix(server/test): merge `baseConfig.test` properties to preserve base configuration
    
    ---------
    
    Co-authored-by: Vedant Kanekar <111733870+Vedant005@users.noreply.github.com>

--- packages/server/src/services/auth/verifyEmail.ts
+++ packages/server/src/services/auth/verifyEmail.ts
@@ -36,8 +36,29 @@ export async function verifyEmail(
     /**
      * Get site title for using it in email footer
      */
-    const siteInfo = await database.select("title").from("settings");
+    const siteInfo = await database
+      .select<
+        Array<{
+          title: string;
+          accentColor: string;
+          logo: string;
+        }>
+      >("title", "accentColor", "logo")
+      .from("settings");
+
+    if (siteInfo.length === 0) {
+      throw new Error("Site settings not found");
+    }
+
     const siteTitle = siteInfo[0].title;
+    let siteColor = siteInfo[0].accentColor;
+    const siteLogo =
+      siteInfo[0].logo ||
+      "https://cdn.logchimp.codecarrot.net/logchimp_circular_logo.png";
+
+    if (siteColor && !siteColor.startsWith("#")) {
+      siteColor = `#${siteColor}`;
+    }
 
     const urlObject = new URL(config.webUrl);
     const onboardingMailContent = await generateContent("verify", {
@@ -45,6 +66,8 @@ export async function verifyEmail(
       domain: urlObject.hostname,
       verificationLink: `${urlObject.origin}/email-verify/?token=${token}`,
       siteTitle,
+      siteColor,
+      siteLogo,
     });
 
     const noReplyEmail = `noreply@${urlObject.hostname}`;
