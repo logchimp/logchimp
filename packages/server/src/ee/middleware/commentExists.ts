9fb403f36cd314e9d62b9fd76d33dea0d1efdba9
Yashu Mittal <mittalyashu77@gmail.com>
2026-03-03 18:23:54 +0530

    refactor(server/comments): extract `getCommentStatement` and improve error handling in `commentExists` middleware

--- packages/server/src/ee/middleware/commentExists.ts
+++ packages/server/src/ee/middleware/commentExists.ts
@@ -2,13 +2,13 @@ import type { NextFunction, Request, Response } from "express";
 import type {
   IApiErrorResponse,
   IUpdatePostCommentRequestParam,
-  TDeletePostCommentRequestParam,
+  TDeletePostCommentRequestParam
 } from "@logchimp/types";
-import database from "../../database";
 
-// utils
+import database from "../../database";
 import error from "../../errorResponse.json";
 import { validUUID } from "../../helpers";
+import logger from "../../utils/logger";
 
 type RequestParam =
   | IUpdatePostCommentRequestParam
@@ -29,7 +29,59 @@ export async function commentExists(
     return;
   }
 
-  const comment = await database("posts_comments as pc")
+  let comment: GetCommentStatement;
+  try {
+    comment = await getCommentStatement(id);
+  } catch (e) {
+    logger.error({
+      message: "failed to get comment from db",
+      error: e,
+    })
+    res.status(500).send({
+      message: error.general.serverError,
+      code: "SERVER_ERROR",
+    });
+    return;
+  }
+
+  if (!comment) {
+    res.status(404).send({
+      message: error.api.comments.commentNotFound,
+      code: "COMMENT_NOT_FOUND",
+    });
+    return;
+  }
+
+  // @ts-expect-error
+  const post = req.post;
+  if (!post || comment.post_id !== post.postId) {
+    res.status(404).send({
+      message: error.api.comments.commentNotFound,
+      code: "COMMENT_NOT_FOUND",
+    });
+    return;
+  }
+
+  // @ts-expect-error
+  req.comment = comment;
+  next();
+}
+
+interface GetCommentStatement {
+  id: string;
+  parent_id: string | null;
+  activity_id: string;
+  body: string;
+  is_edited: boolean;
+  is_spam: boolean;
+  is_internal: boolean;
+  created_at: Date;
+  updated_at: Date;
+  post_id: string | null;
+}
+
+function getCommentStatement(commentId: string) {
+  return database("posts_comments as pc")
     .leftJoin("posts_activity as pa", "pa.posts_comments_id", "pc.id")
     .select<{
       id: string;
@@ -54,28 +106,6 @@ export async function commentExists(
       "pc.updated_at",
       "pa.post_id",
     ])
-    .where("pc.id", "=", id || null)
+    .where("pc.id", "=", commentId)
     .first();
-
-  if (!comment) {
-    res.status(404).send({
-      message: error.api.comments.commentNotFound,
-      code: "COMMENT_NOT_FOUND",
-    });
-    return;
-  }
-
-  // @ts-expect-error
-  const post = req.post;
-  if (!post || comment.post_id !== post.postId) {
-    res.status(404).send({
-      message: error.api.comments.commentNotFound,
-      code: "COMMENT_NOT_FOUND",
-    });
-    return;
-  }
-
-  // @ts-expect-error
-  req.comment = comment;
-  next();
 }
