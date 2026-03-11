da5cdaaddd1855b5d7814b11a3162c8472b356c0
YuF-9468 <corgiking@163.com>
2026-03-11 20:50:31 +0800

    fix(theme): prevent duplicate signup submit race (#1571) (#1608)
    Co-authored-by: YuF-9468 <264538812+YuF-9468@users.noreply.github.com>
    (cherry picked from commit 9ffea24ab2f365271bb3a1abb07d2aa8745981d6)

--- packages/theme/src/pages/Join.vue
+++ packages/theme/src/pages/Join.vue
@@ -34,8 +34,7 @@
         <Button
           type="primary"
           :loading="buttonLoading"
-          :disabled="!siteSettings.allowSignup"
-          @click="join"
+          :disabled="buttonLoading || !siteSettings.allowSignup"
         >
           Create Account
         </Button>
@@ -98,6 +97,10 @@ function hidePasswordError(event: FormFieldErrorType) {
 }
 
 async function join() {
+  if (buttonLoading.value) {
+    return;
+  }
+
   if (!(email.value && password.value)) {
     if (!email.value) {
       emailError.show = true;
