5fdd4cba015f8677f3efd472b73d54d3b8b9f4ca
Yashu Mittal <mittalyashu77@gmail.com>
2026-03-12 21:24:16 +0530

    chore: format the code

--- packages/server/src/routes/v1/index.ts
+++ packages/server/src/routes/v1/index.ts
@@ -9,14 +9,7 @@ import votes from "./votes";
 import users from "./users";
 import settings from "./settings";
 
-router.use(
-  "/api/v1",
-  auth,
-  posts,
-  votes,
-  users,
-  settings,
-);
+router.use("/api/v1", auth, posts, votes, users, settings);
 
 let eeRoutesLoaded = false;
 export async function initialiseEERoutes() {
@@ -28,11 +21,13 @@ export async function initialiseEERoutes() {
     eeRoutesLoaded = true;
   } catch (_) {
     logger.info("Running in open source mode (EE features not available)");
-    logger.info("Upgrade to higher plan to access boards, roadmaps, and roles features.");
-    logger.info("Go to -> https://logchimp.app")
+    logger.info(
+      "Upgrade to higher plan to access boards, roadmaps, and roles features.",
+    );
+    logger.info("Go to -> https://logchimp.app");
   }
 }
 
-initialiseEERoutes()
+initialiseEERoutes();
 
 export default router;
