7613cd93a5d0951614e4d90fe96ddccddf668e56
Ja4V8s28Ck <66721411+Ja4V8s28Ck@users.noreply.github.com>
2026-03-13 18:33:08 +0530

    refactor(authUser.spec.ts): replace `SERVER_ERROR` with `INVALID_TOKEN` for better understanding

--- packages/server/tests/integration/v1/users/authUser.spec.ts
+++ packages/server/tests/integration/v1/users/authUser.spec.ts
@@ -36,7 +36,7 @@ describe("GET /api/v1/users/profile", () => {
     expect(response.body.code).toBe("INVALID_TOKEN");
   });
 
-  it("should throw 'SERVER_ERROR' for passing empty JWT token", async () => {
+  it("should throw 'INVALID_TOKEN' for passing empty JWT token", async () => {
     const fakeToken = createToken({}, {});
 
     const response = await supertest(app)
@@ -44,8 +44,8 @@ describe("GET /api/v1/users/profile", () => {
       .set("Authorization", `Bearer ${fakeToken}`);
 
     expect(response.headers["content-type"]).toContain("application/json");
-    expect(response.statusCode).toBe(500);
-    expect(response.body.code).toBe("SERVER_ERROR");
+    expect(response.statusCode).toBe(401);
+    expect(response.body.code).toBe("INVALID_TOKEN");
   });
 
   it("should throw 'USER_NOT_FOUND' error with fake user ID", async () => {
