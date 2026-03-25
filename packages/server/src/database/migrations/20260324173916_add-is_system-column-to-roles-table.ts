6b5889a04fc086e614cfffb9de3777ac46b99429
Yashu Mittal <mittalyashu77@gmail.com>
2026-03-25 12:34:36 +0530

    feat(db): remove `is_system` column from permissions table in migration file
    Since all the permissions are already system-controlled.

--- packages/server/src/database/migrations/20260324173916_add-is_system-column-to-roles-table.ts
+++ packages/server/src/database/migrations/20260324173916_add-is_system-column-to-roles-table.ts
@@ -5,51 +5,43 @@ import logger from "../../utils/logger";
 export async function up(knex: Knex): Promise<void> {
   try {
     await knex.transaction(async (trx) => {
-      await trx.schema.table("permissions", (table) => {
-        table.smallint("is_system").defaultTo(0);
-      });
       await trx.schema.table("roles", (table) => {
         table.smallint("is_system").defaultTo(0);
       });
-    })
+    });
 
     logger.info({
       code: "DATABASE_MIGRATIONS",
-      message: "Add is_system column to 'permissions' and 'roles' table",
-    })
+      message: "Add is_system column to 'roles' table",
+    });
   } catch (err) {
     logger.error({
       code: "DATABASE_MIGRATIONS",
-      message: "Error adding is_system column to 'permissions' and 'roles' table",
+      message: "Error adding is_system column to 'roles' table",
       err,
-    })
+    });
     throw err;
   }
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   try {
     await knex.transaction(async (trx) => {
-      await trx.schema.table("permissions", (table) => {
-        table.dropColumn("is_system");
-      });
       await trx.schema.table("roles", (table) => {
         table.dropColumn("is_system");
       });
-    })
+    });
 
     logger.info({
       code: "DATABASE_MIGRATIONS",
-      message: "Drop is_system column from 'permissions' and 'roles' table",
-    })
+      message: "Drop is_system column from 'roles' table",
+    });
   } catch (err) {
     logger.error({
       code: "DATABASE_MIGRATIONS",
-      message: "Error dropping is_system column from 'permissions' and 'roles' table",
+      message: "Error dropping is_system column from 'roles' table",
       err,
-    })
+    });
     throw err;
   }
 }
-
