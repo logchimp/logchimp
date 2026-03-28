9efd5d13f9199671f6187b170c7a740b6135eaa4
Yashu Mittal <mittalyashu77@gmail.com>
2026-03-28 18:47:08 +0530

    refactor(theme/posts): move DashboardPostEditor as nested component of PostViewer

---
+++ packages/theme/src/components/dashboard/posts/PostViewer.vue
@@ -0,0 +1,19 @@
+<template>
+  <DashboardPostEditor
+    :post="post"
+  />
+</template>
+
+<script setup lang="ts">
+import type { IDashboardPost } from "@logchimp/types";
+import DashboardPostEditor from "./PostEditor/index.vue";
+
+interface Props {
+  post: IDashboardPost;
+}
+defineProps<Props>();
+
+defineOptions({
+  name: "DashboardPostViewer",
+});
+</script>
