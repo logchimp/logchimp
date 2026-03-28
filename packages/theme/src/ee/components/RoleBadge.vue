fa953296cbcb402992148cb2ae665a367a3abd0b
Yashu Mittal <mittalyashu77@gmail.com>
2026-03-28 11:14:05 +0530

    feat(theme/roles): display system role icon in users list of `AssignRoleUserDropdown.vue` component

--- packages/theme/src/ee/components/RoleBadge.vue
+++ packages/theme/src/ee/components/RoleBadge.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="px-3 py-1 bg-neutral-200 rounded-lg text-sm font-medium select-none">
-    {{role.name}}
+  <div
+    :class="[
+      'flex items-center gap-x-2',
+      'px-3 py-1 bg-neutral-200 rounded-lg text-sm font-medium select-none',
+    ]"
+  >
+    <span>
+      {{role.name}}
+    </span>
+    <ComputerIcon
+      v-if="role.isSystem"
+      class="size-4 stroke-neutral-500"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { IUserRole } from "@logchimp/types";
+import { ComputerIcon } from "lucide-vue";
 
 interface Props {
   role: IUserRole;
