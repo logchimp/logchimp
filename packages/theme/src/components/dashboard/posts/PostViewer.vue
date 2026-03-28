430ae94d4c5f74c0cc4aa7f1222b8b694a321fa2
Yashu Mittal <mittalyashu77@gmail.com>
2026-03-28 19:06:44 +0530

    feat(theme/posts): move `DashboardPageHeader` to PostViewer

--- packages/theme/src/components/dashboard/posts/PostViewer.vue
+++ packages/theme/src/components/dashboard/posts/PostViewer.vue
@@ -1,17 +1,70 @@
 <template>
-  <DashboardPostEditor
-    :post="post"
-  />
+  <DashboardPageHeader>
+    <template v-slot:left>
+      <Breadcrumbs>
+        <BreadcrumbItem to="/dashboard/posts">
+          Posts
+        </BreadcrumbItem>
+
+        <!-- Show divider & title once data loaded -->
+        <template v-if="post.title">
+          <BreadcrumbDivider />
+          <BreadcrumbItem>
+            {{ post.title }}
+          </BreadcrumbItem>
+        </template>
+      </Breadcrumbs>
+    </template>
+
+    <Button
+      type="primary"
+      :loading="loading"
+      :disabled="hasPermission"
+      @click="updatePostHandler"
+    >
+      Save
+    </Button>
+  </DashboardPageHeader>
+
+  <div class="px-3 lg:px-6">
+    <DashboardPostEditor
+      ref="dashboardPostEditorRef"
+      :post="post"
+      @loading="loading = $event"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from "vue";
 import type { IDashboardPost } from "@logchimp/types";
+
 import DashboardPostEditor from "./PostEditor/index.vue";
+import Button from "../../ui/Button.vue";
+import Breadcrumbs from "../../Breadcrumbs.vue";
+import BreadcrumbDivider from "../../ui/breadcrumbs/BreadcrumbDivider.vue";
+import BreadcrumbItem from "../../ui/breadcrumbs/BreadcrumbItem.vue";
+import DashboardPageHeader from "../PageHeader.vue";
+import { useUserStore } from "../../../store/user";
+
+const { permissions } = useUserStore();
 
 interface Props {
   post: IDashboardPost;
 }
 defineProps<Props>();
+
+const loading = ref(false);
+const dashboardPostEditorRef = ref();
+
+const hasPermission = computed(() => {
+  const checkPermission = permissions.includes("post:update");
+  return !checkPermission;
+});
+
+function updatePostHandler() {
+  dashboardPostEditorRef.value.updatePostHandler();
+}
 
 defineOptions({
   name: "DashboardPostViewer",
