d8986b4a803ec23ea2c29cd6ea1e7b3eadd0d0d6
Yashu Mittal <mittalyashu77@gmail.com>
2026-03-29 16:52:09 +0530

    feat(theme/ui): create Tooltip component

---
+++ packages/theme/src/components/ui/Tooltip/Tooltip.vue
@@ -0,0 +1,31 @@
+<template>
+  <TooltipProvider>
+    <TooltipRoot>
+      <TooltipTrigger>
+        <slot name="trigger" />
+      </TooltipTrigger>
+      <TooltipPortal>
+        <TooltipContent
+          :class="[
+            'animate-slide-up-fade pointer-events-auto items-center',
+            'overflow-hidden rounded-lg border border-neutral-200 bg-white shadow-sm',
+            'text-xs px-2 py-1 text-neutral-700'
+          ]"
+          :side-offset="10"
+        >
+          <slot />
+        </TooltipContent>
+      </TooltipPortal>
+    </TooltipRoot>
+  </TooltipProvider>
+</template>
+
+<script setup lang="ts">
+import {
+  TooltipProvider,
+  TooltipRoot,
+  TooltipTrigger,
+  TooltipPortal,
+  TooltipContent,
+} from "reka-ui";
+</script>
