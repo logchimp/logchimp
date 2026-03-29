a79f90bc791b94cacef4cb3f2def682118d1ed23
Yashu Mittal <mittalyashu77@gmail.com>
2026-03-29 19:33:21 +0530

    feat(posts/activity): display 'lock' icon to mark comment as internal

--- packages/theme/src/ee/components/activity/ActivityItem.vue
+++ packages/theme/src/ee/components/activity/ActivityItem.vue
@@ -6,7 +6,17 @@
     />
 
 		<div class="ml-4 flex-1">
-			<div class="font-medium mb-1">{{ activity.author.name }}</div>
+      <div class="flex items-center mb-1">
+        <div class="font-medium">
+          {{ activity.author.name }}
+        </div>
+
+        <div class="flex items-center" v-if="activity.comment.is_internal">
+          <span class="mx-2 text-xs text-neutral-500">•</span>
+          <LockKeyhole class="size-4 stroke-neutral-700" />
+        </div>
+      </div>
+
 			<p class="mb-0.5 text-sm break-all">{{ activity.comment.body }}</p>
 
 			<div class="flex items-center gap-1.5 text-xs text-neutral-600">
@@ -25,6 +35,7 @@
 <script setup lang="ts">
 import dayjs from "dayjs";
 import type { IPostActivity } from "@logchimp/types";
+import { LockKeyhole } from "lucide-vue";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 import { Avatar } from "../../../components/ui/Avatar";
