fe2f8608731865a8c34cdb1e293b6b9020f7fd17
Yashu Mittal <mittalyashu77@gmail.com>
2026-04-05 21:36:18 +0530

    feat(server): execute system roles seeding script file in dev & prod env

--- packages/server/docker-dev-entrypoint.sh
+++ packages/server/docker-dev-entrypoint.sh
@@ -3,5 +3,8 @@
 # Build @logchimp/types
 pnpm --filter="@logchimp/types" build
 
+# Seed system roles and permissions
+pnpm ts-node ./src/ee/services/roles/system.ts
+
 # Run Node dev server using air CLI
 exec air -c .air.toml
