4fb195d8b251a95f0007411dd28ea76f8365e3a6
Yashu Mittal <mittalyashu77@gmail.com>
2026-04-06 18:14:34 +0530

    fix(theme/posts): dynamically update voter count and pluralization in Votes.vue

--- packages/theme/src/components/dashboard/posts/PostSidebar/Votes/Votes.vue
+++ packages/theme/src/components/dashboard/posts/PostSidebar/Votes/Votes.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex items-center justify-between mb-2">
       <span class="text-sm text-neutral-700 font-medium">
-        1 Voters
+        {{votes.length}} Voter{{votes.length > 1 ? 's' : ''}}
       </span>
 
       <button class="border border-neutral-300 hover:bg-neutral-100 bg-white text-sm font-medium text-neutral-700 px-2 py-1 rounded-md">
