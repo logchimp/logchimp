fe61a6bbef012f5c9b52aa05547b714e7439b0fd
Yashu Mittal <mittalyashu77@gmail.com>
2026-04-14 19:37:37 +0530

    feat(theme/posts): add `activityDashboard` for viewing the post comments for public and dashboard view

--- packages/theme/src/ee/store/postActivity.ts
+++ packages/theme/src/ee/store/postActivity.ts
@@ -4,16 +4,39 @@ import type { IPostActivity } from "@logchimp/types";
 
 export const usePostActivityEEStore = defineStore("postActivityEE", () => {
   const activity = reactive<Record<string, IPostActivity[]>>({});
+  const activityDashboard = reactive<Record<string, IPostActivity[]>>({});
 
   function addPostActivity(postId: string, _activity: IPostActivity) {
-    Object.assign(activity, {
-      [postId]: [_activity, ...(activity[postId] || [])],
+    if (!_activity.comment.is_internal) {
+      Object.assign(activity, {
+        [postId]: [_activity, ...(activity[postId] || [])],
+      });
+    }
+
+    Object.assign(activityDashboard, {
+      [postId]: [_activity, ...(activityDashboard[postId] || [])],
     });
   }
 
   function loadPostActivity(postId: string, activities: IPostActivity[]) {
+    const activityArr: Array<IPostActivity> = [];
+    const dashboardActivityArr: Array<IPostActivity> = [];
+
+    for (let i = 0; i < activities.length; i++) {
+      const _activity = activities[i];
+      if (!_activity.comment.is_internal) {
+        activityArr.push(_activity);
+      }
+
+      dashboardActivityArr.push(_activity);
+    }
+
+    console.log("dashboardActivityArr:", dashboardActivityArr);
     Object.assign(activity, {
-      [postId]: [...(activity[postId] || []), ...activities],
+      [postId]: [...(activity[postId] || []), ...activityArr],
+    });
+    Object.assign(activityDashboard, {
+      [postId]: [...(activityDashboard[postId] || []), ...dashboardActivityArr],
     });
   }
 
@@ -21,10 +44,14 @@ export const usePostActivityEEStore = defineStore("postActivityEE", () => {
     Object.assign(activity, {
       [postId]: [],
     });
+    Object.assign(activityDashboard, {
+      [postId]: [],
+    });
   }
 
   return {
     activity,
+    activityDashboard,
 
     addPostActivity,
     loadPostActivity,
