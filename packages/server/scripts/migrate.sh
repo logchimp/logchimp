ed80c8813302e95cf562deda0a8b998c5a8cee0f
Yashu Mittal <mittalyashu77@gmail.com>
2026-04-18 12:03:47 +0530

    feat(server/migrate): seeding system roles using migrate.sh file (#1760)
    * feat(server/migrate): seeding system roles using migrate.sh file
    
    * feat(migrate): quote the interpolated paths in migrate.sh

--- packages/server/scripts/migrate.sh
+++ packages/server/scripts/migrate.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# ------------------------
+# Database migrations
+# ------------------------
+
 # Default path set to production
 CONFIG_PATH="dist/database/config.js"
 
@@ -12,5 +16,25 @@ else
   echo "Using production config: $CONFIG_PATH"
 fi
 
-pnpm knex --knexfile ./$CONFIG_PATH migrate:latest && \
-  echo '=> Migration done'
+echo '=> Starting: Database migration' && \
+  pnpm knex --knexfile ./$CONFIG_PATH migrate:latest && \
+  echo '=> Completed: Database migration'
+
+# ------------------------
+# Seed system roles
+# ------------------------
+
+SYSTEM_ROLE_FILE_BASE_PATH="ee/services/roles/system"
+
+echo '=> Starting: Seeding system roles and permissions'
+
+if [ "$NODE_ENV" = "development" ]
+then
+  echo "Seeding in development env"
+  pnpm ts-node "src/$SYSTEM_ROLE_FILE_BASE_PATH.ts"
+else
+  echo "Seeding in production env"
+  node "dist/$SYSTEM_ROLE_FILE_BASE_PATH.js"
+fi
+
+echo '=> Completed: Seeding system roles and permissions'
