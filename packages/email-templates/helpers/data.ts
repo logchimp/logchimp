306ce926a25f9b130788fbf55d0cceb71aba5bfc
Yashu Mittal <mittalyashu77@gmail.com>
2026-07-18 17:31:41 +0530

    refactor(email-templates): centralize default template data for preview in `helpers/data.ts`

---
+++ packages/email-templates/helpers/data.ts
@@ -0,0 +1,8 @@
+export const defaultData = {
+  siteTitle: "LogChimp",
+  siteLogo: "https://cdn.logchimp.codecarrot.net/logchimp_circular_logo.png",
+  recipientEmail: "me@example.com",
+  url: "https://logchimp.app",
+  domain: "logchimp.app",
+  brandColor: "#484d7c",
+};
