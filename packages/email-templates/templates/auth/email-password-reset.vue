9cf577496af3d740f198a6412c812cd7372f1ffa
Yashu Mittal <mittalyashu77@gmail.com>
2026-07-18 17:27:21 +0530

    fix(email-templates): replace css variable `--brand-color` with inline css style  for brand coloring

--- packages/email-templates/templates/auth/email-password-reset.vue
+++ packages/email-templates/templates/auth/email-password-reset.vue
@@ -12,7 +12,7 @@ const data = {
 </script>
 
 <template>
-  <Layout class="p-4 bg-white" :style="{ '--brand-color': preview('brandColor', data.brandColor) }">
+  <Layout class="p-4 bg-white">
     <link
       href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
       rel="stylesheet"
@@ -38,21 +38,26 @@ const data = {
       </Text>
 
       <Text>
-        <Button
+        <a
           :href="preview('resetLink', data.resetLink)"
           target="_blank"
           rel="noreferrer"
-          class="text-white bg-(--brand-color) font-medium py-3.5 px-6 rounded-md"
+          class="text-white font-medium py-3 px-5 rounded-md text-sm no-underline inline-block"
+          :style="{
+            'background-color': preview('brandColor', data.brandColor),
+          }"
         >
           Reset password
-        </Button>
+        </a>
       </Text>
 
       <Text class="text-sm text-neutral-600">
       Can't click the button above? Copy and paste this link into your browser:
       <a
         :href="preview('resetLink', data.resetLink)" target="_blank" rel="noreferrer"
-        class="text-(--brand-color)"
+        :style="{
+          'color': preview('brandColor', data.brandColor),
+        }"
       >
         {{ preview('resetLink', data.resetLink) }}
       </a>
@@ -65,7 +70,14 @@ const data = {
       <Text>
         {{ preview('siteTitle', data.siteTitle) }}
         <br />
-        <a :href="preview('url', data.url)" target="_blank" rel="noreferrer" class="text-(--brand-color)">
+        <a
+          :href="preview('url', data.url)"
+          target="_blank"
+          rel="noreferrer"
+          :style="{
+            'color': preview('brandColor', data.brandColor),
+          }"
+        >
           {{ preview('domain', data.domain) }}
         </a>
       </Text>
