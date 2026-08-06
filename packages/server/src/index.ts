6f214f1577f400e8d70990f3898089f1ed2309ac
Yashu Mittal <mittalyashu77@gmail.com>
2026-08-06 19:12:38 +0530

    refactor(server): reorganize background worker initialization during server startup

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -19,14 +19,14 @@ const host = config.serverHost || "0.0.0.0";
 (async () => {
   await routesReady;
 
+  // Run background worker
+  const connection = await createWorkerClient();
+  await mailWorker.run(connection);
+
   app.listen(port, host, async () => {
     logger.info(`LogChimp is running in ${process.env.NODE_ENV}...`);
     logger.info(`Listening on port: ${port}`);
     logger.info("Ctrl+C to shut down");
     logger.info(`LogChimp boot ${(Date.now() - startTime) / 1000}s`);
   });
-
-  // Run background worker
-  const connection = await createWorkerClient();
-  await mailWorker.run(connection);
 })();
