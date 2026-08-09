86e07a841049f4b7e3312e8b60b59d707e34e47a
Yashu Mittal <mittalyashu77@gmail.com>
2026-08-09 17:36:09 +0530

    fix(server/worker): return CE + EE queues for users with an active license, otherwise only the CE queue

--- packages/server/src/worker/tasks/index.ts
+++ packages/server/src/worker/tasks/index.ts
@@ -2,11 +2,14 @@ import { mailQueue } from "./mail";
 import { postsQueue } from "./posts";
 
 async function loadQueues() {
+  const ceQueues = [mailQueue, postsQueue];
+
   try {
     const { getEEQueues } = await import("../../ee/worker/tasks");
-    return await getEEQueues();
+    const eeQueues = await getEEQueues();
+    return [...eeQueues, ...ceQueues];
   } catch (_) {
-    return [mailQueue, postsQueue];
+    return ceQueues;
   }
 }
 
