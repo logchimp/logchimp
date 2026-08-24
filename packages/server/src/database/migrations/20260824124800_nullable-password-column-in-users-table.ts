fea7d0b2265b73c1f9e77f96a551ccd657f70ece
Yashu Mittal <mittalyashu77@gmail.com>
2026-08-24 18:36:29 +0530

    feat(db:migration): change `password` column in `users` table as nullable

---
+++ packages/server/src/database/migrations/20260824124800_nullable-password-column-in-users-table.ts
@@ -0,0 +1,44 @@
+import type { Knex } from "knex";
+import logger from "../../utils/logger";
+
+export async function up(knex: Knex): Promise<void> {
+  try {
+    await knex.schema.alterTable("users", (t) => {
+      t.setNullable("password");
+    });
+
+    logger.info({
+      code: "DATABASE_MIGRATIONS",
+      message: "Change 'password' column to nullable in 'users' table",
+    });
+  } catch (err) {
+    logger.error({
+      code: "DATABASE_MIGRATIONS",
+      message:
+        "Failed to change 'password' column to nullable in 'users' table",
+      error: err,
+    });
+    throw err;
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  try {
+    await knex.schema.alterTable("users", (t) => {
+      t.dropNullable("password");
+    });
+
+    logger.info({
+      code: "DATABASE_MIGRATIONS",
+      message: "Change 'password' column not nullable in 'users' table",
+    });
+  } catch (err) {
+    logger.error({
+      code: "DATABASE_MIGRATIONS",
+      message:
+        "Failed to change 'password' column not nullable in 'users' table",
+      error: err,
+    });
+    throw err;
+  }
+}
