5642cbb49c8452228db256f3c9835cfc72464abe
Yashu Mittal <mittalyashu77@gmail.com>
2026-08-25 22:26:15 +0530

    refactor(server/roles): wrap `assignEveryoneRoleQuery` in database transaction

--- packages/server/src/repository/roles.ts
+++ packages/server/src/repository/roles.ts
@@ -2,21 +2,22 @@ import { v4 as uuidv4 } from "uuid";
 import database from "../database";
 
 // assign '@everyone' role
-export async function assignEveryoneRoleQuery(userId: string) {
-  const getRole = await database
-    .select<{ id: string }>("id")
-    .from("roles")
-    .where({
-      name: "@everyone",
-      is_system: 1,
-    })
-    .first();
+export const assignEveryoneRoleQuery = async (userId: string) =>
+  await database.transaction(async (trx) => {
+    const getRole = await trx
+      .select<{ id: string }>("id")
+      .from("roles")
+      .where({
+        name: "@everyone",
+        is_system: 1,
+      })
+      .first();
 
-  await database
-    .insert({
-      id: uuidv4(),
-      role_id: getRole.id,
-      user_id: userId,
-    })
-    .into("roles_users");
-}
+    await trx
+      .insert({
+        id: uuidv4(),
+        role_id: getRole.id,
+        user_id: userId,
+      })
+      .into("roles_users");
+  });
