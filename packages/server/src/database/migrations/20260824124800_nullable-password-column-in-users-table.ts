c4ca6016079539d6ec049b121a4dfab2d4700f94
Yashu Mittal <mittalyashu77@gmail.com>
2026-08-28 19:05:38 +0530

    fix(db:migrate): set 'password' column nullable in 20260824124800_nullable-password-column-in-users-table

--- packages/server/src/database/migrations/20260824124800_nullable-password-column-in-users-table.ts
+++ packages/server/src/database/migrations/20260824124800_nullable-password-column-in-users-table.ts
@@ -3,30 +3,20 @@ import logger from "../../utils/logger";
 
 export async function up(knex: Knex): Promise<void> {
   try {
-    const userWithoutPassword = await knex("users")
-      .whereNull("password")
-      .first("userId");
-
-    if (userWithoutPassword) {
-      throw new Error(
-        "Cannot restore NOT NULL on users.password while users without passwords exist.",
-      );
-    }
-
     await knex.schema.alterTable("users", (t) => {
-      t.dropNullable("password");
+      t.setNullable("password");
     });
 
     logger.info({
       code: "DATABASE_MIGRATIONS",
-      message: "Change 'password' column to NOT NULL in 'users' table",
+      message: "Change 'password' column to nullable in 'users' table",
     });
   } catch (err) {
     logger.error({
       code: "DATABASE_MIGRATIONS",
       message:
-        "Failed to change 'password' column to NOT NULL in 'users' table",
-      err,
+        "Failed to change 'password' column to nullable in 'users' table",
+      error: err,
     });
     throw err;
   }
