6700d883b5814b198717ddb49c88fc0ed19da9da
Yashu Mittal <mittalyashu77@gmail.com>
2026-09-11 15:17:54 +0530

    test(board): add test case for board name search with back slash

--- packages/server/tests/integration/v1/boards.spec.ts
+++ packages/server/tests/integration/v1/boards.spec.ts
@@ -649,6 +649,7 @@ describeEE("GET /boards/search/:name", () => {
     { name: "a@@@@@@@@", searchTerm: "a@@" },
     { name: "completion 100% ready", searchTerm: "100%" },
     { name: "board_with_underscore", searchTerm: "_" },
+    { name: String.raw`board\\with\\backslash`, searchTerm: String.raw`\\` },
   ];
 
   itEE.each(searchSpecialCases)(
