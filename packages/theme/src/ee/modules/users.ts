f31deea1bbed69c5446b0d093330f3b0c7c4495a
Yashu Mittal <mittalyashu77@gmail.com>
2026-09-15 18:26:30 +0530

    refactor(users): rename `UsersEe` to `UsersEEAPI` for consistent naming convention

--- packages/theme/src/ee/modules/users.ts
+++ packages/theme/src/ee/modules/users.ts
@@ -5,7 +5,7 @@ import { VITE_API_URL } from "../../constants";
 // store
 import { APIService } from "../../modules/api";
 
-export class UsersEe extends APIService {
+export class UsersEEAPI extends APIService {
   constructor(baseURL?: string) {
     super(baseURL || `${VITE_API_URL}/api`);
   }
