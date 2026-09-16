7ec1402c70bd1b2aa0933a8fa4a5f3b871882456
Yashu Mittal <mittalyashu77@gmail.com>
2026-09-16 21:49:13 +0530

    chore: format the code

--- packages/server/src/database/migrations/20201026113105_email-verification-table.ts
+++ packages/server/src/database/migrations/20201026113105_email-verification-table.ts
@@ -7,7 +7,6 @@ export async function up(knex: Knex): Promise<void> {
       table
         .string("email", 320)
         .notNullable()
-
         .primary()
         .references("email")
         .inTable("users")
