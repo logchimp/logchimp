6a7051e6466498afb9c820ff8f9ce803410e6a20
Yashu Mittal <mittalyashu77@gmail.com>
2026-09-16 18:48:49 +0530

    refactor(theme): Migrate `roles` API calls to API services class (#1928)
    * refactor(roles): integrate RolesEE API service for roles operations
    
    * fix(theme/roles): use `PATCH` method instead of `PUT` for role updates in `modules/roles.ts`

--- packages/theme/src/ee/modules/roles.ts
+++ packages/theme/src/ee/modules/roles.ts
@@ -1,9 +1,8 @@
-import axios, { type AxiosResponse } from "axios";
 import type {
   ICreateRoleResponseBody,
+  IGetRoleByIdResponseBody,
   IGetRolesParams,
   IPaginatedRolesResponse,
-  IGetRoleByIdResponseBody,
   IUpdateRoleRequestBody,
   IUpdateRoleResponseBody,
 } from "@logchimp/types";
@@ -11,78 +10,58 @@ import type {
 import { VITE_API_URL } from "../../constants";
 
 // store
-import { useUserStore } from "../../store/user";
 import { APIService } from "../../modules/api";
 
-/**
- * Get role by UUID
- * @param {string} id role id
- * @returns {Promise<AxiosResponse<IGetRoleByIdResponseBody>>} response
- */
-export const getRole = async (
-  id: string,
-): Promise<AxiosResponse<IGetRoleByIdResponseBody>> => {
-  const { authToken } = useUserStore();
-
-  return await axios({
-    method: "GET",
-    url: `${VITE_API_URL}/api/v1/roles/${encodeURIComponent(id)}`,
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },
-  });
-};
-
-/**
- * Create role
- * @returns {Promise<AxiosResponse<ICreateRoleResponseBody>>} response
- */
-export const createRole = async (): Promise<
-  AxiosResponse<ICreateRoleResponseBody>
-> => {
-  const { authToken } = useUserStore();
-
-  return await axios({
-    method: "POST",
-    url: `${VITE_API_URL}/api/v1/roles`,
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },
-  });
-};
-
-/**
- * Update a role
- * @param {object} role update role
- * @param {string} role.id role id
- * @param {string} role.name role name
- * @param {string} role.description role description
- * @param {string[]} role.permissions list of permission
- * @returns {Promise<AxiosResponse<IUpdateRoleResponseBody>>} response
- */
-export const updateRole = async (
-  role: IUpdateRoleRequestBody,
-): Promise<AxiosResponse<IUpdateRoleResponseBody>> => {
-  const { authToken } = useUserStore();
-
-  return await axios({
-    method: "PATCH",
-    url: `${VITE_API_URL}/api/v1/roles`,
-    data: {
-      ...role,
-    },
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },
-  });
-};
-
-export class Roles extends APIService {
+export class RolesEEAPI extends APIService {
   constructor(baseURL?: string) {
     super(baseURL || `${VITE_API_URL}/api`);
   }
 
-  async getAll(params: IGetRolesParams = {}): Promise<IPaginatedRolesResponse> {
+  /**
+   * Get role by UUID
+   * @param {string} id role id
+   * @returns {Promise<IGetRoleByIdResponseBody>} response
+   */
+  GetRole = async (id: string): Promise<IGetRoleByIdResponseBody> => {
+    return this.get(`/v1/roles/${encodeURIComponent(id)}`)
+      .then((response) => response?.data)
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  /**
+   * Create role
+   * @returns {Promise<ICreateRoleResponseBody>} response
+   */
+  CreateRole = async (): Promise<ICreateRoleResponseBody> => {
+    return this.post("/v1/roles")
+      .then((response) => response?.data)
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  /**
+   * Update a role
+   * @param {object} role update role
+   * @param {string} role.id role id
+   * @param {string} role.name role name
+   * @param {string} role.description role description
+   * @param {string[]} role.permissions list of permission
+   * @returns {Promise<IUpdateRoleResponseBody>} response
+   */
+  UpdateRole = async (
+    role: IUpdateRoleRequestBody,
+  ): Promise<IUpdateRoleResponseBody> => {
+    return this.patch("/v1/roles", role)
+      .then((response) => response?.data)
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  async GetAll(params: IGetRolesParams = {}): Promise<IPaginatedRolesResponse> {
     const searchParams = new URLSearchParams();
 
     for (const paramsKey in params) {
